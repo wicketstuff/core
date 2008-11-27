496f0b0784b30a85e6885ac56e54aec4b4142058
Jeremy Thomerson <jthomerson@wicketstuff.org>
2008-11-27 22:46:38 +0000

    restructuring parent / child / examples
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@4367 ef7698a4-5110-0410-9fc6-c7eb3693863f

---
+++ calendarviews/calendarviews-examples/src/main/java/org/wicketstuff/calendarviews/LargeView.java
@@ -0,0 +1,126 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.wicketstuff.calendarviews;
+
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.list.ListItem;
+import org.apache.wicket.markup.html.list.ListView;
+import org.apache.wicket.markup.repeater.Item;
+import org.apache.wicket.markup.repeater.data.GridView;
+import org.apache.wicket.markup.repeater.data.IDataProvider;
+import org.apache.wicket.model.AbstractReadOnlyModel;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.PropertyModel;
+import org.joda.time.DateMidnight;
+import org.joda.time.DateTime;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.wicketstuff.calendarviews.model.IEvent;
+import org.wicketstuff.calendarviews.model.IEventProvider;
+
+/**
+ * @author Jeremy Thomerson
+ */
+public class LargeView extends FullWeekCalendarView {
+	
+	private static final long serialVersionUID = 1L;
+	private static final Logger LOGGER = LoggerFactory.getLogger(LargeView.class);
+
+	public LargeView(String id, Date startDate, Date endDate, IEventProvider eventProvider) {
+		super(id, startDate, endDate, eventProvider);
+
+		IDataProvider<DateMidnight> dp = createDaysDataProvider();
+		Collection<? extends IEvent> allEvents = getEventProvider().getObject();
+		final Map<DateMidnight, List<IEvent>> mapOfEvents = convertToMapByDay(allEvents);
+		
+		if (LOGGER.isDebugEnabled()) {
+			LOGGER.debug("Data provider: " + dp);
+		}
+		add(new LargeGridView("rows", dp, mapOfEvents));
+	}
+
+	protected ListView<IEvent> createEventListView(String id, IModel<List<IEvent>> model) {
+		return new ListView<IEvent>(id, model) {
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			protected void populateItem(ListItem<IEvent> item) {
+				item.add(new Label("title", new PropertyModel<String>(item.getModel(), "title")));
+			}
+		};
+	}
+
+	private class LargeGridView extends GridView<DateMidnight> {
+		private static final long serialVersionUID = 1L;
+		private transient Map<DateMidnight, List<IEvent>> mMapOfEvents;
+
+		public LargeGridView(String id, IDataProvider<DateMidnight> dp, Map<DateMidnight, List<IEvent>> mapOfEvents) {
+			super(id, dp);
+			mMapOfEvents = mapOfEvents;
+		}
+		
+		@Override
+		public int getColumns() {
+			return LargeView.this.getNumberOfColumns();
+		}
+
+		@Override
+		protected void populateEmptyItem(Item<DateMidnight> item) {
+			throw new UnsupportedOperationException("LargeView should not have any empty items");
+		}
+
+		@Override
+		protected void populateItem(final Item<DateMidnight> item) {
+			item.add(new Label("date", new PropertyModel<Integer>(item.getModel(), "dayOfMonth")));
+			item.add(createEventListView("events", new AbstractReadOnlyModel<List<IEvent>>() {
+				private static final long serialVersionUID = 1L;
+
+				@Override
+				public List<IEvent> getObject() {
+					return mMapOfEvents.get(item.getModelObject());
+				}
+				
+			}));
+		}
+		
+		@Override
+		protected void onDetach() {
+			super.onDetach();
+			mMapOfEvents = null;
+		}
+	}
+
+	public static LargeView createWeeksView(String id, IEventProvider eventProvider, int weeks) {
+		// TODO add a similar method that allows an offset of weeks (i.e. 3 weeks, starting two weeks past today)
+		Date start = new Date();
+		Date end = new DateTime(start).plusWeeks(weeks - 1).toDate();
+		return new LargeView(id, start, end, eventProvider);
+	}
+
+	public static LargeView createMonthView(String id, IEventProvider eventProvider) {
+		Date start = new DateTime().dayOfMonth().setCopy(1).toDate();
+		Date end = new DateTime(start).plusMonths(1).minusDays(1).toDate();
+		return new LargeView(id, start, end, eventProvider);
+	}
+	
+}
---
+++ calendarviews/calendarviews-examples/src/main/java/org/wicketstuff/calendarviews/model/BasicEvent.java
@@ -0,0 +1,125 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.wicketstuff.calendarviews.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * This is just a simple <tt>IEvent</tt> implementation, mostly used for
+ * testing. You may use it as a wrapper in your own application if you would
+ * like, but it is not necessary.
+ * 
+ * @author Jeremy Thomerson
+ */
+public class BasicEvent implements IEvent, Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private String mTitle;
+	private Date mEndTime;
+	private Date mStartTime;
+	private boolean mAllDayEvent;
+
+	public BasicEvent() {
+		// no-op
+	}
+	
+	public BasicEvent(String title, Date endTime, Date startTime, boolean allDayEvent) {
+		super();
+		mTitle = title;
+		mEndTime = endTime;
+		mStartTime = startTime;
+		mAllDayEvent = allDayEvent;
+	}
+
+	public String getTitle() {
+		return mTitle;
+	}
+
+	public void setTitle(String title) {
+		mTitle = title;
+	}
+
+	public Date getEndTime() {
+		return mEndTime;
+	}
+
+	public void setEndTime(Date endTime) {
+		mEndTime = endTime;
+	}
+
+	public Date getStartTime() {
+		return mStartTime;
+	}
+
+	public void setStartTime(Date startTime) {
+		mStartTime = startTime;
+	}
+
+	public boolean isAllDayEvent() {
+		return mAllDayEvent;
+	}
+
+	public void setAllDayEvent(boolean allDayEvent) {
+		mAllDayEvent = allDayEvent;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (mAllDayEvent ? 1231 : 1237);
+		result = prime * result
+				+ ((mEndTime == null) ? 0 : mEndTime.hashCode());
+		result = prime * result
+				+ ((mStartTime == null) ? 0 : mStartTime.hashCode());
+		result = prime * result + ((mTitle == null) ? 0 : mTitle.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		BasicEvent other = (BasicEvent) obj;
+		if (mAllDayEvent != other.mAllDayEvent)
+			return false;
+		if (mEndTime == null) {
+			if (other.mEndTime != null)
+				return false;
+		} else if (!mEndTime.equals(other.mEndTime))
+			return false;
+		if (mStartTime == null) {
+			if (other.mStartTime != null)
+				return false;
+		} else if (!mStartTime.equals(other.mStartTime))
+			return false;
+		if (mTitle == null) {
+			if (other.mTitle != null)
+				return false;
+		} else if (!mTitle.equals(other.mTitle))
+			return false;
+		return true;
+	}
+
+}
