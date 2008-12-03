e96b8b7383c1c584a7eeb34d27fe4b42498fb902
Jeremy Thomerson <jthomerson@wicketstuff.org>
2008-12-03 06:52:15 +0000

    getting license plugin / formatting in client-and-server-validation
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@4469 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- client-and-server-validation/src/main/java/org/wicketstuff/validation/client/validation.js
+++ client-and-server-validation/src/main/java/org/wicketstuff/validation/client/validation.js
@@ -1,4 +1,6 @@
 /*
+ * Copyright (C) 2008 Jeremy Thomerson <jeremy@thomersonfamily.com>
+ *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
@@ -14,7 +16,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
- 
 /*
  * Wicket Client and Server Validation Framework 
  *
