6da82cc7e6a01d6b575fcfc524679e9527fcad8f
Nino Martinez Wael <nmwael@wicketstuff.org>
2008-12-22 00:07:26 +0000

    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk/wicketstuff-core@4534 ef7698a4-5110-0410-9fc6-c7eb3693863f

---
+++ artwork-parent/artwork/src/main/java/org/wicketstuff/artwork/ArtworkCanvasBehavior.java
@@ -0,0 +1,118 @@
+package org.wicketstuff.artwork;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.wicket.Component;
+import org.apache.wicket.ResourceReference;
+import org.apache.wicket.behavior.AbstractBehavior;
+import org.apache.wicket.markup.html.IHeaderContributor;
+import org.apache.wicket.markup.html.IHeaderResponse;
+import org.wicketstuff.artwork.graphics.Graphics;
+import org.wicketstuff.jslibraries.JSReference;
+import org.wicketstuff.jslibraries.Library;
+import org.wicketstuff.jslibraries.VersionDescriptor;
+/**
+ * 
+ * @author Nino Martinez (nino.martinez.wael *at* gmail *dot* com remember no stars)
+ *
+ */
+public class ArtworkCanvasBehavior extends AbstractBehavior
+		implements IHeaderContributor {
+
+	private List<Graphics> graphicsList;
+	
+	public ArtworkCanvasBehavior(Graphics... graphics) {
+		super();
+		this.graphicsList=Arrays.asList(graphics);
+	}
+	/** The target component. */
+	private Component component;
+
+	@Override
+	public void bind(Component component) {
+		super.bind(component);
+		this.component = component;
+		component.setOutputMarkupId(true);
+
+	} 
+	
+	@Override
+	public void onRendered(Component component) {
+		super.onRendered(component);
+	}
+	@Override
+	public void renderHead(IHeaderResponse response) {
+		response.renderJavascriptReference(getJQueryReference());
+		response.renderJavascriptReference(getLiquidCanvasReference());
+		response.renderJavascriptReference(getLiquidCanvasPluginsReference());
+		response.renderOnLoadJavascript(getCanvasJS());
+	}
+	
+	/**
+	 * Returns the liquid canvas js
+	 * @return
+	 */
+	private String getCanvasJS(){
+		
+		String js="$(\"#"+component.getMarkupId()+"\").liquidCanvas(\"";
+		String endJs="\")";
+		
+		boolean first=true;
+		for(Graphics g:graphicsList){
+			if(!first){
+				js+=" => ";
+			}
+			if(g.isChained())
+			{
+				js+="[ ";
+			}
+			js+=" "+g.getStringForJS();	
+			if(g.isChained())
+			{
+				boolean moreChainedGraphics=true;
+				Graphics inspect=g;
+				while(moreChainedGraphics){
+					if(inspect.isChained()){
+						js+=" ,"+inspect.getStringForJS();
+						inspect=inspect.getChainedGraphics();
+					}
+					else{
+						moreChainedGraphics=false;
+					}
+					
+				}
+				
+				
+				js+=" ]";
+			}
+			first=false;
+		}
+				
+		js+=endJs;
+		return js;
+		
+	}
+	
+	
+	
+
+	private ResourceReference getLiquidCanvasPluginsReference() {
+		return new ResourceReference(ArtworkCanvasBehavior.class,
+				"liquid-canvas-plugins.js");
+
+	}
+
+	private ResourceReference getLiquidCanvasReference() {
+		return new ResourceReference(ArtworkCanvasBehavior.class,
+				"liquid-canvas.js");
+
+	}
+
+	private ResourceReference getJQueryReference() {
+
+		return JSReference.getReference(VersionDescriptor.exactVersion(
+				Library.JQUERY, 1, 2, 6));
+
+	}
+}
---
+++ artwork-parent/artwork/src/main/java/org/wicketstuff/artwork/liquid-canvas-plugins.js
@@ -0,0 +1,108 @@
+(function($) {
+
+  $.registerLiquidCanvasPlugin({
+    name: "rect",
+    paint: function(area) {
+      area.ctx.beginPath();
+      area.ctx.rect(0, 0, area.width, area.height);
+      area.ctx.closePath();
+      if (this.action) this.action.paint(area);  // for chaining
+    }
+  });
+  
+  $.registerLiquidCanvasPlugin({
+    name: "roundedRect",
+    defaultOpts: { radius:20 },
+    paint: function(area) {
+      var ctx = area.ctx;
+      var opts = this.opts;
+      ctx.beginPath();
+      ctx.moveTo(0, opts.radius);
+      ctx.lineTo(0, area.height - opts.radius);
+      ctx.quadraticCurveTo(0, area.height, opts.radius, area.height);
+      ctx.lineTo(area.width - opts.radius, area.height);
+      ctx.quadraticCurveTo(area.width, area.height, area.width, area.height - opts.radius);
+      ctx.lineTo(area.width, opts.radius);
+      ctx.quadraticCurveTo(area.width, 0, area.width - opts.radius, 0);
+      ctx.lineTo(opts.radius, 0);
+      ctx.quadraticCurveTo(0, 0, 0, opts.radius);
+      ctx.closePath();
+      if (this.action) this.action.paint(area);  // for chaining
+    },
+    shrink: function(area, steps) {
+      this.defaultShrink(area, steps);
+      this.opts.radius -= steps;
+    }
+  });
+  
+  // This is a Liquid Canvas Plugin
+  $.registerLiquidCanvasPlugin({
+    name: "fill",
+    defaultOpts: { color:"#aaa" },
+    paint: function(area) {
+      area.ctx.fillStyle = this.opts.color;
+      this.action.paint(area);
+      area.ctx.fill();
+    }
+  });
+
+  $.registerLiquidCanvasPlugin({  // hmmmmmmm, no rotation? no width??? ah patterns!
+    name: "image",
+    defaultOpts: { url:"http://www.ruzee.com/files/liquid-canvas-image.png" },
+    paint: function(area) {
+      var image = new Image();
+      image.src = this.opts.url;
+      image.onload = function() { 
+        area.ctx.drawImage(this, 0, 0); 
+      };
+    }
+  });
+
+  // This is a Liquid Canvas Plugin
+  $.registerLiquidCanvasPlugin({
+    name: "gradient",
+    defaultOpts: { from: "#fff", to:"#666" },
+    paint: function(area) {
+      var grad = area.ctx.createLinearGradient(0, 0, 0, area.height);
+      grad.addColorStop(0, this.opts.from);
+      grad.addColorStop(1, this.opts.to);
+      area.ctx.fillStyle = grad;
+      this.action.paint(area);
+      area.ctx.fill();
+    }
+  });
+  // End of Liquid Canvas Plugin
+  
+  $.registerLiquidCanvasPlugin({
+    name: "shadow",
+    defaultOpts: { width:3, color:'#000', shift:2 },
+    paint: function(area) {
+      var sw = this.opts.width;
+      
+      area.ctx.fillStyle = this.opts.color; 
+      area.ctx.globalAlpha = 1.0 / sw;
+      for (var s = 0; s < sw; ++s) {
+        this.action.paint(area);
+        area.ctx.fill();
+        this.action.shrink(area, 1);
+      }
+      area.ctx.globalAlpha = 1;
+      area.ctx.translate(0, -this.opts.shift);
+    }
+  });
+
+  $.registerLiquidCanvasPlugin({
+    name: "border",
+    defaultOpts: { color:'#f80', width:3 },
+    paint: function(area) {
+      var bw = this.opts.width;
+      area.ctx.strokeStyle = this.opts.color;
+      area.ctx.lineWidth = bw;
+      this.action.shrink(area, bw / 2);
+      this.action.paint(area);
+      area.ctx.stroke();
+      this.action.shrink(area, bw / 2);
+    }
+  });
+
+})(jQuery);
