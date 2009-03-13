f46c5282636dc5237473f0895804ccbb14e8eea7
Richard Wiesinger <ridsch@wicketstuff.org>
2009-03-13 15:28:41 +0000

    Added support for Open Street Map (Layers: Mapnik, Tiles@Home, CycleMap).
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk/wicketstuff-core@4609 ef7698a4-5110-0410-9fc6-c7eb3693863f

---
+++ openlayers-parent/openlayers-examples/src/main/java/org/wicketstuff/openlayers/SimpleOpenStreetMapPage.java
@@ -0,0 +1,53 @@
+package org.wicketstuff.openlayers;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import org.apache.wicket.markup.html.WebPage;
+import org.wicketstuff.openlayers.api.Bounds;
+import org.wicketstuff.openlayers.api.Control;
+import org.wicketstuff.openlayers.api.LonLat;
+import org.wicketstuff.openlayers.api.layer.Layer;
+import org.wicketstuff.openlayers.api.layer.OSM;
+import org.wicketstuff.openlayers.api.layer.OSM.OSMLayer;
+
+/**
+ * @author RW
+ */
+public class SimpleOpenStreetMapPage extends WebPage {
+
+	public SimpleOpenStreetMapPage(){
+		
+		List<Layer> layers = new ArrayList<Layer>();	
+		
+		Layer layerOSMTilesAtHome = new OSM("Osmarender", OSMLayer.TilesAtHome);
+		Layer layerOSMMapnik = new OSM("Mapnik", OSMLayer.Mapnik);
+		Layer layerOSMCycleMap = new OSM("CycleMap", OSMLayer.CycleMap);
+		
+		layers.add(layerOSMMapnik);
+		layers.add(layerOSMTilesAtHome);		
+		layers.add(layerOSMCycleMap);
+		
+		HashMap<String, String> mapOptions = new HashMap<String, String>();
+		Bounds boundsExtend = new Bounds(new LonLat(-20037508.34, -20037508.34), new LonLat(20037508.34, 20037508.34));		
+		mapOptions.put("maxExtent", boundsExtend.getJSconstructor());
+		mapOptions.put("projection", "new OpenLayers.Projection('EPSG:900913')");
+		mapOptions.put("displayProjection", "new OpenLayers.Projection('EPSG:4326')");
+		mapOptions.put("units", "'meters'");
+		mapOptions.put("maxResolution", "156543");
+		mapOptions.put("numZoomLevels", "18");
+
+		OpenLayersMap map = new OpenLayersMap("map", layers, mapOptions);		
+	
+		map.addControl(Control.LayerSwitcher);
+		map.addControl(Control.MousePosition);
+		map.addControl(Control.KeyboardDefaults);		
+		
+		//map.setCenter(new LonLat(10.2, 48.9));
+		//map.setZoom(3);		
+		
+		add(map);
+	}
+}
+
