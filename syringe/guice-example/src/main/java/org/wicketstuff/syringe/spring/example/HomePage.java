4c66e3f027904aaf24f49490988692342b8dcba3
James Carman <jwcarman@wicketstuff.org>
2009-03-20 20:52:37 +0000

    Copying to start guice example
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk/wicketstuff-core@4623 ef7698a4-5110-0410-9fc6-c7eb3693863f

---
+++ syringe/guice-example/src/main/java/org/wicketstuff/syringe/spring/example/HomePage.java
@@ -0,0 +1,22 @@
+package org.wicketstuff.syringe.spring.example;
+
+import org.apache.wicket.PageParameters;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.WebPage;
+import org.wicketstuff.syringe.Inject;
+
+/**
+ * @since 1.4
+ */
+public class HomePage extends WebPage
+{
+    private static final long serialVersionUID = 1L;
+
+    @Inject
+    private IMessageService messageService;
+
+    public HomePage(final PageParameters parameters)
+    {
+        add(new Label("message", messageService.getMessage()));
+    }
+}
