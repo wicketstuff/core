56502359310d6e37d5746e025244bd9e8941e149
James Carman <jwcarman@wicketstuff.org>
2009-03-25 01:46:24 +0000

    Beefing up examples to force serialization.
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk/wicketstuff-core@4638 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- syringe/webbeans-example/src/main/java/org/wicketstuff/syringe/webbeans/example/HomePage.java
+++ syringe/webbeans-example/src/main/java/org/wicketstuff/syringe/webbeans/example/HomePage.java
@@ -1,6 +1,9 @@
 package org.wicketstuff.syringe.webbeans.example;
 
 import org.apache.wicket.PageParameters;
+import org.apache.wicket.model.LoadableDetachableModel;
+import org.apache.wicket.ajax.markup.html.AjaxLink;
+import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.WebPage;
 import org.wicketstuff.syringe.Inject;
@@ -17,6 +20,22 @@ public class HomePage extends WebPage
 
     public HomePage(final PageParameters parameters)
     {
-        add(new Label("message", messageService.getMessage()));
+        final Label label = new Label("message", new LoadableDetachableModel<String>()
+        {
+            protected String load()
+            {
+                return messageService.getMessage();
+            }
+        } );
+        label.setOutputMarkupId(true);
+        add(label);
+        final AjaxLink refreshLink = new AjaxLink("refresh")
+        {
+            public void onClick(AjaxRequestTarget target)
+            {
+                target.addComponent(label);
+            }
+        };
+        add(refreshLink);
     }
 }
