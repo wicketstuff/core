a143e5f061e69e2edf815f06390795fc497630a5
Ryan McKinley <ryantxu@wicketstuff.org>
2009-03-28 03:24:32 +0000

    adding more class info
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk/wicketstuff-core@4647 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- ki-security/ki-security/src/main/java/org/wicketstuff/ki/KiSecurityManagerModel.java
+++ ki-security/ki-security/src/main/java/org/wicketstuff/ki/KiSecurityManagerModel.java
@@ -1,19 +1,35 @@
 package org.wicketstuff.ki;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.wicket.Application;
+import org.apache.wicket.RequestCycle;
 import org.apache.wicket.model.AbstractReadOnlyModel;
 import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.LoadableDetachableModel;
+import org.apache.wicket.protocol.http.WebApplication;
+import org.apache.wicket.protocol.http.WebRequestCycle;
 import org.jsecurity.SecurityUtils;
 import org.jsecurity.mgt.SecurityManager;
 import org.jsecurity.subject.Subject;
+import org.jsecurity.web.servlet.JSecurityFilter;
 
 /**
  * A simple wrapper for to access {@link SecurityUtils#getSecurityManager()}
  * as an {@link IModel}
  */
-public class KiSecurityManagerModel extends AbstractReadOnlyModel<SecurityManager>
+public class KiSecurityManagerModel extends LoadableDetachableModel<SecurityManager>
 {
   @Override
-  public SecurityManager getObject() {
-    return SecurityUtils.getSecurityManager();
+  protected SecurityManager load() {
+    SecurityManager m = SecurityUtils.getSecurityManager();
+    if( m == null ) {
+      m = (SecurityManager)WebApplication.get().getServletContext().getAttribute( JSecurityFilter.SECURITY_MANAGER_CONTEXT_KEY );
+    
+      HttpServletRequest req = 
+        ((WebRequestCycle)RequestCycle.get()).getWebRequest().getHttpServletRequest();
+      System.out.println( "REQ:"+req.getClass() );
+    }
+    return m;
   }
 }
