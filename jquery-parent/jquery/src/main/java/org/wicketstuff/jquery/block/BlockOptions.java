6c7863d2323a17edb41394c50404a3d67165c578
Ryan McKinley <ryantxu@wicketstuff.org>
2009-04-29 21:18:33 +0000

    adding blockUI support
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk/wicketstuff-core@4699 ef7698a4-5110-0410-9fc6-c7eb3693863f

---
+++ jquery-parent/jquery/src/main/java/org/wicketstuff/jquery/block/BlockOptions.java
@@ -0,0 +1,37 @@
+package org.wicketstuff.jquery.block;
+
+import org.wicketstuff.jquery.Options;
+
+
+public class BlockOptions extends Options {
+
+	public BlockOptions setMessage(String message) {
+		set("message", message);
+		return this;
+	}
+
+  public BlockOptions setCSS(Options css) {
+    set("css", css);
+    return this;
+  }
+
+  public BlockOptions setFadeIn(int ms) {
+    set("fadeIn", ms);
+    return this;
+  }
+
+  public BlockOptions setFadeOut(int ms) {
+    set("fadeOut", ms);
+    return this;
+  }
+
+  public BlockOptions setTimeout(int ms) {
+    set("timeout", ms);
+    return this;
+  }
+
+  public BlockOptions setShowOverlay(boolean v) {
+    set("showOverlay", v);
+    return this;
+  }
+}
---
+++ jquery-parent/jquery/src/main/java/org/wicketstuff/jquery/block/BlockingAjaxLink.java
@@ -0,0 +1,78 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.wicketstuff.jquery.block;
+
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.IAjaxCallDecorator;
+import org.apache.wicket.ajax.markup.html.AjaxLink;
+import org.apache.wicket.markup.html.IHeaderContributor;
+import org.apache.wicket.markup.html.IHeaderResponse;
+import org.apache.wicket.markup.html.resources.CompressedResourceReference;
+import org.wicketstuff.jquery.JQueryBehavior;
+
+public abstract class BlockingAjaxLink<T> extends AjaxLink<T> implements IHeaderContributor
+{
+  public static final CompressedResourceReference BLOCK_JS = new CompressedResourceReference(BlockingAjaxLink.class, "jquery.blockUI.js");
+
+  final BlockOptions options;
+  
+	public BlockingAjaxLink(final String id, BlockOptions options )
+	{
+		super(id, null);
+		this.options = options;
+	}
+
+  public BlockingAjaxLink(final String id, String message )
+  {
+    this(id, new BlockOptions().setMessage( message ) );
+  }
+
+  public void renderHead(IHeaderResponse response) {
+    response.renderJavascriptReference(JQueryBehavior.JQUERY_JS);
+    response.renderJavascriptReference(BLOCK_JS);
+  }
+  
+	/**
+	 * Returns ajax call decorator that will be used to decorate the ajax call.
+	 * 
+	 * @return ajax call decorator
+	 */
+	protected IAjaxCallDecorator getAjaxCallDecorator()
+	{
+		return new IAjaxCallDecorator() {
+      public CharSequence decorateScript(CharSequence script) {
+        return "$.blockUI( "+options.toString()+" ); " + script;
+      }
+      
+      public CharSequence decorateOnFailureScript(CharSequence script) {
+        return script;
+      }
+
+      public CharSequence decorateOnSuccessScript(CharSequence script) {
+        return script;
+      }
+		};
+	}
+
+  final public void onClick(final AjaxRequestTarget target)
+  {
+    doClick( target );
+    target.appendJavascript( "$.unblockUI()" );
+  }
+  
+  public abstract void doClick(final AjaxRequestTarget target);
+}
