c6404ed454c56b5e2f200361effffa610e266ff4
Ryan McKinley <ryantxu@wicketstuff.org>
2009-04-29 21:18:33 +0000

    adding blockUI support
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@4699 ef7698a4-5110-0410-9fc6-c7eb3693863f

---
+++ jquery-parent/jquery-examples/src/main/java/org/wicketstuff/jquery/demo/Page4Block.java
@@ -0,0 +1,42 @@
+package org.wicketstuff.jquery.demo;
+
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.markup.html.AjaxFallbackLink;
+import org.apache.wicket.ajax.markup.html.AjaxLink;
+import org.apache.wicket.markup.html.WebMarkupContainer;
+import org.apache.wicket.markup.html.link.Link;
+import org.apache.wicket.markup.html.panel.FeedbackPanel;
+import org.wicketstuff.jquery.Options;
+import org.wicketstuff.jquery.block.BlockOptions;
+import org.wicketstuff.jquery.block.BlockingAjaxLink;
+import org.wicketstuff.jquery.jgrowl.JGrowlFeedbackPanel;
+
+@SuppressWarnings("serial")
+public class Page4Block extends PageSupport {
+
+	public Page4Block() {
+
+	  WebMarkupContainer body = new WebMarkupContainer( "demo" );
+	  add( body );
+	  
+	  BlockOptions options = new BlockOptions();
+	  options.setMessage( "Hello!!!" );
+	  
+
+	  BlockingAjaxLink<Void> ajaxlink = new BlockingAjaxLink<Void>( "ajaxlink", "hello!" ) {
+      @Override
+      public void doClick(AjaxRequestTarget target) {
+        try {
+          Thread.sleep( 3000 );
+        } 
+        catch (InterruptedException e) {
+          e.printStackTrace();
+        }
+        info( "Clicked link: "+this.toString() );
+        target.addChildren( getPage(), FeedbackPanel.class );
+      }
+    };
+    body.add( ajaxlink );
+	}
+
+}
