d3cce3acc40b44030b1c1e0f7aa90c8a94acb11c
Nino Martinez Wael <nmwael@wicketstuff.org>
2009-05-04 18:34:38 +0000

    Nearing something usefull with niftycorners
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk/wicketstuff-core@4710 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- artwork-parent/artwork/src/main/java/org/wicketstuff/artwork/niftycorners/NiftyCornersBehavior.java
+++ artwork-parent/artwork/src/main/java/org/wicketstuff/artwork/niftycorners/NiftyCornersBehavior.java
@@ -2,15 +2,18 @@ package org.wicketstuff.artwork.niftycorners;
 
 import org.apache.wicket.Component;
 import org.apache.wicket.ResourceReference;
+import org.apache.wicket.Response;
 import org.apache.wicket.behavior.AbstractBehavior;
+import org.apache.wicket.markup.ComponentTag;
 import org.apache.wicket.markup.html.IHeaderResponse;
+import org.apache.wicket.markup.html.internal.HeaderResponse;
+import org.apache.wicket.util.string.JavascriptUtils;
 
 public class NiftyCornersBehavior extends AbstractBehavior {
 
-	String tagType = "";
+	String tagName = "";
 
-	public NiftyCornersBehavior(String tagType) {
-		this.tagType = tagType;
+	public NiftyCornersBehavior() {
 	}
 
 	/** The target component. */
@@ -28,21 +31,44 @@ public class NiftyCornersBehavior extends AbstractBehavior {
 	public void renderHead(IHeaderResponse response) {
 		response.renderJavascriptReference(getNiftyCornersJSReference());
 		response.renderCSSReference(getNiftyCornersCSSReference());
-		response.renderOnLoadJavascript(getNiftyJS());
+//		response.renderOnLoadJavascript(getNiftyJS(tagName));
 	}
 
-	private String getNiftyJS() {
+	private String getNiftyJS(String tagName) {
 
-		return "Nifty(\""+tagType+"#" + component.getMarkupId() + "\",\"big\")";
+		return "Nifty(\"" + tagName + "#" + component.getMarkupId()
+				+ "\",\"big\")";
 	}
 
 	private ResourceReference getNiftyCornersJSReference() {
 		return new ResourceReference(NiftyCornersBehavior.class, "niftycube.js");
 
 	}
+
 	private ResourceReference getNiftyCornersCSSReference() {
-		return new ResourceReference(NiftyCornersBehavior.class, "niftyCorners.css");
+		return new ResourceReference(NiftyCornersBehavior.class,
+				"niftyCorners.css");
 
 	}
 
+	@Override
+	public void onComponentTag(Component component, ComponentTag tag) {
+		super.onComponentTag(component, tag);
+		tagName = tag.getName();
+	}
+
+	@Override
+	public void onRendered(final Component component) {
+		// TODO Auto-generated method stub
+		super.onRendered(component);
+		HeaderResponse headerResponse=new HeaderResponse(){
+		
+			@Override
+			protected Response getRealResponse() {
+				// TODO Auto-generated method stub
+				return component.getResponse();
+			}
+		};
+		headerResponse.renderOnDomReadyJavascript(getNiftyJS(tagName));
+	}
 }
