928b5e1c410c0d44269c291743f71691e0e6336c
Tauren Mills <tauren@wicketstuff.org>
2009-06-26 07:56:48 +0000

    Renaming packages and references from ki to shiro
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@4758 ef7698a4-5110-0410-9fc6-c7eb3693863f

---
+++ shiro-security/wicket-shiro-security-examples/shiro-security-example-spring-jdbc/src/test/java/org/wicketstuff/shiro/example/spring/StartSpringExample.java
@@ -0,0 +1,11 @@
+package org.wicketstuff.shiro.example.spring;
+
+import org.wicketstuff.shiro.example.ExampleRunner;
+
+public class StartSpringExample
+{
+	public static void main(String[] args) throws Exception
+	{
+		ExampleRunner.run();
+	}
+}
