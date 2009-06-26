5780f18dbd7044bd60c803c43ae41badb8cf14ae
Tauren Mills <tauren@wicketstuff.org>
2009-06-26 03:21:28 +0000

    Initial commit -- basically just a maven package rename of wicket ki security to wicket shiro security. Class names and dependencies have not been changed yet.
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk/wicketstuff-core@4745 ef7698a4-5110-0410-9fc6-c7eb3693863f

---
+++ shiro-security/wicket-shiro-security-examples/shiro-security-example-base/src/main/java/org/wicketstuff/ki/example/pages/BasePage.java
@@ -0,0 +1,38 @@
+package org.wicketstuff.ki.example.pages;
+
+import org.apache.wicket.markup.html.WebMarkupContainer;
+import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.link.BookmarkablePageLink;
+import org.wicketstuff.ki.component.KiConfigInfoPanel;
+import org.wicketstuff.ki.component.SimpleAuthHeader;
+import org.wicketstuff.ki.example.ExampleApplication;
+
+/**
+ * Simple index page
+ */
+public abstract class BasePage extends WebPage
+{
+	/**
+	 * Constructor.
+	 */
+	public BasePage()
+	{
+		add(new Label("title", getTitle()));
+		add(new SimpleAuthHeader("headerAuth", LoginPage.class));
+
+		WebMarkupContainer links = new WebMarkupContainer("links");
+		links.add(new BookmarkablePageLink<Void>("home", IndexPage.class));
+		links.add(new BookmarkablePageLink<Void>("admin", RequireAdminRolePage.class));
+    links.add(new BookmarkablePageLink<Void>("auth", RequireAuthPage.class));
+    links.add(new BookmarkablePageLink<Void>("view", RequireViewPermissionPage.class));
+		add(links);
+
+		add( new Label("appname", getApplication().getClass().getName() ) );
+    add( ((ExampleApplication)getApplication()).getExampleInfoPanel("example") );
+		add(new KiConfigInfoPanel("info"));
+	}
+	
+
+	abstract String getTitle();
+}
---
+++ shiro-security/wicket-shiro-security-examples/shiro-security-example-base/src/main/java/org/wicketstuff/ki/example/pages/IndexPage.java
@@ -0,0 +1,22 @@
+package org.wicketstuff.ki.example.pages;
+
+
+/**
+ * Simple index page
+ */
+public class IndexPage extends BasePage
+{
+	/**
+	 * Constructor.
+	 */
+	public IndexPage()
+	{
+
+	}
+
+	@Override
+	public String getTitle()
+	{
+		return "Ki Security for Apache Wicket -- example";
+	}
+}
