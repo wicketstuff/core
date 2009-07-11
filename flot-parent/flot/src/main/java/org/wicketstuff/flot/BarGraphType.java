4c28370566eea759bec87e8455b8560b4e1190ad
Michael Würtinger <mwuertinger@wicketstuff.org>
2009-07-11 12:59:55 +0000

    Added the flot project which is a Wicket integration of http://code.google.com/p/flot/.
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk/wicketstuff-core@4798 ef7698a4-5110-0410-9fc6-c7eb3693863f

---
+++ flot-parent/flot/src/main/java/org/wicketstuff/flot/BarGraphType.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2009 Michael Würtinger (mwuertinger@users.sourceforge.net)
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * 		http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.wicketstuff.flot;
+
+
+/**
+ * 
+ * @author Michael Würtinger (mwuertinger@users.sourceforge.net)
+ *
+ */
+public class BarGraphType extends GraphType {
+	public enum Align {LEFT, CENTER};
+	
+	private Double barWidth;
+	private Align align;
+	
+	public BarGraphType(Double lineWidth, Boolean fill, Color fillColor, Double barWidth, Align align) {
+		super(lineWidth, fill, fillColor);
+		this.barWidth = barWidth;
+		this.align = align;
+	}
+	
+	@Override
+	public String toString() {
+		StringBuffer str = new StringBuffer();
+		
+		str.append("bars: {");
+		
+		String superStr = super.toString();
+		
+		if(barWidth != null) {
+			str.append("barWidth: ");
+			str.append(barWidth);
+			if(align != null || superStr.length()>0)
+				str.append(", ");
+		}
+
+		if(align != null) {
+			str.append("align: ");
+			switch(align) {
+			case LEFT:
+				str.append("\"left\"");
+				break;
+			case CENTER:
+				str.append("\"center\"");
+				break;
+			}
+			
+			if(superStr.length()>0)
+				str.append(", ");
+		}
+		
+		str.append(superStr);
+		
+		str.append("}");
+		
+		return str.toString();
+	}
+}
---
+++ flot-parent/flot/src/main/java/org/wicketstuff/flot/FlotPanel.java
@@ -0,0 +1,219 @@
+/*
+ * Copyright 2009 Michael Würtinger (mwuertinger@users.sourceforge.net)
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * 		http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.wicketstuff.flot;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.apache.log4j.Logger;
+import org.apache.wicket.markup.ComponentTag;
+import org.apache.wicket.markup.MarkupStream;
+import org.apache.wicket.markup.html.WebComponent;
+import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.markup.parser.XmlTag;
+import org.apache.wicket.model.IModel;
+
+public class FlotPanel extends Panel {
+	private static final long serialVersionUID = 1L;
+	private static final Logger logger = Logger.getLogger(FlotPanel.class);
+
+	private Map<String, Map<String, Object>> options = new HashMap<String, Map<String, Object>>();
+	
+	public FlotPanel(final String id, final IModel<List<Series>> model) {
+		super(id, model);
+		
+		String[] optionsKeys = {"lines", "points", "legend", "xaxis", "yaxis", "x2axis", "y2axis", "selection", "grid"};
+		
+		for(String key : optionsKeys)
+			options.put(key, new HashMap<String, Object>());
+		
+		options.get("selection").put("mode", "xy");
+		options.get("grid").put("hoverable", true);
+		options.get("grid").put("clickable", true);
+		options.get("yaxis").put("min", 0);
+		options.get("yaxis").put("max", 15);
+		
+		// This custom component fills the <script> tag with the script returned by getFlotScript().
+		add(new WebComponent("flotScript") {
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			protected void onComponentTagBody(final MarkupStream markupStream, final ComponentTag openTag)
+			{
+				replaceComponentTagBody(markupStream, openTag, getFlotScript());
+			}
+			@Override
+			protected void onComponentTag(ComponentTag tag)
+			{
+				super.onComponentTag(tag);
+				// always transform the tag to <span></span> so even labels defined as <span/> render
+				tag.setType(XmlTag.OPEN);
+			}
+		});
+	}
+	
+	/**
+	 * Returns Javascript code which renders the flot graph.
+	 * @throws RuntimeException if FlotPanel.js cannot be loaded.
+	 */
+	private String getFlotScript() {
+		try {
+			final String str = getResourceContents(FlotPanel.class, "FlotPanel.js");
+			
+			logger.info(options);
+			
+			//final String options = "{ lines: { show: true }, points: { show: true }, selection: { mode: \"xy\" }, grid: { hoverable: true, clickable: true }, xaxis: { mode: 'time', min: 1243807200000, max: 1246312800000, timeformat: '%d.%m.%y' }, yaxis: { min: 0, max: 15 }}";
+			
+			@SuppressWarnings("unchecked")
+			final List<Series> data = (List<Series>) getDefaultModelObject();
+			
+			final StringBuffer strData = new StringBuffer();
+			strData.append("[");
+			for(Series dataEntry : data) {
+				strData.append(dataEntry.toString());
+				strData.append(", ");
+			}
+			if(data.size()>0)
+				strData.setLength(strData.length()-2);
+			strData.append("]");
+			
+			String strOptions = mapToString(options);
+			
+			return String.format(str, strData, strOptions);
+		}
+		catch(IOException e) {
+			throw new RuntimeException("Unable to load ProgressView.js", e);
+		}
+	}
+	
+	private static String mapToString(final Map<?,?> map) {
+		final StringBuilder str = new StringBuilder();
+		
+		str.append("{");
+		
+		Set<?> keySet = map.keySet();
+		
+		for(Object key : keySet) {
+			str.append(key);
+			str.append(": ");
+			
+			Object value = map.get(key);
+			
+			if(Map.class.isAssignableFrom(value.getClass()))
+				str.append(mapToString((Map<?,?>) value));
+			else {
+				boolean valueIsString = value.getClass().equals(String.class);
+				
+				if(valueIsString)
+					str.append("\"");
+				str.append(value);
+				if(valueIsString)
+					str.append("\"");
+			}
+			
+			str.append(", ");
+		}
+		
+		if(keySet.size()>0)
+			str.setLength(str.length()-2);
+		
+		str.append("}");
+		
+		return str.toString();
+	}
+	
+	public void setAxisTimeModeX(boolean timeMode) {
+		String mode = timeMode ? "null" : "time";
+		options.get("xaxis").put("mode", mode);
+	}
+	
+	public void setAxisTimeformatX(String timeFormat) {
+		options.get("xaxis").put("timeformat", timeFormat);
+	}
+	
+	public void setAxisMinX(Double axisMinX) {
+		options.get("xaxis").put("min", axisMinX);
+	}
+	
+	public Double getAxisMinX() {
+		return (Double) options.get("xaxis").get("min");
+	}
+
+	public void setAxisMinY(Double axisMinY) {
+		options.get("yaxis").put("min", axisMinY);
+	}
+	
+	public Double getAxisMinY() {
+		return (Double) options.get("yaxis").get("min");
+	}
+
+	public void setAxisMaxX(Double axisMaxX) {
+		options.get("xaxis").put("max", axisMaxX);
+	}
+	
+	public Double getAxisMaxX() {
+		return (Double) options.get("xaxis").get("max");
+	}
+
+	public void setAxisMaxY(Double axisMaxY) {
+		options.get("yaxis").put("max", axisMaxY);
+	}
+	
+	public Double getAxisMaxY() {
+		return (Double) options.get("yaxis").get("max");
+	}
+
+	/**
+	 * Returns the contents of the given resource.
+	 * @param location The resource path.
+	 * @return The resource's content.
+	 * @throws FileNotFoundException If the resource cannot be found.
+	 * @throws IOException If anything goes wrong while reading the resource.
+	 */
+	public static String getResourceContents(Class<?> clazz, String location) throws IOException {
+		InputStream in = clazz.getResourceAsStream(location);
+		if(in == null)
+			throw new FileNotFoundException("'"+location+"' not found.");
+		return getStreamContents(new InputStreamReader(in));
+	}
+	
+	/**
+	 * Returns the complete content of the given stream.
+	 * @throws IOException If anything goes wrong while reading the stream.
+	 */
+	public static String getStreamContents(InputStreamReader inputStreamReader) throws IOException {
+		StringBuffer str = new StringBuffer();
+		BufferedReader in = new BufferedReader(inputStreamReader);
+		
+		while(true)  {
+			String line = in.readLine();
+			if(line==null)
+				break;
+			str.append(line);
+			str.append("\n");
+		}
+		
+		return str.toString();
+	}
+}
+
