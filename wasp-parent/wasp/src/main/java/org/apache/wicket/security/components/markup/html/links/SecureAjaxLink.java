4b8b6dec7533f90503479022c0a6060fe6e6ce8e
Olger Warnier <terranvs@wicketstuff.org>
2009-07-12 14:27:16 +0000

    License fix, fixes test.
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@4803 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- wasp-parent/wasp/src/main/java/org/apache/wicket/security/components/markup/html/links/SecureAjaxLink.java
+++ wasp-parent/wasp/src/main/java/org/apache/wicket/security/components/markup/html/links/SecureAjaxLink.java
@@ -1,13 +1,15 @@
-/**
- * Copyright 2008-2009 Stichting JoiningTracks, The Netherlands
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an "AS IS" BASIS,
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
@@ -42,7 +44,8 @@ abstract public class SecureAjaxLink extends AjaxLink implements ISecureComponen
 	private static final long serialVersionUID = 1L;
 
 	/**
-	 * @param id
+	 * @param id    wicket id as found in the HTML
+     * @param model the model
 	 */
 	public SecureAjaxLink(final String id, final IModel model)
 	{
@@ -51,7 +54,7 @@ abstract public class SecureAjaxLink extends AjaxLink implements ISecureComponen
 	}
 
 	/**
-	 * @param id
+	 * @param id wicket id as found in the HTML
 	 */
 	public SecureAjaxLink(final String id)
 	{
