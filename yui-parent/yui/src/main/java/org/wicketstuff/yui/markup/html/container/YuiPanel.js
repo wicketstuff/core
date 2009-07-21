97ae4fbf44daa34aa6752f990e23875fb03efc12
Erik van Oosten <evoosten@wicketstuff.org>
2009-07-21 12:42:00 +0000

    Added option to register panel to overlaymanager to keep it on top when activated.
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk/wicketstuff-core@4836 ef7698a4-5110-0410-9fc6-c7eb3693863f

---
+++ yui-parent/yui/src/main/java/org/wicketstuff/yui/markup/html/container/YuiPanel.js
@@ -0,0 +1,7 @@
+if(typeof(WicketStuff)=='undefined') WicketStuff={}
+if(typeof(WicketStuff.Yui)=='undefined') WicketStuff.Yui={}
+if(typeof(WicketStuff.Yui.overlaymanager)=='undefined') WicketStuff.Yui.overlaymanager=new YAHOO.widget.OverlayManager()
+
+WicketStuff.Yui.registerPanel = function(yuiPanelId, yuiPanelVar) {
+ if(!WicketStuff.Yui.overlaymanager.find(yuiPanelId)) WicketStuff.Yui.overlaymanager.register(yuiPanelVar)
+}
