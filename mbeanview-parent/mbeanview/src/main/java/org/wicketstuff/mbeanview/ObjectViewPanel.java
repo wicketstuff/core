ee53b526b3e694587b83a7bff6e8c8f8b40adee2
Pedro Santos <pedrosans@wicketstuff.org>
2009-09-09 18:57:27 +0000

    - add an title to object view - implement getViewMethod of MbeanNode to return his tree fragment by default
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk/wicketstuff-core@4948 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- mbeanview-parent/mbeanview/src/main/java/org/wicketstuff/mbeanview/ObjectViewPanel.java
+++ mbeanview-parent/mbeanview/src/main/java/org/wicketstuff/mbeanview/ObjectViewPanel.java
@@ -30,36 +30,38 @@ import org.apache.wicket.markup.html.panel.Panel;
  * 
  */
 public class ObjectViewPanel extends Panel {
-    public static class PropValue implements Serializable {
-	String property;
-	Object value;
-    }
-
-    public ObjectViewPanel(String id, Object object) {
-	super(id);
-	add(new Label("toString", object.toString()));
-	ArrayList properties = new ArrayList();
-	Method[] methods = object.getClass().getMethods();
-	for (int i = 0; i < methods.length; i++) {
-	    if (methods[i].getName().startsWith("get")
-		    && methods[i].getParameterTypes().length == 0) {
-		PropValue prop = new PropValue();
-		try {
-		    prop.property = methods[i].getName();
-		    prop.value = methods[i].invoke(object, (Object[]) null);
-		} catch (Exception e) {
-		    e.printStackTrace();
-		}
-		properties.add(prop);
-	    }
+	public static class PropValue implements Serializable {
+		String property;
+		Object value;
 	}
-	add(new ListView<PropValue>("properties", properties) {
-	    @Override
-	    protected void populateItem(ListItem<PropValue> item) {
-		item.add(new Label("property", item.getModelObject().property));
-		item.add(new Label("value", item.getModelObject().value == null ? null : item
-			.getModelObject().value.toString()));
-	    }
-	});
-    }
+
+	public ObjectViewPanel(String id, Object object) {
+		super(id);
+		add(new Label("className", object.getClass().getName()));
+		add(new Label("toString", object.toString()));
+		ArrayList properties = new ArrayList();
+		Method[] methods = object.getClass().getMethods();
+		for (int i = 0; i < methods.length; i++) {
+			if (methods[i].getName().startsWith("get")
+					&& methods[i].getParameterTypes().length == 0) {
+				PropValue prop = new PropValue();
+				try {
+					prop.property = methods[i].getName();
+					prop.value = methods[i].invoke(object, (Object[]) null);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				properties.add(prop);
+			}
+		}
+		add(new ListView<PropValue>("properties", properties) {
+			@Override
+			protected void populateItem(ListItem<PropValue> item) {
+				item.add(new Label("property", item.getModelObject().property));
+				item.add(new Label("value",
+						item.getModelObject().value == null ? null : item
+								.getModelObject().value.toString()));
+			}
+		});
+	}
 }
