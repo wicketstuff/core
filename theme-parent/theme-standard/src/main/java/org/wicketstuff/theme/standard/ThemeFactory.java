6b1c0a37d91c55096097db318f4fbcc381ea0979
Michael Mosmann <mosmann@wicketstuff.org>
2009-11-09 22:30:43 +0000

    theme tester works
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@5058 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- theme-parent/theme-standard/src/main/java/org/wicketstuff/theme/standard/ThemeFactory.java
+++ theme-parent/theme-standard/src/main/java/org/wicketstuff/theme/standard/ThemeFactory.java
@@ -1,15 +1,17 @@
 package org.wicketstuff.theme.standard;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.apache.wicket.behavior.HeaderContributor;
+import org.apache.wicket.markup.html.CSSPackageResource;
 import org.wicketstuff.theme.IThemeFactory;
 
 public class ThemeFactory implements IThemeFactory
 {
 	public List<HeaderContributor> getHeaderContributors()
 	{
-		return new ArrayList<HeaderContributor>();
+		return Arrays.asList(CSSPackageResource.getHeaderContribution(ThemeFactory.class, "standard.css"));
 	}
 }
--- theme-parent/theme-tester/src/main/java/org/wicketstuff/theme/tester/WicketThemeTestApp.java
+++ theme-parent/theme-tester/src/main/java/org/wicketstuff/theme/tester/WicketThemeTestApp.java
@@ -2,10 +2,14 @@ package org.wicketstuff.theme.tester;
 
 import org.apache.wicket.Page;
 import org.apache.wicket.protocol.http.WebApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.wicketstuff.maven.MavenDevResourceStreamLocator;
 
 public class WicketThemeTestApp extends WebApplication
 {
+	private static final Logger _logger = LoggerFactory.getLogger(WicketThemeTestApp.class);
+	
 	@Override
 	protected void init()
 	{
@@ -13,7 +17,12 @@ public class WicketThemeTestApp extends WebApplication
 		
 		if (DEVELOPMENT.equals(getConfigurationType()))
 		{
+			_logger.error("Debug Mode");
 			getResourceSettings().setResourceStreamLocator(new MavenDevResourceStreamLocator());
+		}
+		else
+		{
+			_logger.error("NO Debug Mode");
 		}
 	}
 	
