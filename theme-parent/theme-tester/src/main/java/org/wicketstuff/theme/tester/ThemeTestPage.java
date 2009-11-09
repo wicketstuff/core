428bd5a0babb036e28c37147f8858308c74f19d1
Michael Mosmann <mosmann@wicketstuff.org>
2009-11-09 22:30:43 +0000

    theme tester works
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk/wicketstuff-core@5058 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- theme-parent/theme-tester/src/main/java/org/wicketstuff/theme/tester/ThemeTestPage.java
+++ theme-parent/theme-tester/src/main/java/org/wicketstuff/theme/tester/ThemeTestPage.java
@@ -1,18 +1,33 @@
 package org.wicketstuff.theme.tester;
 
+import org.apache.wicket.markup.html.panel.FeedbackPanel;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
-import org.wicketstuff.theme.*;
+import org.wicketstuff.theme.IThemeFactory;
+import org.wicketstuff.theme.WicketThemeBasePage;
 import org.wicketstuff.theme.standard.ThemeFactory;
 
 public class ThemeTestPage extends WicketThemeBasePage
 {
+	public ThemeTestPage()
+	{
+		add(new FeedbackPanel("feedback"));
+		
+		for (int i=0;i<2;i++)
+		{
+			info("Info "+i);
+			warn("Warn "+i);
+			error("Error "+i);
+		}
+		
+	}
+	
 	@Override
 	protected IModel<String> getTitleModel()
 	{
 		return Model.of("Theme Test");
 	}
-
+	
 	@Override
 	protected IThemeFactory getThemeFactory()
 	{
