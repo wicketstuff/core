e91e6a5f9748a4b9f70ca4a4cd5490367d727a28
Michael Mosmann <mosmann@wicketstuff.org>
2009-11-10 06:52:28 +0000

    theme and maven support works
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@5060 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- maven-support/src/main/java/org/wicketstuff/maven/MavenDevResourceStreamLocator.java
+++ maven-support/src/main/java/org/wicketstuff/maven/MavenDevResourceStreamLocator.java
@@ -1,3 +1,19 @@
+/*
+ * 
+ * ==============================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
 package org.wicketstuff.maven;
 
 import org.apache.wicket.util.file.File;
@@ -7,6 +23,30 @@ import org.apache.wicket.util.resource.locator.ResourceStreamLocator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * Get resources from maven project directory.
+ * 
+ * Usage:
+ *
+ * 	<code>
+ * 	class MyApp extends WebApplication
+ * 	{
+ * 		@Override
+ * 		protected void init()
+ * 		{
+ * 			...
+ * 			if (DEVELOPMENT.equals(getConfigurationType()))
+ * 			{
+ * 				getResourceSettings().setResourceStreamLocator(new MavenDevResourceStreamLocator());
+ * 			}
+ * 			...
+ * 		}
+ * 	}
+ * </code>
+ * 
+ * @author mosmann
+ *
+ */
 public class MavenDevResourceStreamLocator extends ResourceStreamLocator
 {
 	private static final Logger _logger = LoggerFactory.getLogger(MavenDevResourceStreamLocator.class);
