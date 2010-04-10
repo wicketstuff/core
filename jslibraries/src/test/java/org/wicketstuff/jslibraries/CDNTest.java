46db1ee610682643e013fa413b8cce4100d38321
Uwe Schäfer <uweschaefer@wicketstuff.org>
2010-04-10 13:00:23 +0000

    fixing test failure due to google changed content-types in their Http-Response-Header
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk/wicketstuff-core@5203 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- jslibraries/src/test/java/org/wicketstuff/jslibraries/CDNTest.java
+++ jslibraries/src/test/java/org/wicketstuff/jslibraries/CDNTest.java
@@ -1,20 +1,15 @@
 /**
- * Copyright (C) 2008 Jeremy Thomerson <jeremy@thomersonfamily.com>
- *
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright (C) 2008 Jeremy Thomerson <jeremy@thomersonfamily.com> Licensed to
+ * the Apache Software Foundation (ASF) under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership. The ASF licenses this file to You
+ * under the Apache License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain a copy of the
+ * License at http://www.apache.org/licenses/LICENSE-2.0 Unless required by
+ * applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+ * OF ANY KIND, either express or implied. See the License for the specific
+ * language governing permissions and limitations under the License.
  */
 package org.wicketstuff.jslibraries;
 
@@ -23,48 +18,55 @@ import java.net.URL;
 import java.net.URLConnection;
 import java.util.Set;
 
-import org.wicketstuff.jslibraries.CDN.GoogleRenderer;
-
 import junit.framework.TestCase;
 
+public class CDNTest extends TestCase
+{
 
-public class CDNTest extends TestCase {
+    @Override
+    protected void setUp() throws Exception
+    {
+        super.setUp();
+    }
 
-	protected void setUp() throws Exception {
-		super.setUp();
-	}
+    @Override
+    protected void tearDown() throws Exception
+    {
+        super.tearDown();
+    }
 
-	protected void tearDown() throws Exception {
-		super.tearDown();
-	}
+    public void testCDNs() throws Exception
+    {
 
-	public void testCDNs() throws Exception {
+        IterateAllRegistered(false);
+        IterateAllRegistered(true);
 
-		IterateAllRegistered(false);
-		IterateAllRegistered(true);
+    }
 
-	}
+    private void IterateAllRegistered(final boolean production) throws IOException
+    {
 
-	private void IterateAllRegistered(boolean production) throws IOException {
+        final CDN[] cdns = CDN.values();
+        for (int j = 0; j < cdns.length; j++)
+        {
+            final CDN cdn = cdns[j];
 
-		CDN[] cdns = CDN.values();
-		for (int j = 0; j < cdns.length; j++) {
-			CDN cdn = cdns[j];
-
-			Library[] libs = Library.values();
-			for (int i = 0; i < libs.length; i++) {
-				Library library = libs[i];
-				Set<Version> versions = library.getVersions(cdn);
-				for (Version v : versions) {
-					URL render = cdn.render(library, v, production);
-					// test if something comes back
-					URLConnection c = render.openConnection();
-					String type = c.getContentType();
-					System.out.println("checking URL: " + render.toExternalForm());
-					assertTrue(type.startsWith("application/x-javascript"));
-				}
-			}
-		}
-	}
+            final Library[] libs = Library.values();
+            for (int i = 0; i < libs.length; i++)
+            {
+                final Library library = libs[i];
+                final Set<Version> versions = library.getVersions(cdn);
+                for (final Version v : versions)
+                {
+                    final URL render = cdn.render(library, v, production);
+                    // test if something comes back
+                    final URLConnection c = render.openConnection();
+                    final String type = c.getContentType();
+                    System.out.println("checking URL: " + render.toExternalForm());
+                    assertTrue(type.contains("javascript"));
+                }
+            }
+        }
+    }
 
 }
