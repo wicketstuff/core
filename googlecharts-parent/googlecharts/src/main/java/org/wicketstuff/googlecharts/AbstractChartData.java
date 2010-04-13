729e82a7fdfc2c56bf46806707ce0d183bb1b924
Peter Major <aldaris88@wicketstuff.org>
2010-04-13 21:06:06 +0000

    Google Charts Initial Commit
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@5216 ef7698a4-5110-0410-9fc6-c7eb3693863f

---
+++ googlecharts-parent/googlecharts/src/main/java/org/wicketstuff/googlecharts/AbstractChartData.java
@@ -0,0 +1,42 @@
+/*
+ * Created on Dec 11, 2007
+ */
+package org.wicketstuff.googlecharts;
+
+/**
+ * @author Daniel Spiewak
+ */
+public abstract class AbstractChartData implements IChartData {
+
+    private ChartDataEncoding encoding;
+    private double max;
+
+    public AbstractChartData() {
+        this(62);
+    }
+
+    public AbstractChartData(double max) {
+        this(ChartDataEncoding.SIMPLE, max);
+    }
+
+    public AbstractChartData(ChartDataEncoding encoding, double max) {
+        this.encoding = encoding;
+        this.max = max;
+    }
+
+    public ChartDataEncoding getEncoding() {
+        return encoding;
+    }
+
+    public double getMax() {
+        return max;
+    }
+
+    public void setEncoding(ChartDataEncoding encoding) {
+        this.encoding = encoding;
+    }
+
+    public void setMax(double max) {
+        this.max = max;
+    }
+}
---
+++ googlecharts-parent/googlecharts/src/main/java/org/wicketstuff/googlecharts/IChartFill.java
@@ -0,0 +1,12 @@
+/*
+ * Created on Dec 11, 2007
+ */
+package org.wicketstuff.googlecharts;
+
+import java.io.Serializable;
+
+/**
+ * @author Daniel Spiewak
+ */
+public interface IChartFill extends Serializable {
+}
