ee473dc28c1ca6c587f566a09899f0d5a1635c56
Tauren Mills <tauren@wicketstuff.org>
2010-05-13 10:08:33 +0000

    Adding shiro native session example
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@5249 ef7698a4-5110-0410-9fc6-c7eb3693863f

---
+++ shiro-security/wicket-shiro-examples/shiro-example-spring-hibernate-native/src/test/java/org/wicketstuff/shiro/example/sprhibnative/StartHibernateNativeExample.java
@@ -0,0 +1,11 @@
+package org.wicketstuff.shiro.example.sprhibnative;
+
+import org.wicketstuff.shiro.example.ExampleRunner;
+
+public class StartHibernateNativeExample
+{
+	public static void main(String[] args) throws Exception
+	{
+		ExampleRunner.run();
+	}
+}
