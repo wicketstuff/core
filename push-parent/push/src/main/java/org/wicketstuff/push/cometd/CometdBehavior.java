2151eb29281ae3ed222899227ec547c13d412d09
Rodolfo Hansen <kryptt@wicketstuff.org>
2010-06-28 20:42:35 +0000

    Code formatting. No functional changes
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk/wicketstuff-core@5426 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- push-parent/push/src/main/java/org/wicketstuff/push/cometd/CometdBehavior.java
+++ push-parent/push/src/main/java/org/wicketstuff/push/cometd/CometdBehavior.java
@@ -10,44 +10,44 @@ import org.wicketstuff.push.IChannelListener;
 import org.wicketstuff.push.dojo.DojoPackagedTextTemplate;
 
 public class CometdBehavior extends CometdAbstractBehavior {
-  private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-  private final IChannelListener listener;
+	private final IChannelListener listener;
 
-  public CometdBehavior(final String channelId,
-      final IChannelListener listener) {
-    super(channelId);
-    this.listener = listener;
-  }
+	public CometdBehavior(final String channelId, final IChannelListener listener) {
+		super(channelId);
+		this.listener = listener;
+	}
 
-  @Override
-  public final String getCometdInterceptorScript() {
-    final Map<String, Object> map = new HashMap<String, Object>();
-    map.put("behaviorMarkupId", getBehaviorMarkupId());
-    map.put("url", getCallbackUrl().toString());
-    return new DojoPackagedTextTemplate(CometdBehavior.class,
-        "CometdDefaultBehaviorTemplate.js").asString(map);
-  }
+	@Override
+	public final String getCometdInterceptorScript() {
+		
+		final Map<String, Object> map = new HashMap<String, Object>();
+		map.put("behaviorMarkupId", getBehaviorMarkupId());
+		map.put("url", getCallbackUrl().toString());
+		
+		return new DojoPackagedTextTemplate(CometdBehavior.class, "CometdDefaultBehaviorTemplate.js").asString(map);
+	}
 
-  @Override
-  public final CharSequence getPartialSubscriber() {
-    return "'onEventFor"+ getBehaviorMarkupId() + "'";
-  }
+	@Override
+	public final CharSequence getPartialSubscriber() {
+		return "'onEventFor" + getBehaviorMarkupId() + "'";
+	}
 
-  @Override
-  protected final void respond(final AjaxRequestTarget target) {
+	@Override
+	protected final void respond(final AjaxRequestTarget target) {
 
-    final Map<String, String[]> map = ((WebRequestCycle) RequestCycle.get())
-        .getRequest().getParameterMap();
-    final Map<String, String> eventAttribute = new HashMap<String, String>();
+		final Map<String, String[]> map = ((WebRequestCycle) RequestCycle.get())
+				.getRequest().getParameterMap();
+		final Map<String, String> eventAttribute = new HashMap<String, String>();
 
-    for (final Map.Entry<String, String[]> entry : map.entrySet()) {
-      eventAttribute.put(entry.getKey(), entry.getValue()[0]);
-    }
+		for (final Map.Entry<String, String[]> entry : map.entrySet()) {
+			eventAttribute.put(entry.getKey(), entry.getValue()[0]);
+		}
 
-    final CometdTarget cTarget = new CometdTarget(target);
-    listener.onEvent(getChannelId(), eventAttribute, cTarget);
+		final CometdTarget cTarget = new CometdTarget(target);
+		listener.onEvent(getChannelId(), eventAttribute, cTarget);
 
-  }
+	}
 
 }
