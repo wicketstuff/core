5b416550cac3969c5080607ac503bf2ce0d69d83
Rodolfo Hansen <kryptt@wicketstuff.org>
2010-06-28 19:08:01 +0000

    Moving wicketstuff-push to the wicketstuff-core system
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@5362 ef7698a4-5110-0410-9fc6-c7eb3693863f

---
+++ push-parent/push/src/main/java/org/wicketstuff/push/timer/TimerChannelPublisher.java
@@ -0,0 +1,38 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.wicketstuff.push.timer;
+
+import java.io.Serializable;
+
+import org.wicketstuff.push.IChannelPublisher;
+import org.wicketstuff.push.ChannelEvent;
+
+/**
+ * Publisher that could be associated with a {@link TimerChannelBehavior}
+ * for more documentation see {@link TimerChannelBehavior}
+ * 
+ * @author Vincent Demay
+ */
+public class TimerChannelPublisher implements IChannelPublisher, Serializable
+{
+	private static final long serialVersionUID = 1L;
+
+	public void publish(ChannelEvent event)
+	{
+		EventStore.get().add(event);
+	}
+}
