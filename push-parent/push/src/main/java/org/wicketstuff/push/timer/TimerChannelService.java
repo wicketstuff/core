c5727348b0a125e34c657c2f4eae59e2985dd008
Rodolfo Hansen <kryptt@wicketstuff.org>
2010-06-28 20:41:49 +0000

    Some slight modifications.
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@5415 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- push-parent/push/src/main/java/org/wicketstuff/push/timer/TimerChannelService.java
+++ push-parent/push/src/main/java/org/wicketstuff/push/timer/TimerChannelService.java
@@ -27,7 +27,6 @@ public class TimerChannelService implements IChannelService, Serializable {
 		component.add(timerChannelBehavior);
 		EventStore.get().addEventStoreListener(new EventStoreListener(){
 
-      @Override
       public void EventTriggered(final String eventChannel, final Map<String, String> data)
 			{
 				if (listenerChannel.equals(eventChannel)){
