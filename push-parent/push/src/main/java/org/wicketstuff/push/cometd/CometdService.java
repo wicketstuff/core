69c343a8290579b2747b13405664005c54628a32
Rodolfo Hansen <kryptt@wicketstuff.org>
2010-06-28 20:42:26 +0000

    Format the code. No functional changes
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@5424 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- push-parent/push/src/main/java/org/wicketstuff/push/cometd/CometdService.java
+++ push-parent/push/src/main/java/org/wicketstuff/push/cometd/CometdService.java
@@ -34,158 +34,160 @@ import org.wicketstuff.push.IChannelService;
  * This mean that each time an event is published, a new connection is made to
  * the server to get the actual page update required by the
  * {@link IChannelListener}.
- *
+ * 
  * @author Xavier Hanin
  * @author Rodolfo Hansen
  * @see IChannelService
  */
 public class CometdService implements IChannelService {
 
-  private final class RemovalForwardingListener implements MessageListener {
+	private final class RemovalForwardingListener implements MessageListener {
 
-	public boolean onMessage(final ServerSession fromClient, final ServerSession toClient, final ServerMessage msg) {
-      final String channel = (String) msg.get("subscription");
-      if (removalListeners.containsKey(channel)) {
-        fromClient.addListener(removalListeners.get(channel));
-      }
-      return true;
-    }
-  }
+		public boolean onMessage(final ServerSession fromClient,
+				final ServerSession toClient, final ServerMessage msg) {
+			final String channel = (String) msg.get("subscription");
+			if (removalListeners.containsKey(channel)) {
+				fromClient.addListener(removalListeners.get(channel));
+			}
+			return true;
+		}
+	}
 
-  private final class RemovalListener implements RemoveListener {
+	private final class RemovalListener implements RemoveListener {
 
-    private final RemoveListener removeListener;
-    private final Session sess;
+		private final RemoveListener removeListener;
+		private final Session sess;
 
-    public RemovalListener(final RemoveListener listener, final Session sess) {
-      removeListener = listener;
-      this.sess = sess;
-    }
+		public RemovalListener(final RemoveListener listener, final Session sess) {
+			removeListener = listener;
+			this.sess = sess;
+		}
 
-    public void removed(final ServerSession session, final boolean timeout) {
-      final boolean hasNoApp = !Application.exists();
-      final boolean hasNoSession = !Session.exists();
-      if (hasNoApp) {
-        Application.set(getApplication());
-      }
-      if (hasNoSession && sess != null) {
-        Session.set(sess);
-      }
+		public void removed(final ServerSession session, final boolean timeout) {
+			final boolean hasNoApp = !Application.exists();
+			final boolean hasNoSession = !Session.exists();
+			if (hasNoApp) {
+				Application.set(getApplication());
+			}
+			if (hasNoSession && sess != null) {
+				Session.set(sess);
+			}
 
-      removeListener.removed(session, timeout);
+			removeListener.removed(session, timeout);
 
-      if (hasNoApp) {
-        Application.unset();
-      }
-      if (hasNoSession) {
-        Session.unset();
-      }
-    }
-  }
+			if (hasNoApp) {
+				Application.unset();
+			}
+			if (hasNoSession) {
+				Session.unset();
+			}
+		}
+	}
 
-  public static final String BAYEUX_CLIENT_PREFIX = "wicket-push";
+	public static final String BAYEUX_CLIENT_PREFIX = "wicket-push";
 
-  private final Map<String, RemoveListener> removalListeners;
+	private final Map<String, RemoveListener> removalListeners;
 
-  final WebApplication _application;
-  private BayeuxServer       _bayeux;
-  private ServerSession       serviceClient;
-  private boolean      listeningToConnect;
+	final WebApplication _application;
+	private BayeuxServer _bayeux;
+	private ServerSession serviceClient;
+	private boolean listeningToConnect;
 
-  public CometdService(final WebApplication application) {
-    _application = application;
-    removalListeners = Collections
-        .synchronizedMap(new WeakHashMap<String, RemoveListener>());
-    listeningToConnect = false;
-  }
+	public CometdService(final WebApplication application) {
+		_application = application;
+		removalListeners = Collections
+				.synchronizedMap(new WeakHashMap<String, RemoveListener>());
+		listeningToConnect = false;
+	}
 
-  public void addChannelListener(final Component component,
-      final String channel, final IChannelListener listener) {
-    component.add(new CometdBehavior(channel, listener));
-  }
+	public void addChannelListener(final Component component,
+			final String channel, final IChannelListener listener) {
+		component.add(new CometdBehavior(channel, listener));
+	}
 
-  /**
-   * Cometd Specific method to Listen for client removals
-   *
-   * @param channel
-   * @param listener
-   * @param sess Wicket Session you wish to associate with the listener.
-   */
-  public void addChannelRemoveListener(final String chnl,
-      final RemoveListener listener, final Session sess) {
-    if (!listeningToConnect) {
-//      getBayeux().getChannel(Channel.META_SUBSCRIBE).subscribe(serviceClient);
-    	Object serverChannel = getBayeux().getChannel(Channel.META_SUBSCRIBE);
-//    	serverChannel.subscribe(serviceClient);
-      serviceClient.addListener(new RemovalForwardingListener());
-      listeningToConnect = true;
-    }
-    removalListeners.put("/" + chnl, new RemovalListener(listener, sess));
-  }
+	/**
+	 * Cometd Specific method to Listen for client removals
+	 * 
+	 * @param channel
+	 * @param listener
+	 * @param sess
+	 *            Wicket Session you wish to associate with the listener.
+	 */
+	public void addChannelRemoveListener(final String chnl,
+			final RemoveListener listener, final Session sess) {
+		if (!listeningToConnect) {
+			// getBayeux().getChannel(Channel.META_SUBSCRIBE).subscribe(serviceClient);
+			Object serverChannel = getBayeux().getChannel(
+					Channel.META_SUBSCRIBE);
+			// serverChannel.subscribe(serviceClient);
+			serviceClient.addListener(new RemovalForwardingListener());
+			listeningToConnect = true;
+		}
+		removalListeners.put("/" + chnl, new RemovalListener(listener, sess));
+	}
 
+	/**
+	 * @see #addChannelRemoveListener(String, RemoveListener, Session)
+	 */
+	public void addChannelRemoveListener(final String chnl,
+			final RemoveListener listener) {
+		addChannelRemoveListener(chnl, listener, null);
+	}
 
-  /**
-   * @see #addChannelRemoveListener(String, RemoveListener, Session)
-   */
-  public void addChannelRemoveListener(final String chnl,
-      final RemoveListener listener) {
-    addChannelRemoveListener(chnl, listener, null);
-  }
+	/**
+	 * Implementation of {@link IChannelService#publish(ChannelEvent)}, which
+	 * actually sends a cometd event to the client with a "proxy" attribute set
+	 * to "true", which in turn triggers a wicket ajax call to get the listener
+	 * notified and update the page.
+	 * 
+	 * @event the event to publish, which will be modify with "proxy" set to
+	 *        "true"
+	 */
+	public void publish(final ChannelEvent event) {
+		/*
+		 * to avoid using implementation specific events, we set the proxy data
+		 * here. this property is used by CometdDefaultBehaviorTemplate.js to
+		 * know that the event should actually be converted in a wicket ajax
+		 * call to get the actual page refresh
+		 */
+		event.addData("proxy", "true");
+		final String channelId = "/" + event.getChannel();
+		Object channel = getBayeux().getChannel(channelId);
+		System.err.println(channel);
+		// final ClientSessionChannel channel = (ClientSessionChannel)
+		// getBayeux().getSession("").getLocalSession().g Channel(channelId);
+		// channel.publish(event.getData(), event.getId());
+	}
 
-  /**
-   * Implementation of {@link IChannelService#publish(ChannelEvent)}, which
-   * actually sends a cometd event to the client with a "proxy" attribute set to
-   * "true", which in turn triggers a wicket ajax call to get the listener
-   * notified and update the page.
-   *
-   * @event the event to publish, which will be modify with "proxy" set to
-   *        "true"
-   */
-  public void publish(final ChannelEvent event) {
-    /*
-     * to avoid using implementation specific events, we set the proxy data
-     * here. this property is used by CometdDefaultBehaviorTemplate.js to know
-     * that the event should actually be converted in a wicket ajax call to get
-     * the actual page refresh
-     */
-    event.addData("proxy", "true");
-    final String channelId = "/" + event.getChannel();
-    Object channel = getBayeux().getChannel(channelId);
-    System.err.println(channel);
-//    final ClientSessionChannel channel = (ClientSessionChannel) getBayeux().getSession("").getLocalSession().g Channel(channelId);
-//    channel.publish(event.getData(), event.getId());
-  }
+	private final BayeuxServer getBayeux() {
+		if (_bayeux == null) {
+			initBayeux();
+		}
 
-  private final BayeuxServer getBayeux() {
-    if (_bayeux == null) {
-      initBayeux();
-    }
+		return _bayeux;
+	}
 
-    return _bayeux;
-  }
+	/**
+	 * Initializes the Jetty CometD Bayeux Service to be used.
+	 */
+	private void initBayeux() {
+		final String cfgType = _application.getConfigurationType();
+		_bayeux = (BayeuxServer) _application.getServletContext().getAttribute(
+				BayeuxServer.ATTRIBUTE);
 
-  /**
-   * Initializes the Jetty CometD Bayeux Service to be used.
-   */
-  private void initBayeux() {
-    final String cfgType = _application.getConfigurationType();
-    _bayeux = (BayeuxServer) _application.getServletContext().getAttribute(
-        BayeuxServer.ATTRIBUTE);
+		/*
+		 * if (_bayeux instanceof AbstractBayeux &&
+		 * Application.DEVELOPMENT.equalsIgnoreCase(cfgType)) {
+		 * ((AbstractBayeux) _bayeux).setLogLevel(2); }
+		 * 
+		 * if (_bayeux instanceof ContinuationBayeux) { ((ContinuationBayeux)
+		 * _bayeux).setJSONCommented(true); }
+		 */
+		serviceClient = _bayeux.getSession(BAYEUX_CLIENT_PREFIX);
+	}
 
-/*    if (_bayeux instanceof AbstractBayeux
-        && Application.DEVELOPMENT.equalsIgnoreCase(cfgType)) {
-      ((AbstractBayeux) _bayeux).setLogLevel(2);
-    }
-
-    if (_bayeux instanceof ContinuationBayeux) {
-      ((ContinuationBayeux) _bayeux).setJSONCommented(true);
-    }
-*/
-    serviceClient = _bayeux.getSession(BAYEUX_CLIENT_PREFIX);
-  }
-
-  public Application getApplication() {
-    return _application;
-  }
+	public Application getApplication() {
+		return _application;
+	}
 
 }
