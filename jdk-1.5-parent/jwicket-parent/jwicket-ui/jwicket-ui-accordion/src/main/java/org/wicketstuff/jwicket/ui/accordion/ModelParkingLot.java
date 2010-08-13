920cd66351d72d846628cc6a0acbc998515b182e
Michael O'Cleirigh <mocleiri@wicketstuff.org>
2010-08-13 03:24:18 +0000

    wicketstuff-core changes for the 1.4.10 release
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/branches/wicketstuff-core-1.4.10@5471 ef7698a4-5110-0410-9fc6-c7eb3693863f

---
+++ jdk-1.5-parent/jwicket-parent/jwicket-ui/jwicket-ui-accordion/src/main/java/org/wicketstuff/jwicket/ui/accordion/ModelParkingLot.java
@@ -0,0 +1,25 @@
+package org.wicketstuff.jwicket.ui.accordion;
+
+
+import java.io.Serializable;
+import java.util.List;
+
+import org.apache.wicket.Component;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.markup.html.WebMarkupContainer;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.model.IModel;
+
+public class ModelParkingLot<T> extends WebMarkupContainer {
+		private static final long serialVersionUID = 1L;
+
+		public ModelParkingLot(final String id, final IModel<T> model) {
+			super(id, model);
+		}
+
+		@SuppressWarnings("unchecked")
+		public IModel<T> getModel() {
+			return (IModel<T>)getDefaultModel();
+		}
+	}
+
