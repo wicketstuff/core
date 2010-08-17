21929982d82bd23c76b284fff70f63715255c02e
Michael O'Cleirigh <mocleiri@wicketstuff.org>
2010-08-17 15:50:35 +0000

    Forgot the import statements for the new ModelParkingLot class in jwicket
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/branches/wicketstuff-core-1.4@5480 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- jdk-1.5-parent/jwicket-parent/jwicket-ui/jwicket-ui-accordion/src/main/java/org/wicketstuff/jwicket/ui/accordion/ModelParkingLot.java
+++ jdk-1.5-parent/jwicket-parent/jwicket-ui/jwicket-ui-accordion/src/main/java/org/wicketstuff/jwicket/ui/accordion/ModelParkingLot.java
@@ -1,4 +1,12 @@
 package org.wicketstuff.jwicket.ui.accordion;
+import java.io.Serializable;
+import java.util.List;
+
+import org.apache.wicket.Component;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.markup.html.WebMarkupContainer;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.model.IModel;
 
 public class ModelParkingLot extends WebMarkupContainer {
 		private static final long serialVersionUID = 1L;
