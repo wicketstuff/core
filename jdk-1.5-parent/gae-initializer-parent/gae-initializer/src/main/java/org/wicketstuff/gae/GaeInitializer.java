f037436cbfb5a8658a33115fe1dc0ae941c8e4e8
Martin Grigorov <martingrigorov@wicketstuff.org>
2010-12-08 14:56:19 +0000

    Update GAE initializer with the newest resource setting - file upload cleaner (WICKET-3122) While here add some javadoc
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@5680 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- jdk-1.5-parent/gae-initializer-parent/gae-initializer/src/main/java/org/wicketstuff/gae/GaeInitializer.java
+++ jdk-1.5-parent/gae-initializer-parent/gae-initializer/src/main/java/org/wicketstuff/gae/GaeInitializer.java
@@ -6,14 +6,21 @@ import org.apache.wicket.pageStore.memory.DataStoreEvictionStrategy;
 import org.apache.wicket.pageStore.memory.PageNumberEvictionStrategy;
 import org.apache.wicket.util.lang.WicketObjects;
 
+/**
+ * A Wicket initializer that configures the application
+ * that way so it is possible to run it in Google AppEngine
+ */
 public class GaeInitializer implements IInitializer {
 
 	public void init(Application application) {
 		
+		// disable ModificationWatcher
 		application.getResourceSettings().setResourcePollFrequency(null);
 		
+		// use plain JDK Object(Input|Output)Stream
 		WicketObjects.setObjectStreamFactory(new GaeObjectStreamFactory());
 		
+		// save older version of pages in the HttpSession
 		final DataStoreEvictionStrategy evictionStrategy;
 		if (application instanceof GaeApplication) {
 			evictionStrategy = ((GaeApplication)application).getEvictionStrategy();
@@ -22,6 +29,9 @@ public class GaeInitializer implements IInitializer {
 		}
 		
 		application.setPageManagerProvider(new GaePageManagerProvider(application, evictionStrategy));
+		
+		// disable file cleaning because it starts a new thread
+		application.getResourceSettings().setFileUploadCleaner(null);
 	}
 
 }
