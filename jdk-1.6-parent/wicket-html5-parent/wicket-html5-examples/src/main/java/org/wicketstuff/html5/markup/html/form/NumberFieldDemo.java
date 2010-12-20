589a14b855a607c2fda0e8db00bc997e98d743b9
Martin Grigorov <martingrigorov@wicketstuff.org>
2010-12-20 16:51:30 +0000

    Use the new HTML5 input types from Wicket core
    git-svn-id: file:///home/igor/dev/stuff/svnbackup/trunk@5687 ef7698a4-5110-0410-9fc6-c7eb3693863f

--- jdk-1.6-parent/wicket-html5-parent/wicket-html5-examples/src/main/java/org/wicketstuff/html5/markup/html/form/NumberFieldDemo.java
+++ jdk-1.6-parent/wicket-html5-parent/wicket-html5-examples/src/main/java/org/wicketstuff/html5/markup/html/form/NumberFieldDemo.java
@@ -2,6 +2,7 @@ package org.wicketstuff.html5.markup.html.form;
 
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.NumberTextField;
 import org.apache.wicket.markup.html.panel.FeedbackPanel;
 import org.apache.wicket.model.PropertyModel;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
@@ -9,6 +10,7 @@ import org.wicketstuff.html5.BasePage;
 
 public class NumberFieldDemo extends BasePage {
 
+	@SuppressWarnings("unused")
 	private Double d;
 	
 	public NumberFieldDemo(PageParameters parameters) {
@@ -20,7 +22,10 @@ public class NumberFieldDemo extends BasePage {
 		Form<Void> form = new Form<Void>("form");
 		add(form);
 		
-		form.add(new NumberField("number", new PropertyModel<Double>(this, "d")));
+		NumberTextField numberField = new NumberTextField("number", new PropertyModel<Double>(this, "d"));
+		numberField.setRequired(false);
+		form.add(numberField);
+		numberField.setMinimum(4.0d);
 		
 		add(new Label("numberLabel", new PropertyModel<Double>(this, "d")));
 	}
--- jdk-1.6-parent/wicket-html5-parent/wicket-html5-examples/src/main/java/org/wicketstuff/html5/markup/html/form/RangeTextFieldDemo.java
+++ jdk-1.6-parent/wicket-html5-parent/wicket-html5-examples/src/main/java/org/wicketstuff/html5/markup/html/form/RangeTextFieldDemo.java
@@ -2,29 +2,24 @@ package org.wicketstuff.html5.markup.html.form;
 
 import java.io.Serializable;
 
-import org.apache.wicket.Component;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.form.AjaxFormComponentUpdatingBehavior;
-import org.apache.wicket.markup.html.IHeaderResponse;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.RangeTextField;
 import org.apache.wicket.model.CompoundPropertyModel;
 import org.apache.wicket.model.Model;
-import org.apache.wicket.request.resource.CompressedResourceReference;
 import org.apache.wicket.util.time.Duration;
 import org.wicketstuff.html5.BasePage;
 
 public class RangeTextFieldDemo extends BasePage {
-
-	private static final CompressedResourceReference WICKET_AJAX_HTML5_JS = 
-			new CompressedResourceReference(RangeTextFieldDemo.class, "wicket-ajax-html5.js");
 
 	private final Pojo pojo;
 	
 	public RangeTextFieldDemo() {
 		
 		this.pojo = new Pojo();
-		pojo.range = 2.0d;
+		pojo.range = 2.3d;
 		
 		Form<Pojo> form = new Form<Pojo>("form", new CompoundPropertyModel<Pojo>(pojo));
 		add(form);
@@ -32,9 +27,8 @@ public class RangeTextFieldDemo extends BasePage {
 		RangeTextField rangeTextField = new RangeTextField("range");
 		form.add(rangeTextField);
 		
-		rangeTextField.setMinimum(0.0d);
+		rangeTextField.setMinimum(1.4d);
 		rangeTextField.setMaximum(10.0d);
-		rangeTextField.setStep(0.5d);
 		
 		final Label rangeLabel = new Label("rangeLabel", new Model<String>(pojo.range.toString()));
 		rangeLabel.setOutputMarkupId(true);
@@ -47,12 +41,6 @@ public class RangeTextFieldDemo extends BasePage {
 			protected void onUpdate(AjaxRequestTarget target) {
 				rangeLabel.setDefaultModelObject(pojo.range.toString());
 				target.add(rangeLabel);
-			}
-			
-			public void renderHead(Component c, IHeaderResponse response) {
-				super.renderHead(c, response);
-				// override Wicket.Form.serializeInput so that input[type=range] is serialized too
-				response.renderJavaScriptReference(WICKET_AJAX_HTML5_JS);
 			}
 		}.setThrottleDelay(Duration.milliseconds(500)));
 	}
