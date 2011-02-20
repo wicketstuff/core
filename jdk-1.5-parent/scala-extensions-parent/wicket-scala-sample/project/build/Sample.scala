5cb23bec181092a1d7f4381010151997f7f3fded
martin-g <mgrigorov@apache.org>
2011-02-20 13:37:00 +0200

    Issue 10
    Update SBT configurations to make the projects buildable.
    TODO: improve multiproject setup

--- jdk-1.5-parent/scala-extensions-parent/wicket-scala-sample/project/build/Sample.scala
+++ jdk-1.5-parent/scala-extensions-parent/wicket-scala-sample/project/build/Sample.scala
@@ -3,9 +3,11 @@ import sbt._
 class SampleProject(info: ProjectInfo) extends DefaultWebProject(info)
 {
   val localRepo = "Local Maven Repository" at "file://"+Path.userHome+"/.m2/repository"
-  val wicketScala = "org.wicketstuff.scala" % "wicket-scala" % "1.4-SNAPSHOT"
-  val junit = "junit" % "junit" % "4.6" % "test->default"
-  val jettyD = "org.mortbay.jetty" % "jetty" % "6.1.11" % "test->default"
+
+  val wicketScala = "org.wicketstuff.scala" % "wicket-scala" % "1.5-SNAPSHOT"
+  val junit = "junit" % "junit" % "4.8.2" % "test->default"
+  val jettyD = "org.mortbay.jetty" % "jetty" % "6.1.26" % "test->default"
+  val servletD = "javax.servlet" % "servlet-api" % "2.5" 
 
   override def mainResources = super.mainResources +++ descendents(mainScalaSourcePath ##, -sourceExtensions)
 
--- jdk-1.5-parent/scala-extensions-parent/wicket-scala/project/build/WicketScala.scala
+++ jdk-1.5-parent/scala-extensions-parent/wicket-scala/project/build/WicketScala.scala
@@ -3,17 +3,18 @@ import sbt._
 class WicketScalaProject(info: ProjectInfo) extends DefaultWebProject(info)
 {
   val localRepo = "Local Maven Repository" at "file://"+Path.userHome+"/.m2/repository"
-  val wicketScala = "org.wicketstuff.scala" % "wicket-scala" % "1.4-SNAPSHOT"
-  val junit = "junit" % "junit" % "4.6" % "test->default"
-  val jettyD = "org.mortbay.jetty" % "jetty" % "6.1.11" % "test->default"
 
-  val specsD = "org.scala-tools.testing" % "specs" % "1.5.0"  
-  val scalaLibD = "org.scala-lang" % "scala-library" % "2.7.5"
-  val scalaTestD = "org.scala-tools.testing" % "scalatest" % "0.9.5" % "test->default"
-  val wicketD = "org.apache.wicket" % "wicket" % "1.4.0"
-  val wicketExtD = "org.apache.wicket" % "wicket-extensions" % "1.4.0"
-  val sevletApiD = "javax.servlet" % "servlet-api" % "2.4" % "test->default"
-  val slf4jD = "org.slf4j" % "slf4j-log4j12" % "1.4.2"
+  val wicketScala = "org.wicketstuff.scala" % "wicket-scala" % "1.5-SNAPSHOT"
+  val junit = "junit" % "junit" % "4.8.2" % "test->default"
+  val jettyD = "org.mortbay.jetty" % "jetty" % "6.1.26" % "test->default"
+
+  val specsD = "org.scala-tools.testing" % "specs_2.8.1" % "1.6.7.2" % "test->default"
+  val scalaLibD = "org.scala-lang" % "scala-library" % "2.8.1"
+  val scalaTestD = "org.scalatest" % "scalatest" % "1.3" % "test->default"
+  val wicketD = "org.apache.wicket" % "wicket" % "1.5-SNAPSHOT"
+  val wicketExtD = "org.apache.wicket" % "wicket-extensions" % "1.5-SNAPSHOT"
+  val sevletApiD = "javax.servlet" % "servlet-api" % "2.5" % "test->default"
+  val slf4jD = "org.slf4j" % "slf4j-log4j12" % "1.6.1"
   
   override def mainResources = super.mainResources +++ descendents(mainScalaSourcePath ##, -sourceExtensions)
 
