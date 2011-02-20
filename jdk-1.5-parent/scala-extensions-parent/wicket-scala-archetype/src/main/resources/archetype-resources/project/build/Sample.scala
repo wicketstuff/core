0c4549a2857e6fb2b6262eea8436fb8a6abc778d
martin-g <mgrigorov@apache.org>
2011-02-20 13:25:38 +0200

    Issue 10
    Update SBT project configuration

--- jdk-1.5-parent/scala-extensions-parent/wicket-scala-archetype/src/main/resources/archetype-resources/project/build/Sample.scala
+++ jdk-1.5-parent/scala-extensions-parent/wicket-scala-archetype/src/main/resources/archetype-resources/project/build/Sample.scala
@@ -3,11 +3,14 @@ import sbt._
 class SampleProject(info: ProjectInfo) extends DefaultWebProject(info)
 {
   val localRepoR = "Local Maven Repository" at "file://"+Path.userHome+"/.m2/repository"
+
   val wicketScalaD = "org.wicketstuff.scala" % "wicket-scala" % "1.5-SNAPSHOT"
-  //val wicketD = "org.apache.wicket" % "wicket-core" % "1.5-SNAPSHOT"
+  val wicketD = "org.apache.wicket" % "wicket-core" % "1.5-SNAPSHOT"
+
   val junitD = "junit" % "junit" % "4.8.2" % "test->default"
   val jettyD = "org.mortbay.jetty" % "jetty" % "6.1.26" % "test->default"
+  val specsD = "org.scala-tools.testing" % "specs_2.8.1" % "1.6.7.2" % "test->default"
+  var scalatestD = "org.scalatest" % "scalatest" % "1.3" % "test->default"
 
   override def mainResources = super.mainResources +++ descendents(mainScalaSourcePath ##, -sourceExtensions)
-
 }
