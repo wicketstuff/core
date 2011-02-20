515093286c55139ef9551019211475d5edede597
martin-g <mgrigorov@apache.org>
2011-02-20 17:07:22 +0200

    Issue 10
    Fix the archetype's SBT config:
     - use Velocity variable to print the '#' symbol
     - add compile dependency to log4j

--- jdk-1.5-parent/scala-extensions-parent/wicket-scala-archetype/src/main/resources/archetype-resources/project/build/Sample.scala
+++ jdk-1.5-parent/scala-extensions-parent/wicket-scala-archetype/src/main/resources/archetype-resources/project/build/Sample.scala
@@ -6,11 +6,14 @@ class SampleProject(info: ProjectInfo) extends DefaultWebProject(info)
 
   val wicketScalaD = "org.wicketstuff.scala" % "wicket-scala" % "1.5-SNAPSHOT"
   val wicketD = "org.apache.wicket" % "wicket-core" % "1.5-SNAPSHOT"
+  val slf4j_log4jD = "org.slf4j" % "slf4j-log4j12" % "1.6.1"
+  val log4jD = "log4j" % "log4j" % "1.2.16"
 
   val junitD = "junit" % "junit" % "4.8.2" % "test->default"
   val jettyD = "org.mortbay.jetty" % "jetty" % "6.1.26" % "test->default"
   val specsD = "org.scala-tools.testing" % "specs_2.8.1" % "1.6.7.2" % "test->default"
   var scalatestD = "org.scalatest" % "scalatest" % "1.3" % "test->default"
 
-  override def mainResources = super.mainResources +++ descendents(mainScalaSourcePath ##, -sourceExtensions)
+#set( $sharp = "#")
+  override def mainResources = super.mainResources +++ descendents(mainScalaSourcePath $sharp$sharp, -sourceExtensions)
 }
