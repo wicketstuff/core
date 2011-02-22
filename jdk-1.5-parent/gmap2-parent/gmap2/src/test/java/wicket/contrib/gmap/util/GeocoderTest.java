6ed7abeedd16912683db6045661b32870d34f0e1
akiraly <kiralyattila.hu@gmail.com>
2011-02-22 16:58:27 +0100

    [gmap2] Fixing encoding in a test.

--- jdk-1.5-parent/gmap2-parent/gmap2/src/test/java/wicket/contrib/gmap/util/GeocoderTest.java
+++ jdk-1.5-parent/gmap2-parent/gmap2/src/test/java/wicket/contrib/gmap/util/GeocoderTest.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 
 import junit.framework.Assert;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
 import wicket.contrib.gmap.api.GLatLng;
@@ -16,12 +15,16 @@ public class GeocoderTest {
 	 * according to http://www.backups.nl/geocoding/ the provided address is correct but for some reason
 	 * the test fails with status 602 (Unknown address)
 	 * @maintainer: please remove @Ignore when the issue is fixed.
+	 * 
+	 * akiraly:
+	 * I am not the maintainer but this file was not in UTF-8 encoding. After
+	 * correcting that test seems to pass. So I commented @Ignore for now.
 	 */
-	@Ignore
+	//@Ignore
 	@Test
 	public void testGeocoding() throws IOException {
 		Geocoder coder = new Geocoder("ABQIAAAADk60rPazQzTdmKT7CvyKXRQEr9A52YiMqcOFyKYFGBLG6IXC6RQpL6leG0_YYqR-KhNEFosZSJieGQ");
-		GLatLng result = coder.geocode("Salzburgerstraﬂe 205, 4030 Linz, ÷sterreich");
+		GLatLng result = coder.geocode("Salzburgerstra√üe 205, 4030 Linz, √ñsterreich");
 		Assert.assertNotNull(result);
 	}
 }
