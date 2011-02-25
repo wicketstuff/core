39246878e25e27b69b07048c53b2d8f61a88cc80
martin-g <mgrigorov@apache.org>
2011-02-25 11:27:30 +0200

    Add Jetty runner for Scala archetype.

--- jdk-1.5-parent/scala-extensions-parent/wicket-scala-archetype/src/main/resources/archetype-resources/project/build/Sample.scala
+++ jdk-1.5-parent/scala-extensions-parent/wicket-scala-archetype/src/main/resources/archetype-resources/project/build/Sample.scala
@@ -10,7 +10,7 @@ class SampleProject(info: ProjectInfo) extends DefaultWebProject(info)
   val log4jD = "log4j" % "log4j" % "1.2.16"
 
   val junitD = "junit" % "junit" % "4.8.2" % "test->default"
-  val jettyD = "org.mortbay.jetty" % "jetty" % "6.1.26" % "test->default"
+  val jettyD = "org.eclipse.jetty.aggregate" % "jetty-all-server" % "7.3.0.v20110203" % "test->default"
   val specsD = "org.scala-tools.testing" % "specs_2.8.1" % "1.6.7.2" % "test->default"
   var scalatestD = "org.scalatest" % "scalatest" % "1.3" % "test->default"
 
