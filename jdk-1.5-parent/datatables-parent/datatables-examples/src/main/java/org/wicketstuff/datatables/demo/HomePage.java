b2e54c57e4ba912347339ee805e398b84f613324
Bruno Borges <bruno.borges@gmail.com>
2011-03-17 01:02:48 -0300

    Simple version with demo table

--- jdk-1.5-parent/datatables-parent/datatables-examples/src/main/java/org/wicketstuff/datatables/demo/HomePage.java
+++ jdk-1.5-parent/datatables-parent/datatables-examples/src/main/java/org/wicketstuff/datatables/demo/HomePage.java
@@ -16,9 +16,39 @@
  */
 package org.wicketstuff.datatables.demo;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.list.ListItem;
+import org.apache.wicket.markup.html.list.ListView;
+import org.wicketstuff.datatables.DemoDatatable;
 
 public class HomePage extends WebPage {
 
-}
+	public HomePage() {
+		WebMarkupContainer table = new DemoDatatable("table");
+		add(table);
 
+		List<String[]> rows = new ArrayList<String[]>();
+		for (int i = 0; i < 50; i++) {
+			rows.add(new String[] { "Foo" + i, "Bar" + i, "fizzbuzz" + i });
+		}
+
+		ListView<String[]> lv = new ListView<String[]>("rows", rows) {
+			@Override
+			protected void populateItem(ListItem<String[]> item) {
+				String[] row = item.getModelObject();
+
+				item.add(new Label("col1", row[0]));
+				item.add(new Label("col2", row[1]));
+				item.add(new Label("col3", row[2]));
+			}
+		};
+
+		table.add(lv);
+	}
+
+}
