c8f8e17ceaa65e3b6cb0c0cadbdce1b6cb94277e
Bruno Borges <bruno.borges@gmail.com>
2011-03-21 17:11:33 -0300

    forking datatables component to wicket 1.4

---
+++ jdk-1.5-parent/datatables-parent/datatables-examples/src/main/java/org/wicketstuff/datatables/demo/HomePage.java
@@ -0,0 +1,54 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.wicketstuff.datatables.demo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.wicket.markup.html.WebMarkupContainer;
+import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.list.ListItem;
+import org.apache.wicket.markup.html.list.ListView;
+import org.wicketstuff.datatables.DemoDatatable;
+
+public class HomePage extends WebPage {
+
+	public HomePage() {
+		WebMarkupContainer table = new DemoDatatable("table");
+		add(table);
+
+		List<String[]> rows = new ArrayList<String[]>();
+		for (int i = 0; i < 50; i++) {
+			rows.add(new String[] { "Foo" + i, "Bar" + i, "fizzbuzz" + i });
+		}
+
+		ListView<String[]> lv = new ListView<String[]>("rows", rows) {
+			@Override
+			protected void populateItem(ListItem<String[]> item) {
+				String[] row = item.getModelObject();
+
+				item.add(new Label("col1", row[0]));
+				item.add(new Label("col2", row[1]));
+				item.add(new Label("col3", row[2]));
+			}
+		};
+
+		table.add(lv);
+	}
+
+}
---
+++ jdk-1.5-parent/datatables-parent/datatables/src/main/java/org/wicketstuff/datatables/DemoDatatable.java
@@ -0,0 +1,60 @@
+package org.wicketstuff.datatables;
+
+import org.apache.wicket.AttributeModifier;
+import org.apache.wicket.markup.html.IHeaderResponse;
+import org.apache.wicket.markup.html.WebMarkupContainer;
+import org.apache.wicket.model.Model;
+import org.apache.wicket.request.resource.PackageResourceReference;
+
+public class DemoDatatable extends WebMarkupContainer {
+
+	private static final long serialVersionUID = -4387194295178034384L;
+
+	public DemoDatatable(String id) {
+		super(id);
+		setOutputMarkupId(true);
+
+		add(new AttributeModifier("class", new Model("display")));
+	}
+
+	@Override
+	public void renderHead(IHeaderResponse response) {
+		renderDemoCSS(response);
+		renderBasicJS(response);
+
+		StringBuilder js = new StringBuilder();
+		js.append("$(document).ready( function() {\n");
+		js.append("	$('#" + getMarkupId() + "').dataTable( {\n");
+		js.append("		\"bJQueryUI\": true,\n");
+		js.append("		\"sPaginationType\": \"full_numbers\"\n");
+		js.append("	} );\n");
+		js.append("} );");
+
+		response.renderJavaScript(js, getId() + "_datatables");
+	}
+
+	private void renderDemoCSS(IHeaderResponse response) {
+		final Class<DemoDatatable> _ = DemoDatatable.class;
+		response.renderCSSReference(new PackageResourceReference(_,
+				"media/css/demo_table_jui.css"), "screen");
+
+		response.renderCSSReference(new PackageResourceReference(_,
+				"media/css/" + getJUITheme() + "/jquery-ui-1.8.10.custom.css"),
+				"screen");
+	}
+
+	private String getJUITheme() {
+		return "smoothness";
+	}
+
+	private void renderBasicJS(IHeaderResponse response) {
+		final Class<DemoDatatable> _ = DemoDatatable.class;
+
+		response.renderJavaScriptReference(new PackageResourceReference(_,
+				"media/js/jquery.js"));
+		response.renderJavaScriptReference(new PackageResourceReference(_,
+				"media/js/jquery.dataTables.min.js"));
+		response.renderJavaScriptReference(new PackageResourceReference(_,
+				"media/js/jquery-ui-1.8.10.custom.min.js"));
+	}
+}
