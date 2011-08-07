c214825b9d0931dd649d261c09cf087dd62c0d6d
martin-g <mgrigorov@apache.org>
2011-08-07 13:56:56 +0300

    Add ModalX project.
    Contribution by Chris Colman.

---
+++ jdk-1.5-parent/modalx-parent/modalx/src/main/java/com/sas/ui/wicket/modal/IWicketModalVisit.java
@@ -0,0 +1,46 @@
+// -[KeepHeading]-
+
+
+// -[Copyright]-
+
+/**
+ * © 2006, 2009. Step Ahead Software Pty Ltd. All rights reserved.
+ * 
+ * Source file created and managed by Javelin (TM) Step Ahead Software.
+ * To maintain code and model synchronization you may directly edit code in method bodies
+ * and any sections starting with the 'Keep_*' marker. Make all other changes via Javelin.
+ * See http://stepaheadsoftware.com for more details.
+ */
+package com.sas.ui.wicket.modal;
+
+import java.lang.*;
+
+
+// -[KeepBeforeClass]-
+
+
+// -[Class]-
+
+/**
+ * Class Name : IWicketModalVisit
+ * Diagram    : Wicket Generic Modal Windows
+ * Project    : Echobase framework
+ * Type       : interface
+ * Interface implemented by instances used for tracking user visits on a site.
+ * 
+ * @author Chris Colman
+ */
+public abstract 
+interface IWicketModalVisit
+{
+// -[KeepWithinClass]-
+
+
+// -[Fields]-
+
+
+// -[Methods]-
+
+}
+
+
