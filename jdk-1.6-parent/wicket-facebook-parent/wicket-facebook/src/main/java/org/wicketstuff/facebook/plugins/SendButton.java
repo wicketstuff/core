f2bd2d46420bdb438162fefe34377c28738c2a86
Till Freier <tillfreier@googlemail.com>
2011-11-02 16:39:54 +0100

    SendButton Plugin

---
+++ jdk-1.6-parent/wicket-facebook-parent/wicket-facebook/src/main/java/org/wicketstuff/facebook/plugins/SendButton.java
@@ -0,0 +1,23 @@
+package org.wicketstuff.facebook.plugins;
+
+/**
+ * https://developers.facebook.com/docs/reference/plugins/send/
+ * 
+ * @author Till Freier
+ *
+ */
+public class SendButton extends AbstractFacebookPlugin
+{
+
+	public SendButton(String id)
+	{
+		super(id, "fb-send");
+		
+	}
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
