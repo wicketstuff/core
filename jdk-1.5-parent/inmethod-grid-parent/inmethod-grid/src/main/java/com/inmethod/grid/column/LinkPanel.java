516af5f7860cd57206c10680da28f10e738ea4d6
=Tom B <=raystorm_@hotmail.com>
2012-02-17 10:43:56 -0900

    [InMethod-Grid] Added Additional columns  LinkColumn - abstract  PageLinkColumn - example implementation for linking to an internal Wicket Page
    Conflicts:
    	jdk-1.5-parent/inmethod-grid-parent/inmethod-grid/src/main/java/com/inmethod/grid/column/PropertyColumn.java

---
+++ jdk-1.5-parent/inmethod-grid-parent/inmethod-grid/src/main/java/com/inmethod/grid/column/LinkPanel.java
@@ -0,0 +1,93 @@
+package com.inmethod.grid.column;
+
+import org.apache.wicket.Component;
+import org.apache.wicket.Response;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.link.Link;
+import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.model.IModel;
+
+import com.inmethod.grid.IRenderable;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: tfburton
+ * Date: 1/3/12
+ * Time: 2:11 PM
+ *
+ * @author tfburton
+ *         Panel for Displaying a Link in a cell.
+ */
+public abstract class LinkPanel extends Panel //implements IRenderable
+{
+  private String label;
+
+  /** @see Component#Component(String) */
+  public LinkPanel(String id, String Label)
+  {
+    super(id);
+    label = Label;
+  }
+
+  /** @see Component#Component(String, IModel) */
+  public LinkPanel(String id, String Label, IModel model)
+  {
+    super(id, model);
+    label = Label;
+  }
+
+  /**
+   * Called just before a component is rendered.
+   * <p>
+   * <strong>NOTE</strong>: If you override this, you *must* call
+   * super.onBeforeRender() within
+   * your implementation.
+   *
+   * Because this method is responsible for cascading {@link #onBeforeRender()}
+   * call to its
+   * children it is strongly recommended that super call is made at the end of
+   * the override.
+   * </p>
+   *
+   * @see Component#callOnBeforeRenderIfNotVisible()
+   */
+  @Override
+  protected void onBeforeRender()
+  {
+    this.add(new Link("link")
+                 {
+                    /** Called when a link is clicked. */
+                    @Override
+                    public void onClick()
+                    {
+                      LinkPanel.this.onClick();
+                    }
+                 }.add(new Label("label",label)));
+
+    super.onBeforeRender();
+  }
+
+  /**
+   * Renders the output for given cell model. The implementation must take care
+   * of proper escaping
+   * (e.g. translating &lt; to &amp;lt;, etc.) where appropriate.
+   *
+   * @param rowModel model for given row
+   * @param response
+   * /
+  public void render(IModel rowModel, Response response)
+  {
+    /*
+    if ( null !=  this.findMarkupStream()
+      && null != this.findMarkupStream().getTag() )
+    { response.write(this.findMarkupStream().getTag()); }
+    * /
+    response.write();
+    super.render();
+  }
+  */
+
+  public abstract void onClick(); // { }
+
+
+}
