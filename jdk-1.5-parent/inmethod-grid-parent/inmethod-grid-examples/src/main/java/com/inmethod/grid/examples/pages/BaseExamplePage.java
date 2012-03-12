31f7129395ecfc7737848694fa9581a252e1b805
=Tom B <=raystorm_@hotmail.com>
2012-03-12 12:38:28 -0800

    [InMethod Grid] Temporarily move back to jdk-1.5-parent to facilitate an easier cherry-pick of the Appendable Changes.

---
+++ jdk-1.5-parent/inmethod-grid-parent/inmethod-grid-examples/src/main/java/com/inmethod/grid/examples/pages/BaseExamplePage.java
@@ -0,0 +1,23 @@
+package com.inmethod.grid.examples.pages;
+
+import org.apache.wicket.markup.html.WebPage;
+
+/**
+ * Page that contains common markup for all example pages.
+ * 
+ * @author Matej Knopp
+ */
+public class BaseExamplePage extends WebPage
+{
+
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Constructor.
+	 */
+	public BaseExamplePage()
+	{
+
+	}
+
+}
