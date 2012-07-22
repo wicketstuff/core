8cdc90922507042d3027511acdf9f1402e3446b3
jr <jr@jr>
2012-07-22 22:38:23 +0200

    Implemented a callback method for the HTML5 Geolocation API in case the user denies or ignores/dismisses the request for sharing his location. Along the way the example code was updated to demonstrate the new callback method. And since Foursquare (which was used before in the example) requires authentication now, a reverse geocoding request to OpenStreetMap is used as an example.

--- jdk-1.6-parent/wicket-html5-parent/wicket-html5-examples/src/main/java/org/wicketstuff/html5/geolocation/GeolocationDemo.java
+++ jdk-1.6-parent/wicket-html5-parent/wicket-html5-examples/src/main/java/org/wicketstuff/html5/geolocation/GeolocationDemo.java
@@ -5,132 +5,142 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
-
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.markup.html.WebMarkupContainer;
-import org.apache.wicket.markup.html.basic.Label;
-import org.apache.wicket.markup.repeater.RepeatingView;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
+import org.apache.wicket.markup.html.basic.MultiLineLabel;
+import org.apache.wicket.model.Model;
 import org.wicketstuff.html5.BasePage;
 
 /**
- * See http://www.ibm.com/developerworks/web/library/wa-html5webapp/index.html
+ * Demonstrates the use of the HTML5 Geolocation API.
+ * The demo tries to obtain a location (longitude and latitude) and use this 
+ * location to obtain textual information about this location.
+ * The reverse geocoding is done via OpenStreemap,
+ * see http://wiki.openstreetmap.org/wiki/Nominatim#Reverse_Geocoding_.2F_Address_lookup
  */
 public class GeolocationDemo extends BasePage
 {
-	private static final long serialVersionUID = -5976829447267815420L;
-	private static final String BASE_URL = "http://api.foursquare.com/v1/venues.json?";
 
-	public GeolocationDemo()
-	{
+    private static final long serialVersionUID = -5976829447267815420L;
+    private static final String BASE_URL = "http://nominatim.openstreetmap.org/reverse?format=xml&zoom=18&addressdetails=1&";
+    private String result = "";
 
-		final WebMarkupContainer locations = new WebMarkupContainer("locations");
-		locations.setOutputMarkupId(true);
-		add(locations);
-		locations.add(new WebMarkupContainer("location"));
+    public GeolocationDemo()
+    {
 
-		final WebMarkupContainer geoLocator = new WebMarkupContainer("geolocator");
+        Model<String> resultModel = new Model<String>()
+        {
+            @Override
+            public String getObject()
+            {
+                return result;
+            }
+        };
 
-		geoLocator.add(new AjaxGeolocationBehavior()
-		{
-			private static final long serialVersionUID = 1L;
+        final WebMarkupContainer wmcResult = new WebMarkupContainer("wmcResult");
+        wmcResult.setOutputMarkupId(true);
+        MultiLineLabel lbResult = new MultiLineLabel("result", resultModel);
 
-			@Override
-			protected void onGeoAvailable(AjaxRequestTarget target, String latitude,
-				String longitude)
-			{
+        wmcResult.add(lbResult);
+        add(wmcResult);
 
-				String locationsJson = proxyRequest(latitude, longitude);
-				RepeatingView venues = new RepeatingView("location");
 
-				try
-				{
-					JSONObject obj = new JSONObject(locationsJson);
+        final WebMarkupContainer geoLocator = new WebMarkupContainer("geolocator");
 
-					JSONArray groupsArray = obj.getJSONArray("groups");
-					JSONObject groupOne = groupsArray.getJSONObject(0);
-					JSONArray venuesArray = groupOne.getJSONArray("venues");
-					int length = venuesArray.length();
-					for (int i = 0; i < length; i++)
-					{
-						JSONObject venueObject = venuesArray.getJSONObject(i);
-						StringBuilder venue = new StringBuilder();
-						venue.append("City: ")
-							.append(venueObject.getString("city"))
-							.append(", State: ")
-							.append(venueObject.getString("state"))
-							.append(", near by: ")
-							.append(venueObject.getString("name"));
+        AjaxGeolocationBehavior geolocationBehaviour = new AjaxGeolocationBehavior()
+        {
 
-						venues.add(new Label(venues.newChildId(), venue.toString()));
-					}
+            private static final long serialVersionUID = 1L;
 
-				}
-				catch (JSONException e)
-				{
-					e.printStackTrace();
-				}
-				locations.addOrReplace(venues);
+            @Override
+            protected void onGeoAvailable(AjaxRequestTarget target, String latitude,
+                    String longitude)
+            {
+                // permission to locate user was granted
+                // we ask the OpenStreetMap webservice for details about this location 
+                // and display the XML-response as a plain string 
+                // (to keep things simple we are not parsing the XML)
+                result = "We received a location. Latitude: "+ latitude+" Longitude: " + longitude +"\n";
+                result = result + " The XML-Resonse from OpenStreetMap: \n" + proxyRequest(latitude, longitude);
+                target.add(wmcResult);
+            }
 
-				target.add(locations);
-			}
-		});
-		add(geoLocator);
-	}
+            @Override
+            protected void onNotAvailable(AjaxRequestTarget target, String errorCode, String errorMessage)
+            {
+                // if the location is not available, show the reason why
+                result = "An error occured. Error code: "+ errorCode + " Error message:" + errorMessage;
+                target.add(wmcResult);
+            }
+        };
+        // if the user does not react on the request to share his location with
+        // us within 5 seconds we want the onNotAvailable-method to get triggered
+        geolocationBehaviour.setTimeout(5000);
 
-	private String proxyRequest(final String latitude, final String longitude)
-	{
+        geoLocator.add(geolocationBehaviour);
 
-		final StringBuilder sb = new StringBuilder();
-		InputStream stream = null;
 
-		try
-		{
+        add(geoLocator);
+    }
 
-			String urlStr = BASE_URL + "geolat=" + latitude + "&geolong=" + longitude;
+    /**
+     * Asks the OpenStreetMap webservice about details for the provided
+     * location.
+     * @param latitude Latitude value of the position
+     * @param longitude Longitude value of the position 
+     * @return the XML-response from the webservice as a plain string
+     */
+    private final String proxyRequest(final String latitude, final String longitude)
+    {
 
-			final URL url = new URL(urlStr);
-			stream = url.openStream();
-			BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
-			try
-			{
-				String line = "";
-				while (line != null)
-				{
-					line = reader.readLine();
-					if (line != null)
-					{
-						sb.append(line);
-					}
-				}
-			}
-			finally
-			{
-				reader.close();
-			}
+        final StringBuilder sb = new StringBuilder();
+        InputStream stream = null;
 
-		}
-		catch (final Exception e)
-		{
-			e.printStackTrace();
-		}
-		finally
-		{
-			if (stream != null)
-			{
-				try
-				{
-					stream.close();
-				}
-				catch (IOException e)
-				{
-					e.printStackTrace();
-				}
-			}
-		}
+        try
+        {
+            String urlStr = BASE_URL + "lat=" + latitude + "&lon=" + longitude;
 
-		return sb.toString();
-	}
+            final URL url = new URL(urlStr);
+            stream = url.openStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+            try
+            {
+                String line = "";
+                while (line != null)
+                {
+                    line = reader.readLine();
+                    if (line != null)
+                    {
+                        sb.append(line);
+                        sb.append("\n");
+                    }
+                }
+            }
+            finally
+            {
+                reader.close();
+            }
+
+        }
+        catch (final Exception e)
+        {
+            e.printStackTrace();
+        }
+        finally
+        {
+            if (stream != null)
+            {
+                try
+                {
+                    stream.close();
+                }
+                catch (IOException e)
+                {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return sb.toString();
+    }
 }
