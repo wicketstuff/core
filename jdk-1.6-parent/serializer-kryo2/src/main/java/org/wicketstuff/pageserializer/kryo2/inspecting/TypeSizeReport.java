57cd3c6f9bf3e26565315a99e9025f5ea7536291
Michael Mosmann <michael@mosmann.de>
2012-10-18 22:30:25 +0200

    type size report

--- jdk-1.6-parent/serializer-kryo2/src/main/java/org/wicketstuff/pageserializer/kryo2/inspecting/TypeSizeReport.java
+++ jdk-1.6-parent/serializer-kryo2/src/main/java/org/wicketstuff/pageserializer/kryo2/inspecting/TypeSizeReport.java
@@ -1,7 +1,12 @@
 package org.wicketstuff.pageserializer.kryo2.inspecting;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,6 +20,25 @@ public class TypeSizeReport implements ISerializedObjectTreeProcessor {
 	public void process(ISerializedObjectTree tree) {
 		Map<Class<?>, Counter> map=new HashMap<Class<?>, TypeSizeReport.Counter>();
 		process(tree,map);
+
+		List<Map.Entry<Class<?>, Counter>> sorted=new ArrayList<Map.Entry<Class<?>,Counter>>();
+		sorted.addAll(map.entrySet());
+		Collections.sort(sorted,new Comparator<Map.Entry<Class<?>, Counter>>() {
+			@Override
+			public int compare(Entry<Class<?>, Counter> o1,
+					Entry<Class<?>, Counter> o2) {
+				int s1=o1.getValue().size;
+				int s2=o2.getValue().size;
+				return s1==s2 ? 0 : s1>s2 ? -1 : 1;
+			}
+		});
+		
+		StringBuilder sb=new StringBuilder();
+		sb.append("\n-----------------------------\n");
+		for (Map.Entry<Class<?>, Counter> e : sorted) {
+			sb.append(e.getKey().getName()).append("=").append(e.getValue().size).append("\n");
+		}
+		LOG.debug(sb.toString());
 	}
 	
 	private void process(ISerializedObjectTree tree, Map<Class<?>, Counter> map) {
@@ -24,6 +48,7 @@ public class TypeSizeReport implements ISerializedObjectTreeProcessor {
 		for (ISerializedObjectTree child : tree.children()) {
 			process(child,map);
 		}
+		
 	}
 
 	private Counter getOrCreate(Map<Class<?>, Counter> map,
