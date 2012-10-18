a74b1588916d3d386c1b621d7620992b12d9d84b
Michael Mosmann <michael@mosmann.de>
2012-10-18 23:26:24 +0200

    processor refactoring

---
+++ jdk-1.6-parent/serializer-kryo2/src/main/java/org/wicketstuff/pageserializer/kryo2/inspecting/Interfaces.java
@@ -0,0 +1,26 @@
+package org.wicketstuff.pageserializer.kryo2.inspecting;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+public class Interfaces {
+
+	public static <T> T proxyList(final Class<T> type, final T ... instances) {
+		InvocationHandler handler=new InvocationHandler() {
+			
+			@Override
+			public Object invoke(Object proxy, Method method, Object[] args)
+					throws Throwable {
+				
+				Method realMethod=type.getMethod(method.getName(), method.getParameterTypes());
+				
+				for (T p : instances) {
+					realMethod.invoke(p, args);
+				}
+				return null;
+			}
+		};
+		return (T) Proxy.newProxyInstance(ClassLoader.getSystemClassLoader(), new Class[] {type},handler);
+	}
+}
---
+++ jdk-1.6-parent/serializer-kryo2/src/main/java/org/wicketstuff/pageserializer/kryo2/inspecting/TreeSizeReport.java
@@ -0,0 +1,11 @@
+package org.wicketstuff.pageserializer.kryo2.inspecting;
+
+public class TreeSizeReport implements ISerializedObjectTreeProcessor {
+
+	@Override
+	public void process(ISerializedObjectTree tree) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
