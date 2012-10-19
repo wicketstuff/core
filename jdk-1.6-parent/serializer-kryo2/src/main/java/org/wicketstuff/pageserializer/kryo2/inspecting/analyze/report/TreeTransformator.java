1c9b990f17305fec4406d06308dc0b5434d1bc63
Michael Mosmann <michael@mosmann.de>
2012-10-19 15:45:39 +0200

    tests done

--- jdk-1.6-parent/serializer-kryo2/src/main/java/org/wicketstuff/pageserializer/kryo2/inspecting/analyze/report/TreeTransformator.java
+++ jdk-1.6-parent/serializer-kryo2/src/main/java/org/wicketstuff/pageserializer/kryo2/inspecting/analyze/report/TreeTransformator.java
@@ -26,8 +26,9 @@ public class TreeTransformator {
 				for (ISerializedObjectTree child : source.children()) {
 					ISerializedObjectTree filtered = compact(child, filter,level + 1);
 					filteredList.add(filtered);
-					if (filtered != child)
+					if (filtered != child) {
 						changed = true;
+					}
 				}
 
 				if (changed) {
@@ -41,4 +42,45 @@ public class TreeTransformator {
 					source.size()+source.childSize(), new ArrayList<ISerializedObjectTree>());
 		}
 	}
+	
+	public static ISerializedObjectTree strip(ISerializedObjectTree source,
+			ITreeFilter filter) {
+		int level=0;
+		
+		if (!filter.accept(source, level)) throw new IllegalArgumentException("can not strip top level element");
+		
+		return strip(source,filter,level);
+	}
+
+	private static ISerializedObjectTree strip(ISerializedObjectTree source,
+			ITreeFilter filter, int level) {
+		
+		boolean changed = false;
+		int localSize=0;
+		
+		List<ISerializedObjectTree> filteredList = new ArrayList<ISerializedObjectTree>();
+		for (ISerializedObjectTree child : source.children()) {
+			if (filter.accept(child, level +1 )) {
+				ISerializedObjectTree filtered = strip(child, filter,level + 1);
+				filteredList.add(filtered);
+				if (filtered != child) {
+					changed = true;
+				}
+			} else {
+				changed=true;
+				localSize=localSize+child.size();
+				for (ISerializedObjectTree childOfChild : child.children()) {
+					ISerializedObjectTree filtered = strip(childOfChild,filter,level+1);
+					filteredList.add(filtered);
+				}
+			}
+		}
+		
+		if (changed) {
+			return new ImmutableTree(source.type(), source.label(),
+					source.size()+localSize, filteredList);
+		}
+		return source;
+	}
+
 }
