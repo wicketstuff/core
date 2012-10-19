b82613dac4ee34a39c429099a7c1d9428271a324
Michael Mosmann <michael@mosmann.de>
2012-10-19 15:18:03 +0200

    tree builder

--- jdk-1.6-parent/serializer-kryo2/src/test/java/org/wicketstuff/pageserializer/kryo2/inspecting/analyze/report/TestTreeTransformator.java
+++ jdk-1.6-parent/serializer-kryo2/src/test/java/org/wicketstuff/pageserializer/kryo2/inspecting/analyze/report/TestTreeTransformator.java
@@ -1,54 +1,43 @@
 package org.wicketstuff.pageserializer.kryo2.inspecting.analyze.report;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import junit.framework.Assert;
 
 import org.junit.Test;
 import org.wicketstuff.pageserializer.kryo2.inspecting.analyze.ISerializedObjectTree;
-import org.wicketstuff.pageserializer.kryo2.inspecting.analyze.ImmutableTree;
 
 public class TestTreeTransformator {
 
 	@Test
 	public void noop() {
-		TreeSizeReport tr=new TreeSizeReport();
-		
-		ISerializedObjectTree tree = buildTree(3, 2, new Factor(1, 2),
-				new Factor(10, 1));
-		
-		tr.process(tree);
-		
-		ISerializedObjectTree filtered=TreeTransformator.compact(tree, new AcceptAll());
-		Assert.assertTrue(Trees.equals(tree,filtered));
-		tr.process(filtered);
-		
-		filtered=TreeTransformator.compact(tree, new MaxDepth(1));
-		Assert.assertFalse(Trees.equals(tree,filtered));
-		tr.process(filtered);
-	}
+		TreeSizeReport tr = new TreeSizeReport();
 
-	private ISerializedObjectTree buildTree(int level, int subNodes,
-			Factor childFactor, Factor sizeFactor) {
-		int curSize = sizeFactor.calc(level);
-		int curSubNodes = childFactor.calc(subNodes);
-		List<ISerializedObjectTree> children = new ArrayList<ISerializedObjectTree>();
-		if (level>1) {
-			for (int i = 0; i < subNodes; i++) {
-				children.add(buildTree(level - 1, subNodes, childFactor, sizeFactor));
-			}
-		}
-		return new ImmutableTree(String.class, null, curSize, children);
+		ISerializedObjectTree tree = Trees.build(String.class, 30)
+				.withChild(String.class, 20).withChild(String.class, 10)
+				.multiply(2).parent().multiply(2).asTree();
+
+		tr.process(tree);
+
+		ISerializedObjectTree filtered = TreeTransformator.compact(tree,
+				new AcceptAll());
+		Assert.assertTrue(Trees.equals(tree, filtered));
+		tr.process(filtered);
+
+		filtered = TreeTransformator.compact(tree, new MaxDepth(1));
+		Assert.assertFalse(Trees.equals(tree, filtered));
+		tr.process(filtered);
+		
+		ISerializedObjectTree expected = Trees.build(String.class, 30)
+				.withChild(String.class, 40).multiply(2).asTree();
+		Assert.assertTrue(Trees.equals(expected, filtered));
 	}
 
 	private static class AcceptAll implements ITreeFilter {
 		@Override
-		public boolean accept(ISerializedObjectTree source,int level) {
+		public boolean accept(ISerializedObjectTree source, int level) {
 			return true;
 		}
 	}
-	
+
 	private static class MaxDepth implements ITreeFilter {
 
 		private final int level;
@@ -56,25 +45,12 @@ public class TestTreeTransformator {
 		public MaxDepth(int level) {
 			this.level = level;
 		}
-		
+
 		@Override
 		public boolean accept(ISerializedObjectTree source, int level) {
-			return this.level>level;
+			return this.level > level;
 		}
-		
+
 	}
 
-	static class Factor {
-		private final int mul;
-		private final int div;
-
-		public Factor(int mul, int div) {
-			this.mul = mul;
-			this.div = div;
-		}
-
-		public int calc(int source) {
-			return (source * mul) / div;
-		}
-	}
 }
