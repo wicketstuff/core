083d3f92ed7b59e4f37d781c4f9ffec8a0755577
Michael Mosmann <michael@mosmann.de>
2012-10-19 14:27:50 +0200

    tree filter

--- jdk-1.6-parent/serializer-kryo2/src/main/java/org/wicketstuff/pageserializer/kryo2/inspecting/analyze/report/TreeTransformator.java
+++ jdk-1.6-parent/serializer-kryo2/src/main/java/org/wicketstuff/pageserializer/kryo2/inspecting/analyze/report/TreeTransformator.java
@@ -1,21 +1,39 @@
 package org.wicketstuff.pageserializer.kryo2.inspecting.analyze.report;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.wicketstuff.pageserializer.kryo2.inspecting.analyze.ISerializedObjectTree;
+import org.wicketstuff.pageserializer.kryo2.inspecting.analyze.ImmutableTree;
 
 public class TreeTransformator {
 	private TreeTransformator() {
 		// no instance
 	}
-	
-	public static ISerializedObjectTree transform(ISerializedObjectTree source, ITreeFilter filter) {
-		if (!filter.accept(source)) {
-			return compact(source,filter);
-		}
-		return source;
-	}
 
-	private static ISerializedObjectTree compact(ISerializedObjectTree source,
+	public static ISerializedObjectTree compact(ISerializedObjectTree source,
 			ITreeFilter filter) {
-		return source;
+		if (filter.accept(source)) {
+			if (!source.children().isEmpty()) {
+				boolean changed = false;
+
+				List<ISerializedObjectTree> filteredList = new ArrayList<ISerializedObjectTree>();
+				for (ISerializedObjectTree child : source.children()) {
+					ISerializedObjectTree filtered = compact(child, filter);
+					filteredList.add(filtered);
+					if (filtered != child)
+						changed = true;
+				}
+
+				if (changed) {
+					return new ImmutableTree(source.type(), source.label(),
+							source.size(), filteredList);
+				}
+			}
+			return source;
+		} else {
+			return new ImmutableTree(source.type(), source.label(),
+					source.size()+source.childSize(), new ArrayList<ISerializedObjectTree>());
+		}
 	}
 }
