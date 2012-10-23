dd6453ce662fd7d83b3c8d3e897422442d7fdeff
Michael Mosmann <michael@mosmann.de>
2012-10-23 23:11:30 +0200

    report builder works

---
+++ jdk-1.6-parent/serializer-kryo2/src/main/java/org/wicketstuff/pageserializer/kryo2/inspecting/analyze/reportbuilder/AttributeBuilder.java
@@ -0,0 +1,75 @@
+package org.wicketstuff.pageserializer.kryo2.inspecting.analyze.reportbuilder;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class AttributeBuilder
+{
+	Map<Class<? extends Enum>, Enum> attributes = new HashMap<Class<? extends Enum>, Enum>();
+	Map<TypedAttribute, Serializable> typedAttributes = new HashMap<TypedAttribute, Serializable>();
+
+	public AttributeBuilder set(Enum<?> attribute)
+	{
+		attributes.put(attribute.getClass(), attribute);
+		return this;
+	}
+
+	public <T extends Enum<T>> T get(T defaultValue)
+	{
+		T ret = (T)attributes.get(defaultValue.getClass());
+		return ret != null ? ret : defaultValue;
+	}
+
+	public <T extends Serializable> AttributeBuilder set(TypedAttribute<T> type, T value)
+	{
+		typedAttributes.put(type, value);
+		return this;
+	}
+
+	public <T extends Serializable> T get(TypedAttribute<T> type, T defaultValue)
+	{
+		T ret = (T)typedAttributes.get(type);
+		return ret != null ? ret : defaultValue;
+	}
+
+	public IAttributes build()
+	{
+		return new ImmutableAttributes(attributes, typedAttributes);
+	}
+
+	private static class ImmutableAttributes implements IAttributes
+	{
+		private final Map<Class<? extends Enum>, Enum> attributes;
+		private final Map<TypedAttribute, Serializable> typedAttributes;
+
+		private ImmutableAttributes(Map<Class<? extends Enum>, Enum> attributes,
+			Map<TypedAttribute, Serializable> typedAttributes)
+		{
+			final Map<Class<? extends Enum>, Enum> attributesCopy = new HashMap<Class<? extends Enum>, Enum>();
+			attributesCopy.putAll(attributes);
+			final Map<TypedAttribute, Serializable> typedAttributesCopy = new HashMap<TypedAttribute, Serializable>();
+			typedAttributesCopy.putAll(typedAttributes);
+
+			this.attributes = attributesCopy;
+			this.typedAttributes = typedAttributesCopy;
+		}
+
+		@Override
+		public <T extends Enum<T>> T get(T defaultValue)
+		{
+			T ret = (T)attributes.get(defaultValue.getClass());
+			return ret != null ? ret : defaultValue;
+		}
+
+		@Override
+		public <T extends Serializable> T get(TypedAttribute<T> type, T defaultValue)
+		{
+			T ret = (T)typedAttributes.get(type);
+			return ret != null ? ret : defaultValue;
+		}
+	}
+
+
+}
---
+++ jdk-1.6-parent/serializer-kryo2/src/main/java/org/wicketstuff/pageserializer/kryo2/inspecting/analyze/reportbuilder/Column.java
@@ -0,0 +1,54 @@
+package org.wicketstuff.pageserializer.kryo2.inspecting.analyze.reportbuilder;
+
+
+public class Column
+{
+	final IAttributes attributes;
+
+	final String name;
+
+	public Column(String name, IAttributes attributes)
+	{
+		this.name = name;
+		this.attributes = attributes;
+	}
+
+	public Column(String name)
+	{
+		this(name, new AttributeBuilder().build());
+	}
+
+	public IAttributes attributes()
+	{
+		return attributes;
+	}
+
+	public static enum Align implements TypedAttribute<Align> {
+		Left, Right;
+	}
+
+	static class IntegerType implements TypedAttribute<Integer>
+	{
+
+	}
+
+	static class CharType implements TypedAttribute<Character>
+	{
+
+	}
+
+	static class StringType implements TypedAttribute<String>
+	{
+
+	}
+
+	static class AlignType implements TypedAttribute<Align>
+	{
+
+	}
+
+	public static final TypedAttribute<Character> FillBefore = new CharType();
+	public static final TypedAttribute<Character> FillAfter = new CharType();
+	public static final TypedAttribute<String> Separator = new StringType();
+	public static final TypedAttribute<String> Ident = new StringType();
+}
