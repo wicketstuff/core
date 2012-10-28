3a4936ca72001f91cf9298cc961ad13356053d14
Sebastien Briquet <sebfz1@gmail.com>
2012-10-28 12:57:29 +0100

    Updated GoogleAnalyticsBehavior

--- wicket-jquery-ui-samples/src/main/java/com/googlecode/wicket/jquery/ui/samples/TemplatePage.java
+++ wicket-jquery-ui-samples/src/main/java/com/googlecode/wicket/jquery/ui/samples/TemplatePage.java
@@ -11,7 +11,7 @@ import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
 import org.apache.wicket.request.Url;
-import org.apache.wicket.request.cycle.RequestCycle;
+import org.apache.wicket.request.resource.ResourceReference;
 import org.apache.wicket.resource.TextTemplateResourceReference;
 
 public abstract class TemplatePage extends WebPage
@@ -30,8 +30,7 @@ public abstract class TemplatePage extends WebPage
 	{
 		super.onInitialize();
 
-		CharSequence url = this.urlFor(this.getClass(), null);
-		this.add(new GoogleAnalyticsBehavior(url));
+		this.add(new GoogleAnalyticsBehavior(this));
 	}
 }
 
@@ -41,9 +40,32 @@ class GoogleAnalyticsBehavior extends Behavior
 
 	private final String url;
 
-	public GoogleAnalyticsBehavior(final CharSequence url)
+	public GoogleAnalyticsBehavior(final WebPage page)
 	{
-		this.url = url.toString();
+		this.url = this.getUrl(page);
+	}
+
+	private String getUrl(WebPage page)
+	{
+		Url pageUrl = Url.parse(page.urlFor(page.getClass(), null).toString());
+		Url baseUrl = new Url(page.getRequestCycle().getUrlRenderer().getBaseUrl());
+
+		baseUrl.resolveRelative(pageUrl);
+
+		return String.format("%s/%s", page.getRequest().getContextPath(), baseUrl);
+	}
+
+	private IModel<Map<String, Object>> newResourceModel()
+	{
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("url", this.url);
+
+		return Model.ofMap(map);
+	}
+
+	private ResourceReference newResourceReference()
+	{
+		return new TextTemplateResourceReference(GoogleAnalyticsBehavior.class, "gaq.js", this.newResourceModel());
 	}
 
 	@Override
@@ -51,14 +73,6 @@ class GoogleAnalyticsBehavior extends Behavior
 	{
 		super.renderHead(component, response);
 
-		response.renderJavaScriptReference(new TextTemplateResourceReference(GoogleAnalyticsBehavior.class, "gaq.js", this.getResourceModel()), "gaq");
-	}
-
-	private IModel<Map<String, Object>> getResourceModel()
-	{
-		Map<String, Object> map = new HashMap<String, Object>();
-		map.put("url", RequestCycle.get().getUrlRenderer().renderFullUrl(Url.parse(this.url)));
-
-		return Model.ofMap(map);
+		response.renderJavaScriptReference(this.newResourceReference(), "gaq");
 	}
 }
