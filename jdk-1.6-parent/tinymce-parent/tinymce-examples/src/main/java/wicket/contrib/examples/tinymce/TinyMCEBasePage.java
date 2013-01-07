d16b9dd37dbf638e384d174433ad7916d647ee26
klu <kluitmann@valudo.de>
2013-01-07 11:21:06 +0100

    Corrected some formatting issues.

--- jdk-1.6-parent/tinymce-parent/tinymce-examples/src/main/java/wicket/contrib/examples/tinymce/TinyMCEBasePage.java
+++ jdk-1.6-parent/tinymce-parent/tinymce-examples/src/main/java/wicket/contrib/examples/tinymce/TinyMCEBasePage.java
@@ -7,16 +7,18 @@ import wicket.contrib.examples.WicketExamplePage;
 /**
  * @author Iulian-Corneliu COSTAN, JavaLuigi
  */
-public class TinyMCEBasePage extends WicketExamplePage {
+public class TinyMCEBasePage extends WicketExamplePage
+{
 
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    public TinyMCEBasePage() {
-        add(new BookmarkablePageLink("simple", SimpleTinyMCEPage.class));
-        add(new BookmarkablePageLink("full", FullFeaturedTinyMCEPage.class));
-        add(new BookmarkablePageLink("word", WordTinyMCEPage.class));
-        add(new BookmarkablePageLink("ajax", AjaxTinyMCEPage.class));
-        add(new BookmarkablePageLink("inline", InlineTinyMCEPage.class));
-        add(new BookmarkablePageLink("image", ImageUploadTinyMCEPage.class));
-    }
+	public TinyMCEBasePage()
+	{
+		add(new BookmarkablePageLink("simple", SimpleTinyMCEPage.class));
+		add(new BookmarkablePageLink("full", FullFeaturedTinyMCEPage.class));
+		add(new BookmarkablePageLink("word", WordTinyMCEPage.class));
+		add(new BookmarkablePageLink("ajax", AjaxTinyMCEPage.class));
+		add(new BookmarkablePageLink("inline", InlineTinyMCEPage.class));
+		add(new BookmarkablePageLink("image", ImageUploadTinyMCEPage.class));
+	}
 }
