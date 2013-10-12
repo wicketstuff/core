48d0ffaf4b4fc1de9df2dac93f9bafdfe46fce7a
Michael Mosmann <michael@mosmann.de>
2013-10-12 20:53:32 +0200

    initial version of wicket fast serializer

---
+++ jdk-1.6-parent/serializer-fast/src/main/java/org/wicketstuff/pageserializer/fast/FastWicketSerialException.java
@@ -0,0 +1,10 @@
+package org.wicketstuff.pageserializer.fast;
+
+
+public class FastWicketSerialException extends RuntimeException {
+
+	public FastWicketSerialException(String message, Exception cause) {
+		super(message, cause);
+	}
+
+}
