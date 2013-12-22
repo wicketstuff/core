7371610fc344aa5e44c4079f720b416ad2f6708e
Rob Sonke <r.sonke@maxxton.com>
2013-12-22 17:39:27 +0100

    Made a stateless version of the GMap component

--- jdk-1.6-parent/gmap3-parent/gmap3/src/main/java/org/wicketstuff/gmap/StatelessGMapHeaderContributor.java
+++ jdk-1.6-parent/gmap3-parent/gmap3/src/main/java/org/wicketstuff/gmap/StatelessGMapHeaderContributor.java
@@ -2,9 +2,19 @@ package org.wicketstuff.gmap;
 
 import org.apache.wicket.Component;
 
+/**
+ * 
+ * @author robsonke
+ *
+ */
 public class StatelessGMapHeaderContributor extends GMapHeaderContributor {
 	private static final long serialVersionUID = 1L;
 
+	public StatelessGMapHeaderContributor(final String scheme)
+	{
+		super(scheme);
+	}
+	
 	@Override
 	public boolean getStatelessHint(Component component) {
 		return true;
