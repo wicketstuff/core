353d4fb4ed51230544b72582d0d94bfd45633c36
Sebastien <sebfz1@gmail.com>
2014-05-06 20:52:00 +0200

    Downgraded jetty to 8.1.15.v20140411

--- wicket-jquery-ui-samples/src/test/java/embedded/jetty/Start.java
+++ wicket-jquery-ui-samples/src/test/java/embedded/jetty/Start.java
@@ -1,12 +1,9 @@
 package embedded.jetty;
 
 import org.apache.wicket.util.time.Duration;
-import org.eclipse.jetty.server.HttpConfiguration;
-import org.eclipse.jetty.server.HttpConnectionFactory;
-import org.eclipse.jetty.server.SecureRequestCustomizer;
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.server.ServerConnector;
-import org.eclipse.jetty.server.SslConnectionFactory;
+import org.eclipse.jetty.server.bio.SocketConnector;
+import org.eclipse.jetty.server.ssl.SslSocketConnector;
 import org.eclipse.jetty.util.resource.Resource;
 import org.eclipse.jetty.util.ssl.SslContextFactory;
 import org.eclipse.jetty.webapp.WebAppContext;
@@ -18,51 +15,38 @@ public class Start
 	 */
 	private static final String CONTEXT_PATH = "/wicket-jquery-ui";
 	private static final int PORT = 8080;
-	private static final long TIMEOUT = Duration.ONE_HOUR.getMilliseconds();
+	private static final int TIMEOUT = (int) Duration.ONE_HOUR.getMilliseconds();
 
 	public static void main(String[] args) throws Exception
 	{
 		System.setProperty("wicket.configuration", "development");
 
+		SocketConnector connector = new SocketConnector();
+		connector.setPort(PORT);
+		connector.setSoLingerTime(-1);
+		connector.setMaxIdleTime(TIMEOUT);
+		connector.setConfidentialPort(8443);
+
 		Server server = new Server();
-
-		HttpConfiguration http_config = new HttpConfiguration();
-		http_config.setSecureScheme("https");
-		http_config.setSecurePort(8443);
-		http_config.setOutputBufferSize(32768);
-
-		ServerConnector http = new ServerConnector(server, new HttpConnectionFactory(http_config));
-		http.setPort(PORT);
-		http.setIdleTimeout(TIMEOUT);
-
-		server.addConnector(http);
-
+		server.addConnector(connector);		
+		
 		Resource keystore = Resource.newClassPathResource("/keystore");
+
 		if (keystore != null && keystore.exists())
 		{
-			// if a keystore for a SSL certificate is available, start a SSL
-			// connector on port 8443.
-			// By default, the quickstart comes with a Apache Wicket Quickstart
-			// Certificate that expires about half way september 2021. Do not
-			// use this certificate anywhere important as the passwords are
-			// available in the source.
+			
+			SslContextFactory factory = new SslContextFactory();
+			factory.setKeyStoreResource(keystore);
+			factory.setKeyStorePassword("wicket");
+			factory.setTrustStore("/keystore");
+			factory.setKeyManagerPassword("wicket");
 
-			SslContextFactory sslContextFactory = new SslContextFactory();
-			sslContextFactory.setKeyStoreResource(keystore);
-			sslContextFactory.setKeyStorePassword("wicket");
-			sslContextFactory.setKeyManagerPassword("wicket");
+			SslSocketConnector sslConnector = new SslSocketConnector(factory);
+			sslConnector.setMaxIdleTime(TIMEOUT);
+			sslConnector.setPort(8443);
+			sslConnector.setAcceptors(4);
 
-			HttpConfiguration https_config = new HttpConfiguration(http_config);
-			https_config.addCustomizer(new SecureRequestCustomizer());
-
-			ServerConnector https = new ServerConnector(server, new SslConnectionFactory(sslContextFactory, "http/1.1"), new HttpConnectionFactory(https_config));
-			https.setPort(8443);
-			https.setIdleTimeout(500000);
-
-			server.addConnector(https);
-			System.out.println("SSL access to the examples has been enabled on port 8443");
-			System.out.println("You can access the application using SSL on https://localhost:8443");
-			System.out.println();
+			server.addConnector(sslConnector);			
 		}
 
 		WebAppContext context = new WebAppContext();
