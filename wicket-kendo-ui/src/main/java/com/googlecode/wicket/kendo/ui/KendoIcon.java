eb3b50feecdb535f71f377510179969bbd167936
Sebastien <sebfz1@gmail.com>
2014-08-03 15:29:55 +0200

    Upgraded KendoIcon Removed KendoIcon#COLUMNS Removed KendoIcon#EXCEPTION Removed KendoIcon#GROUP Removed KendoIcon#INSERT_ Removed KendoIcon#PIN Removed KendoIcon#SORT_ASC Removed KendoIcon#SORT_DESC Removed KendoIcon#UNGROUP Removed KendoIcon#UNPIN

--- wicket-kendo-ui/src/main/java/com/googlecode/wicket/kendo/ui/KendoIcon.java
+++ wicket-kendo-ui/src/main/java/com/googlecode/wicket/kendo/ui/KendoIcon.java
@@ -19,7 +19,7 @@ package com.googlecode.wicket.kendo.ui;
 import com.googlecode.wicket.kendo.ui.form.button.Button;
 
 /**
- * Provides some default jQuery icon class. Might be used to decorate a {@link Button} for instance.
+ * Provides default Kendo UI icons. Might be used to decorate a {@link Button} for instance.
  *
  * @author Sebastien Briquet - sebfz1
  *
@@ -28,52 +28,43 @@ public class KendoIcon
 {
 	public static final String NONE = "";
 
-	public static final String ARROW_N = "arrow_n";
-	public static final String ARROW_E = "arrow_e";
-	public static final String ARROW_S = "arrow_s";
-	public static final String ARROW_W = "arrow_w";
-	public static final String ARROWHEAD_E = "arrowhead_e";
-	public static final String ARROWHEAD_N = "arrowhead_n";
-	public static final String ARROWHEAD_S = "arrowhead_s";
-	public static final String ARROWHEAD_W = "arrowhead_w";
+	public static final String ARROW_N = "arrow-n";
+	public static final String ARROW_E = "arrow-e";
+	public static final String ARROW_S = "arrow-s";
+	public static final String ARROW_W = "arrow-w";
+	public static final String ARROWHEAD_E = "arrowhead-e";
+	public static final String ARROWHEAD_N = "arrowhead-n";
+	public static final String ARROWHEAD_S = "arrowhead-s";
+	public static final String ARROWHEAD_W = "arrowhead-w";
 	public static final String CALENDAR = "calendar";
 	public static final String CANCEL = "cancel";
 	public static final String CLOCK = "clock";
 	public static final String CLOSE = "close";
 	public static final String COLLAPSE = "collapse";
-	public static final String COLLAPSE_W = "collapse_w";
-	public static final String COLUMNS = "columns";
+	public static final String COLLAPSE_W = "collapse-w";
 	public static final String CUSTOM = "custom";
-	public static final String EXCEPTION = "exception";
 	public static final String EXPAND = "expand";
-	public static final String EXPAND_W = "expand_w";
-	public static final String FOLDER_ADD = "folder_add";
-	public static final String FOLDER_UP = "folder_up";
+	public static final String EXPAND_W = "expand-w";
+	public static final String FOLDER_ADD = "folder-add";
+	public static final String FOLDER_UP = "folder-up ";
 	public static final String FUNNEL = "funnel";
-	public static final String FUNNEL_CLEAR = "funnel_clear";
-	public static final String GROUP = "group";
-	public static final String INSERT_ = "insert_";
-	public static final String INSERT_M = "insert_m";
-	public static final String INSERT_N = "insert_n";
-	public static final String INSERT_S = "insert_s";
+	public static final String FUNNEL_CLEAR = "funnel-clear";
+	public static final String INSERT_M = "insert-m";
+	public static final String INSERT_N = "insert-n";
+	public static final String INSERT_S = "insert-s";
 	public static final String MAXIMIZE = "maximize";
 	public static final String MINIMIZE = "minimize";
 	public static final String NOTE = "note";
 	public static final String PENCIL = "pencil";
-	public static final String PIN = "pin";
 	public static final String PLUS = "plus";
 	public static final String REFRESH = "refresh";
 	public static final String RESTORE = "restore";
 	public static final String SEARCH = "search";
-	public static final String SEEK_E = "seek_e";
-	public static final String SEEK_N = "seek_n";
-	public static final String SEEK_S = "seek_s";
-	public static final String SEEK_W = "seek_w";
-	public static final String SORT_ASC = "sort_asc";
-	public static final String SORT_DESC = "sort_desc";
+	public static final String SEEK_E = "seek-e";
+	public static final String SEEK_N = "seek-n";
+	public static final String SEEK_S = "seek-s";
+	public static final String SEEK_W = "seek-w";
 	public static final String TICK = "tick";
-	public static final String UNGROUP = "ungroup";
-	public static final String UNPIN = "unpin";
 
 	/**
 	 * Constants class
