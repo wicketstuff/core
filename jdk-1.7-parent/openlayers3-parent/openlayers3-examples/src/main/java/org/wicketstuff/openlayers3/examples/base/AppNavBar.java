c8887912b62f3a27373847058735719a79a735ef
cmiles74 <twitch@nervestaple.com>
2014-08-14 15:38:05 -0400

    Worked on the examples.

--- jdk-1.7-parent/openlayers3-parent/openlayers3-examples/src/main/java/org/wicketstuff/openlayers3/examples/base/AppNavBar.java
+++ jdk-1.7-parent/openlayers3-parent/openlayers3-examples/src/main/java/org/wicketstuff/openlayers3/examples/base/AppNavBar.java
@@ -5,21 +5,31 @@ import de.agilecoders.wicket.core.markup.html.bootstrap.navbar.Navbar;
 import de.agilecoders.wicket.core.markup.html.bootstrap.navbar.NavbarButton;
 import de.agilecoders.wicket.core.markup.html.bootstrap.navbar.NavbarComponents;
 import org.apache.wicket.model.Model;
-import org.wicketstuff.openlayers3.examples.HomePage;
-import org.wicketstuff.openlayers3.examples.SimpleMap;
+import org.wicketstuff.openlayers3.examples.*;
 
 /**
  * Provides the navigation bar for the application.
  */
 public class AppNavBar extends Navbar {
 
+    /**
+     * Creates a new instance.
+     *
+     * @param id Wicket element ID
+     */
     public AppNavBar(final String id) {
         super(id);
 
+        // navigation title
         setBrandName(Model.of("Wicket Stuff OpenLayers 3"));
+
+        // navigation items
         addComponents(NavbarComponents.transform(Navbar.ComponentPosition.LEFT,
                 new NavbarButton<Void>(HomePage.class, Model.of("Home"))
                         .setIconType(GlyphIconType.home),
-                new NavbarButton<Void>(SimpleMap.class, Model.of("Map"))));
+                new NavbarButton<Void>(SimplePage.class, Model.of("Simple")),
+                new NavbarButton<Void>(OsmPage.class, Model.of("OpenStreetMap")),
+                new NavbarButton<Void>(MarkerPage.class, Model.of("Marker")),
+                new NavbarButton<Void>(PopoverPage.class, Model.of("Popover"))));
     }
 }
