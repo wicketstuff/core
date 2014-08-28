0d928287d848c25e65e9371818ce84c985a3d91e
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2014-08-28 16:37:26 +0300

    [datatables] Extract the Person related classes to outer classes

--- jdk-1.6-parent/datatables-parent/datatables-examples/src/main/java/org/wicketstuff/datatables/demo/NewPage.java
+++ jdk-1.6-parent/datatables-parent/datatables-examples/src/main/java/org/wicketstuff/datatables/demo/NewPage.java
@@ -1,16 +1,12 @@
 package org.wicketstuff.datatables.demo;
 
-import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 import org.apache.wicket.extensions.markup.html.repeater.data.table.HeadersToolbar;
 import org.apache.wicket.extensions.markup.html.repeater.data.table.IColumn;
 import org.apache.wicket.extensions.markup.html.repeater.data.table.PropertyColumn;
-import org.apache.wicket.extensions.markup.html.repeater.util.SortableDataProvider;
 import org.apache.wicket.markup.html.WebPage;
-import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.wicketstuff.datatables.DataTables;
@@ -48,39 +44,4 @@ public class NewPage extends WebPage {
 //        table.getOptions().order(new Sort(2, Sort.Direction.DESC), new Sort(0, Sort.Direction.ASC)); // multi column ordering
     }
 
-    private static class Person implements Serializable {
-        private String firstName;
-        private String lastName;
-        private int age;
-
-        private Person(String firstName, String lastName, int age) {
-            this.firstName = firstName;
-            this.lastName = lastName;
-            this.age = age;
-        }
-    }
-    
-    private static class PeopleDataProvider extends SortableDataProvider<Person, String> {
-
-        private final List<Person> people;
-        
-        private PeopleDataProvider(List<Person> people) {
-            this.people = people;
-        }
-        
-        @Override
-        public Iterator<? extends Person> iterator(long first, long count) {
-            return people.subList((int)first, (int)count).iterator();
-        }
-
-        @Override
-        public long size() {
-            return people.size();
-        }
-
-        @Override
-        public IModel<Person> model(Person person) {
-            return Model.of(person);
-        }
-    }
 }
