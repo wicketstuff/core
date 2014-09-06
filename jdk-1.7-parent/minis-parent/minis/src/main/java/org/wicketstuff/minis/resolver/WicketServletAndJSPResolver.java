1c53cfe209828132331f61318220861d3a742af2
klopfdreh <nocturnts@googlemail.com>
2014-09-06 08:10:21 +0200

    [minis] JSP / JSF / Servlet integration EL Taglibs (master)

--- jdk-1.7-parent/minis-parent/minis/src/main/java/org/wicketstuff/minis/resolver/WicketServletAndJSPResolver.java
+++ jdk-1.7-parent/minis-parent/minis/src/main/java/org/wicketstuff/minis/resolver/WicketServletAndJSPResolver.java
@@ -47,6 +47,8 @@ import org.slf4j.LoggerFactory;
  * <code><pre>&lt;wicket:jsp file="/de/test/jspwicket/TestPage.jsp"&gt;&lt;/wicket:jsp&gt;</pre></code>
  * or
  * <code><pre>&lt;wicket:servlet path="/de/test/jspwicket/Servlet"&gt;&lt;/wicket:servlet&gt;</pre></code>
+ * or
+ * <code><pre>&lt;wicket:jsf file="/TestPage.xhtml"&gt;&lt;/wicket:jsf&gt;</pre></code>
  *
  * <b>Links:</b><br>
  * https://cwiki.apache.org/confluence/display/WICKET/Including+JSP+files+in+
@@ -55,246 +57,266 @@ import org.slf4j.LoggerFactory;
  * wrapping-td4407174.html<br>
  * <br>
  * 
- * @see org.apache.wicket.protocol.http.WebApplication#init()
+ * @see org.apache.wicket.protocol.http.WebApplication.init()
  * 
  * @author Tobias Soloschenko
  */
 public class WicketServletAndJSPResolver implements IComponentResolver {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	private static final String SERVLET_AND_JSP_ENCODING = "UTF-8";
+    private static final String SERVLET_AND_JSP_ENCODING = "UTF-8";
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(WicketServletAndJSPResolver.class);
+    private static final Logger LOGGER = LoggerFactory
+	    .getLogger(WicketServletAndJSPResolver.class);
 
-	// Registration of the tag identifier
-	static {
+    // Registration of the tag identifier
+    static {
 	if (LOGGER.isTraceEnabled()) {
-		LOGGER.trace("Registering"
-			+ WicketServletAndJSPResolver.class.getName());
+	    LOGGER.trace("Registering"
+		    + WicketServletAndJSPResolver.class.getName());
 	}
 	WicketTagIdentifier.registerWellKnownTagName("jsp");
+	WicketTagIdentifier.registerWellKnownTagName("jsf");
 	WicketTagIdentifier.registerWellKnownTagName("servlet");
+    }
+
+    @Override
+    public Component resolve(MarkupContainer container,
+	    MarkupStream markupStream, ComponentTag tag) {
+	if (tag instanceof WicketTag) {
+	    WicketTag wtag = (WicketTag) tag;
+	    if ("jsp".equalsIgnoreCase(wtag.getName())) {
+		String file = wtag.getAttributes().getString("file");
+		if (file == null || file.trim().length() == 0) {
+		    throw new MarkupException(
+			    "Wrong format of <wicket:jsp file='/foo.jsp'>: attribute 'file' is missing");
+		}
+		return new ServletAndJspFileContainer(file, Type.JSP);
+	    } else if ("jsf".equalsIgnoreCase(wtag.getName())) {
+		String file = wtag.getAttributes().getString("file");
+		if (file == null || file.trim().length() == 0) {
+		    throw new MarkupException(
+			    "Wrong format of <wicket:jsf file='/foo.xhtml'>: attribute 'file' is missing");
+		}
+		return new ServletAndJspFileContainer(file, Type.JSF);
+	    } else if ("servlet".equalsIgnoreCase(wtag.getName())) {
+		String path = wtag.getAttributes().getString("path");
+		if (path == null || path.trim().length() == 0) {
+		    throw new MarkupException(
+			    "Wrong format of <wicket:servlet path='/Test'>: attribute 'path' is missing");
+		}
+		return new ServletAndJspFileContainer(path, Type.SERVLET);
+	    }
+	}
+	return null;
+    }
+
+    /**
+     * The JSP container which contains the JSP output and renders it to the
+     * Wicket HTML page
+     */
+    private static class ServletAndJspFileContainer extends MarkupContainer {
+
+	private static final long serialVersionUID = -4296125929087527034L;
+
+	private final String resource;
+
+	private final Type type;
+
+	public ServletAndJspFileContainer(String resource, Type type) {
+	    super(resource);
+	    this.resource = resource;
+	    this.type = type;
+	}
+
+	/**
+	 * Required so that the tag don't have to be opened / closed. It can
+	 * also be defined as empty tag.
+	 */
+	@Override
+	protected void onComponentTag(final ComponentTag tag) {
+	    tag.setType(XmlTag.TagType.OPEN);
+	    super.onComponentTag(tag);
+	}
+
+	/**
+	 * Renders the component tag body with the content of the JSP output
+	 */
+	@Override
+	public void onComponentTagBody(MarkupStream markupStream,
+		ComponentTag openTag) {
+
+	    // Get the everything required to include the jsp file
+	    RequestCycle cycle = getRequestCycle();
+	    ServletRequest request = (HttpServletRequest) cycle.getRequest()
+		    .getContainerRequest();
+	    JSPIncludeHttpServletResponseWrapper response = new JSPIncludeHttpServletResponseWrapper(
+		    (HttpServletResponse) cycle.getResponse()
+			    .getContainerResponse());
+	    ServletContext context = ((WebApplication) Application.get())
+		    .getServletContext();
+
+	    // Handle a missing jsp file
+	    handleMissingResource(context);
+
+	    try {
+		// include the JSP file by the given request / response
+		context.getRequestDispatcher(resource).include(request,
+			response);
+
+		// replace the component tag body with the result of the JSP
+		// output
+		replaceComponentTagBody(markupStream, openTag,
+			response.getOutput());
+	    } catch (ServletException | IOException e) {
+		throw new WicketRuntimeException(e);
+	    }
+
+	}
+
+	/**
+	 * Handles missing files.
+	 * 
+	 * @param context
+	 *            the servlet context
+	 * @throws WicketRuntimeException
+	 *             if the resource file couldn't be resolved or an exception
+	 *             should be thrown if it is missing
+	 */
+	private void handleMissingResource(ServletContext context)
+		throws WicketRuntimeException {
+	    try {
+		if (type == Type.JSP || type == Type.JSF) {
+		    if (context.getResource(resource) == null) {
+			promptMissingResource(context, type);
+		    }
+		} else {
+		    // Servlet Registration is available at servlet api 3.0 so
+		    // we can't use it currently for wicket 6.16.0 because of
+		    // min requirements
+		    // boolean found = false;
+		    // Iterator<? extends ServletRegistration>
+		    // servletRegistrationIterator = context
+		    // .getServletRegistrations().values().iterator();
+		    // while (servletRegistrationIterator.hasNext()) {
+		    // Iterator<String> mappingsIterator =
+		    // servletRegistrationIterator
+		    // .next().getMappings().iterator();
+		    // while (mappingsIterator.hasNext()) {
+		    // String mapping = mappingsIterator.next();
+		    // if (resource.equals(mapping)) {
+		    // found = true;
+		    // }
+		    // }
+		    // }
+		    // if (!found) {
+		    // promptMissingResource(context);
+		    // }
+
+		    if (context.getRequestDispatcher(resource) == null) {
+			promptMissingResource(context, type);
+		    }
+		}
+	    } catch (MalformedURLException e) {
+		throw new WicketRuntimeException(e);
+	    }
+	}
+
+	/**
+	 * This method throws an exception if wicket is configured to throw an
+	 * exception for missing resources, or gives a warning message through
+	 * the logging mechanism.
+	 * 
+	 * @param context
+	 *            the context to be printed.
+	 * @param type the type
+	 */
+	private void promptMissingResource(ServletContext context, Type type) {
+	    if (shouldThrowExceptionForMissingFile()) {
+		throw new WicketRuntimeException(
+			String.format(
+				"Cannot locate resource %s of type: %s within current context: %s",
+				resource, type.toString(),
+				context.getContextPath()));
+	    } else {
+		LOGGER.warn(String
+			.format("Resource of type: %s will not be processed. Cannot locate it %s within current context: %s",
+				type.toString(), resource,
+				context.getContextPath()));
+	    }
+	}
+
+	/**
+	 * Checks if an exception should be thrown, if a resource file is
+	 * missing.
+	 * 
+	 * @return if an exception should be thrown
+	 */
+	private boolean shouldThrowExceptionForMissingFile() {
+	    return Application.get().getResourceSettings()
+		    .getThrowExceptionOnMissingResource();
+	}
+    }
+
+    /**
+     * The ByteArrayServletOutputStream writes bytes to the given
+     * ByteArrayOutputStream
+     */
+    private static class ByteArrayServletOutputStream extends
+	    ServletOutputStream {
+	private ByteArrayOutputStream baos;
+
+	public ByteArrayServletOutputStream(ByteArrayOutputStream baos) {
+	    this.baos = baos;
 	}
 
 	@Override
-	public Component resolve(MarkupContainer container, MarkupStream markupStream, ComponentTag tag) {
-		if (tag instanceof WicketTag) {
-			WicketTag wtag = (WicketTag) tag;
-			if ("jsp".equalsIgnoreCase(wtag.getName())) {
-			String file = wtag.getAttributes().getString("file");
-			if (file == null || file.trim().length() == 0) {
-				throw new MarkupException(
-					"Wrong format of <wicket:jsp file='/foo.jsp'>: attribute 'file' is missing");
-			}
-			return new ServletAndJspFileContainer(file, Type.JSP);
-			} else if ("servlet".equalsIgnoreCase(wtag.getName())) {
-			String path = wtag.getAttributes().getString("path");
-			if (path == null || path.trim().length() == 0) {
-				throw new MarkupException(
-					"Wrong format of <wicket:servlet path='/Test'>: attribute 'path' is missing");
-			}
-			return new ServletAndJspFileContainer(path, Type.SERVLET);
-			}
-		}
-		return null;
+	public void write(int param) throws IOException {
+	    baos.write(param);
+	}
+    }
+
+    /**
+     * This HttpServletResponseWrapper is used to get the JSP's output by
+     * including it with a request dispatcher.
+     */
+    private static class JSPIncludeHttpServletResponseWrapper extends
+	    HttpServletResponseWrapper {
+
+	private final ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+	private final ServletOutputStream byteArrayServletOutputStream = new ByteArrayServletOutputStream(
+		byteArrayOutputStream);
+	private final PrintWriter printWriter = new PrintWriter(
+		byteArrayOutputStream);
+
+	public JSPIncludeHttpServletResponseWrapper(HttpServletResponse response) {
+	    super(response);
 	}
 
-	/**
-	 * The JSP container which contains the JSP output and renders it to the
-	 * Wicket HTML page
-	 */
-	private static class ServletAndJspFileContainer extends MarkupContainer {
-
-		private static final long serialVersionUID = -4296125929087527034L;
-
-		private final String resource;
-
-		private final Type type;
-
-		public ServletAndJspFileContainer(String resource, Type type) {
-			super(resource);
-			this.resource = resource;
-			this.type = type;
-		}
-
-		/**
-		 * Required so that the tag don't have to be opened / closed. It can
-		 * also be defined as empty tag.
-		 */
-		@Override
-		protected void onComponentTag(final ComponentTag tag) {
-			tag.setType(XmlTag.TagType.OPEN);
-			super.onComponentTag(tag);
-		}
-
-		/**
-		 * Renders the component tag body with the content of the JSP output
-		 */
-		@Override
-		public void onComponentTagBody(MarkupStream markupStream,
-			ComponentTag openTag) {
-
-			// Get the everything required to include the jsp file
-			RequestCycle cycle = getRequestCycle();
-			ServletRequest request = (HttpServletRequest) cycle.getRequest().getContainerRequest();
-			JSPIncludeHttpServletResponseWrapper response = new JSPIncludeHttpServletResponseWrapper(
-				(HttpServletResponse) cycle.getResponse().getContainerResponse());
-			ServletContext context = ((WebApplication) Application.get()).getServletContext();
-
-			// Handle a missing jsp file
-			handleMissingResource(context);
-
-			try {
-			// include the JSP file by the given request / response
-				context.getRequestDispatcher(resource).include(request, response);
-
-				// replace the component tag body with the result of the JSP
-				// output
-				replaceComponentTagBody(markupStream, openTag, response.getOutput());
-			} catch (ServletException | IOException e) {
-				throw new WicketRuntimeException(e);
-			}
-
-		}
-
-		/**
-		 * Handles missing files.
-		 *
-		 * @param context
-		 *            the servlet context
-		 * @throws WicketRuntimeException
-		 *             if the resource file couldn't be resolved or an exception
-		 *             should be thrown if it is missing
-		 */
-		private void handleMissingResource(ServletContext context)
-			throws WicketRuntimeException {
-			try {
-			if (type == Type.JSP) {
-				if (context.getResource(resource) == null) {
-				promptMissingResource(context, type);
-				}
-			} else {
-				// Servlet Registration is available at servlet api 3.0 so
-				// we can't use it currently for wicket 6.16.0 because of
-				// min requirements
-				// boolean found = false;
-				// Iterator<? extends ServletRegistration>
-				// servletRegistrationIterator = context
-				// .getServletRegistrations().values().iterator();
-				// while (servletRegistrationIterator.hasNext()) {
-				// Iterator<String> mappingsIterator =
-				// servletRegistrationIterator
-				// .next().getMappings().iterator();
-				// while (mappingsIterator.hasNext()) {
-				// String mapping = mappingsIterator.next();
-				// if (resource.equals(mapping)) {
-				// found = true;
-				// }
-				// }
-				// }
-				// if (!found) {
-				// promptMissingResource(context);
-				// }
-
-				if (context.getRequestDispatcher(resource) == null) {
-					promptMissingResource(context, type);
-				}
-			}
-			} catch (MalformedURLException e) {
-				throw new WicketRuntimeException(e);
-			}
-		}
-
-		/**
-		 * This method throws an exception if wicket is configured to throw an
-		 * exception for missing resources, or gives a warning message through
-		 * the logging mechanism.
-		 *
-		 * @param context
-		 *            the context to be printed.
-		 * @param type the type
-		 */
-		private void promptMissingResource(ServletContext context, Type type) {
-			if (shouldThrowExceptionForMissingFile()) {
-				throw new WicketRuntimeException(
-					String.format(
-						"Cannot locate resource %s of type: %s within current context: %s",
-						resource, type.toString(),
-						context.getContextPath()));
-			} else {
-				LOGGER.warn(String.format(
-					"Resource of type: %s will not be processed. Cannot locate it %s within current context: %s",
-					type.toString(), resource, context.getContextPath()));
-			}
-		}
-
-		/**
-		 * Checks if an exception should be thrown, if a resource file is
-		 * missing.
-		 *
-		 * @return if an exception should be thrown
-		 */
-		private boolean shouldThrowExceptionForMissingFile() {
-			return Application.get().getResourceSettings().getThrowExceptionOnMissingResource();
-		}
+	@Override
+	public ServletOutputStream getOutputStream() {
+	    return byteArrayServletOutputStream;
 	}
 
-	/**
-	 * The ByteArrayServletOutputStream writes bytes to the given
-	 * ByteArrayOutputStream
-	 */
-	private static class ByteArrayServletOutputStream extends ServletOutputStream {
-		private ByteArrayOutputStream baos;
-
-		public ByteArrayServletOutputStream(ByteArrayOutputStream baos) {
-			this.baos = baos;
-		}
-
-		@Override
-		public void write(int param) throws IOException {
-			baos.write(param);
-		}
+	@Override
+	public PrintWriter getWriter() throws IOException {
+	    return printWriter;
 	}
 
-	/**
-	 * This HttpServletResponseWrapper is used to get the JSP's output by
-	 * including it with a request dispatcher.
-	 */
-	private static class JSPIncludeHttpServletResponseWrapper extends HttpServletResponseWrapper {
-
-		private final ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-
-		private final ServletOutputStream byteArrayServletOutputStream = new ByteArrayServletOutputStream(byteArrayOutputStream);
-
-		private final PrintWriter printWriter = new PrintWriter(byteArrayOutputStream);
-
-		public JSPIncludeHttpServletResponseWrapper(HttpServletResponse response) {
-			super(response);
-		}
-
-		@Override
-		public ServletOutputStream getOutputStream() {
-			return byteArrayServletOutputStream;
-		}
-
-		@Override
-		public PrintWriter getWriter() throws IOException {
-			return printWriter;
-		}
-
-		public String getOutput() throws UnsupportedEncodingException {
-			// if something has been written with the writer - we need to flush it!
-			printWriter.flush();
-			return byteArrayOutputStream.toString(SERVLET_AND_JSP_ENCODING);
-		}
+	public String getOutput() throws UnsupportedEncodingException {
+	    // if something has been written with the writer - we need to flush
+	    // it!
+	    printWriter.flush();
+	    return byteArrayOutputStream.toString(SERVLET_AND_JSP_ENCODING);
 	}
+    }
 
-	/**
-	 * If the markup container is a jsp or a servlet
-	 */
-	private enum Type {
-		JSP, SERVLET
-	}
+    /**
+     * If the markup container is a jsp or a servlet
+     */
+    private enum Type {
+	JSP, JSF, SERVLET
+    }
 
 }
