0e496c52d64b03f9322437cc3f8f08ad9631d674
davids <patrick.davids@nuboit.de>
2014-09-16 17:16:23 +0200

    calling DateUtils.UTC static, again / removed TODO

--- wicket-kendo-ui/src/main/java/com/googlecode/wicket/kendo/ui/scheduler/Scheduler.java
+++ wicket-kendo-ui/src/main/java/com/googlecode/wicket/kendo/ui/scheduler/Scheduler.java
@@ -18,13 +18,13 @@ package com.googlecode.wicket.kendo.ui.scheduler;
 
 import java.util.Calendar;
 import java.util.Date;
-import java.util.TimeZone;
 
 import org.apache.wicket.ajax.AjaxRequestTarget;
 
 import com.googlecode.wicket.jquery.core.JQueryBehavior;
 import com.googlecode.wicket.jquery.core.JQueryContainer;
 import com.googlecode.wicket.jquery.core.Options;
+import com.googlecode.wicket.jquery.core.utils.DateUtils;
 import com.googlecode.wicket.kendo.ui.scheduler.resource.ResourceList;
 import com.googlecode.wicket.kendo.ui.scheduler.resource.ResourceListModel;
 import com.googlecode.wicket.kendo.ui.scheduler.views.SchedulerViewType;
@@ -273,8 +273,7 @@ public class Scheduler extends JQueryContainer implements ISchedulerListener
 			@Override
 			protected void setEndDate(SchedulerModel model, Date date)
 			{
-				// Calendar calendar = Calendar.getInstance(DateUtils.UTC);
-				Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("UTC"));// TODO investigate; getting static from DateUtils ran into NoSuchFieldException UTC, dont know why / Patrick
+				Calendar calendar = Calendar.getInstance(DateUtils.UTC);
 
 				calendar.setTime(date);
 				calendar.set(Calendar.HOUR_OF_DAY, 23); // add ?
