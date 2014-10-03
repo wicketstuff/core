a9f53585025aa393b62a5d2050900e7e9816683c
Guillaume Smet <guillaume.smet@gmail.com>
2014-10-03 10:05:10 +0200

    Upgrade Fast to 1.61

--- jdk-1.6-parent/serializer-fast/src/test/java/org/wicketstuff/pageserializer/fast/FastSerializerTest.java
+++ jdk-1.6-parent/serializer-fast/src/test/java/org/wicketstuff/pageserializer/fast/FastSerializerTest.java
@@ -66,7 +66,7 @@ public class FastSerializerTest
 		Assert.assertNotNull("The produced data should not be null!", data);
 
 		// data length can fluctuate based on the object field values
-		Assert.assertEquals("The produced data length is not correct!", 370, data.length);
+		Assert.assertEquals("The produced data length is not correct!", 373, data.length);
 
 		Object object = pageSerializer.deserialize(data);
 		Assert.assertTrue(
@@ -104,7 +104,7 @@ public class FastSerializerTest
 	}
 	
 	@Test(expected=FastWicketSerialException.class)
-	public void notSerializableCompontentThrowsException()
+	public void notSerializableComponentThrowsException()
 	{
 		
 		NotSerializablePage page = tester.startPage(NotSerializablePage.class,
