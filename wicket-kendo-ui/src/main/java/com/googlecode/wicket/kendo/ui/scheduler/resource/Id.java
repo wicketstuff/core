2f99b65e9d3198cdf925661281d3fea6431a8cc1
davids <patrick.davids@nuboit.de>
2014-10-17 16:17:10 +0200

    implemented equals and hashCode for Resource/Id to enable them be used in Choices

--- wicket-kendo-ui/src/main/java/com/googlecode/wicket/kendo/ui/scheduler/resource/Id.java
+++ wicket-kendo-ui/src/main/java/com/googlecode/wicket/kendo/ui/scheduler/resource/Id.java
@@ -159,4 +159,31 @@ public class Id<T> implements IClusterable
 			return String.valueOf(this.get());
 		}
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Id other = (Id) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
 }
--- wicket-kendo-ui/src/main/java/com/googlecode/wicket/kendo/ui/scheduler/resource/Resource.java
+++ wicket-kendo-ui/src/main/java/com/googlecode/wicket/kendo/ui/scheduler/resource/Resource.java
@@ -189,4 +189,31 @@ public class Resource implements Serializable
 
 		return value.toString();
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Resource other = (Resource) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
 }
