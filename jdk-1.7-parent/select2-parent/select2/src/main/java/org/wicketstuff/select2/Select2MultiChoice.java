fa77e2ec77da7abe775eda857667adb5e537fab5
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2014-11-25 11:53:42 +0100

    [select2] Add support for JSON IDs
    Merges https://github.com/ivaynberg/wicket-select2/pull/87

--- jdk-1.7-parent/select2-parent/select2/src/main/java/org/wicketstuff/select2/Select2MultiChoice.java
+++ jdk-1.7-parent/select2-parent/select2/src/main/java/org/wicketstuff/select2/Select2MultiChoice.java
@@ -61,7 +61,8 @@ public class Select2MultiChoice<T> extends AbstractSelect2Choice<T, Collection<T
 			choices = new ArrayList<>();
 		} else {
 			if (isAjax()) {
-				choices = getProvider().toChoices(Arrays.asList(input.split(",")));
+				List<String> ids = splitInput( input );
+				choices = getProvider().toChoices( ids );
 			} else {
 				choices = new ArrayList<>();
 				String[] data = input.split(",");
@@ -139,6 +140,37 @@ public class Select2MultiChoice<T> extends AbstractSelect2Choice<T, Collection<T
 					OnDomReadyHeaderItem.forScript(JQuery.execute("$('#%s').select2('data', %s);", getJquerySafeMarkupId(),
 							selection.toJson())));
 		}
+	}
+
+	static List<String> splitInput( String input ) {
+		if (input.startsWith("{") && input.endsWith("}")) {
+			// Assume we're using JSON IDs
+			List<String> result = new ArrayList<>();
+
+			int openBracket = 0;
+			Integer lastStartIdx = null;
+			for (int i = 0; i < input.length(); i++) {
+				char c = input.charAt(i);
+				if (c == '{') {
+					openBracket++;
+					if (lastStartIdx == null) {
+						lastStartIdx = i;
+					}
+				}
+				if (c == '}') {
+					openBracket--;
+					if (openBracket == 0) {
+						String substring = input.substring(lastStartIdx, i + 1);
+						result.add(substring);
+						lastStartIdx = null;
+					}
+				}
+			}
+
+			return result;
+		}
+
+		return Arrays.asList(input.split( "," ));
 	}
 
 }
