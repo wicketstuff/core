f2739197cf053f1b43ab4c02e54f8e9110dea7fe
Mathias Born <mathias.born@signalimpuls.de>
2014-12-11 08:16:17 +0100

    Only use whitespaces

--- jdk-1.7-parent/gmap3-parent/gmap3-examples/src/main/java/org/wicketstuff/examples/gmap/bounds/HomePage.java
+++ jdk-1.7-parent/gmap3-parent/gmap3-examples/src/main/java/org/wicketstuff/examples/gmap/bounds/HomePage.java
@@ -21,41 +21,44 @@ public class HomePage extends WicketExamplePage
     {
         final GMap map = new GMap("topPanel");
         List<GLatLng> markersToShow = new ArrayList<GLatLng>();
-        
+
         // add some markers
         GLatLng glatlng1 = new GLatLng(47.4915285, 8.2050407);
         GMarkerOptions opts1 = new GMarkerOptions(map, glatlng1);
         GMarker marker1 = new GMarker("marker1", opts1);
         map.addOverlay(marker1);
-        
+
         GLatLng glatlng2 = new GLatLng(50.7706934, 2.2164129);
         GMarkerOptions opts2 = new GMarkerOptions(map, glatlng2);
         GMarker marker2 = new GMarker("marker2", opts2);
         map.addOverlay(marker2);
-        
+
         GLatLng glatlng3 = new GLatLng(48.858859, 2.34706);
         GMarkerOptions opts3 = new GMarkerOptions(map, glatlng3);
         GMarker marker3 = new GMarker("marker3", opts3);
         map.addOverlay(marker3);
-        
+
         // add them to the list of coordinates that we need in our viewport
         markersToShow.add(glatlng1);
         markersToShow.add(glatlng2);
         markersToShow.add(glatlng3);
         map.fitMarkers(markersToShow);
         add(map);
-        
-        //###############################################
+
+        // ###############################################
         // e.g. to center and fit zoom of an address
-        //###############################################
+        // ###############################################
         final GMap mapCenterAddress = new GMap("mapCenterAddress");
         Geocoder gecoder = new Geocoder();
-        try {
+        try
+        {
             gecoder.centerAndFitZoomForAdress(mapCenterAddress, "Frankfurt am Main");
-        } catch (Exception e) {
+        }
+        catch (Exception e)
+        {
             e.printStackTrace();
         }
         add(mapCenterAddress);
-        
+
     }
 }
--- jdk-1.7-parent/gmap3-parent/gmap3/src/main/java/org/wicketstuff/gmap/geocoder/Geocoder.java
+++ jdk-1.7-parent/gmap3-parent/gmap3/src/main/java/org/wicketstuff/gmap/geocoder/Geocoder.java
@@ -45,172 +45,169 @@ import org.wicketstuff.gmap.geocoder.pojos.NortheastSoutwestInfo;
 public class Geocoder implements Serializable
 {
 
-	private static final long serialVersionUID = 1L;
-	private static final Logger LOGGER = Logger.getLogger(Geocoder.class);
-	// Constants
-	public static final String OUTPUT_XML = "xml";
-	public static final String OUTPUT_JSON = "json";
-	private final String output = OUTPUT_JSON;
-	/**
-	 * Result-Object of an gecoder request
-	 */
-	private GeocoderResult geocoderResult;
-	private static ObjectMapper objectMapper;
+    private static final long serialVersionUID = 1L;
+    private static final Logger LOGGER = Logger.getLogger(Geocoder.class);
+    // Constants
+    public static final String OUTPUT_XML = "xml";
+    public static final String OUTPUT_JSON = "json";
+    private final String output = OUTPUT_JSON;
+    /**
+     * Result-Object of an gecoder request
+     */
+    private GeocoderResult geocoderResult;
+    private static ObjectMapper objectMapper;
 
-	static
-	{
+    static
+    {
+        objectMapper = new ObjectMapper();
+        objectMapper.configure(DeserializationConfig.Feature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    }
 
-		objectMapper = new ObjectMapper();
-		objectMapper.configure(DeserializationConfig.Feature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    public Geocoder()
+    {
+    }
 
+    public GLatLng decode(String response) throws GeocoderException, JSONException
+    {
+        try
+        {
+            geocoderResult = objectMapper.readValue(response, GeocoderResult.class);
+        }
+        catch (JsonParseException e)
+        {
+            LOGGER.error(e);
+        }
+        catch (JsonMappingException e)
+        {
+            LOGGER.error(e);
+        }
+        catch (IOException e)
+        {
+            LOGGER.error(e);
+        }
 
-	}
+        GeocoderStatus status = geocoderResult.getStatus();
 
-	public Geocoder()
-	{
-	}
+        if (status != GeocoderStatus.OK)
+        {
+            throw new GeocoderException(status);
+        }
 
-	public GLatLng decode(String response) throws GeocoderException, JSONException
-	{
-		try
-		{
-			geocoderResult = objectMapper.readValue(response, GeocoderResult.class);
-		}
-		catch (JsonParseException e)
-		{
-			LOGGER.error(e);
-		}
-		catch (JsonMappingException e)
-		{
-			LOGGER.error(e);
-		}
-		catch (IOException e)
-		{
-			LOGGER.error(e);
-		}
+        if (geocoderResult.getResults().length < 1)
+        {
+            throw new RuntimeException(); // TODO: throw something better
+        }
 
-		GeocoderStatus status = geocoderResult.getStatus();
+        return geocoderResult.getResults()[0].getGeometry().getLocation();
 
-		if (status != GeocoderStatus.OK)
-		{
-			throw new GeocoderException(status);
-		}
+    }
 
-		if (geocoderResult.getResults().length < 1)
-		{
-			throw new RuntimeException(); // TODO: throw something better
-		}
+    /**
+     * builds the google geo-coding url
+     *
+     * @param address
+     * @return
+     */
+    public String encode(final String address)
+    {
+        StringBuilder sb = new StringBuilder("http://maps.googleapis.com/maps/api/geocode/");
+        sb.append(output);
+        sb.append("?");
+        sb.append("address=").append(urlEncode(address));
+        sb.append("&sensor=false");
+        return sb.toString();
+    }
 
-		return geocoderResult.getResults()[0].getGeometry().getLocation();
+    /**
+     * @param address
+     * @return
+     * @throws IOException
+     */
+    public GLatLng geocode(final String address) throws Exception
+    {
+        InputStream is = invokeService(encode(address));
+        if (is != null)
+        {
+            try
+            {
+                String content = org.apache.wicket.util.io.IOUtils.toString(is);
+                return decode(content);
+            }
+            finally
+            {
+                is.close();
+            }
+        }
+        return null;
+    }
 
-	}
+    /**
+     * fetches the url content
+     *
+     * @param address
+     * @return
+     * @throws IOException
+     */
+    protected InputStream invokeService(final String address) throws IOException
+    {
+        URL url = new URL(address);
+        return url.openStream();
+    }
 
-	/**
-	 * builds the google geo-coding url
-	 *
-	 * @param address
-	 * @return
-	 */
-	public String encode(final String address)
-	{
-		StringBuilder sb = new StringBuilder("http://maps.googleapis.com/maps/api/geocode/");
-		sb.append(output);
-		sb.append("?");
-		sb.append("address=").append(urlEncode(address));
-		sb.append("&sensor=false");
-		return sb.toString();
-	}
+    /**
+     * url-encode a value
+     *
+     * @param value
+     * @return
+     */
+    private String urlEncode(final String value)
+    {
+        try
+        {
+            return URLEncoder.encode(value, "UTF-8");
+        }
+        catch (UnsupportedEncodingException ex)
+        {
+            throw new RuntimeException(ex.getMessage());
+        }
+    }
 
-	/**
-	 * @param address
-	 * @return
-	 * @throws IOException
-	 */
-	public GLatLng geocode(final String address) throws Exception
-	{
-		InputStream is = invokeService(encode(address));
-		if (is != null)
-		{
-			try
-			{
-				String content = org.apache.wicket.util.io.IOUtils.toString(is);
-				return decode(content);
-			}
-			finally
-			{
-				is.close();
-			}
-		}
-		return null;
-	}
+    /**
+     * READY-ONLY Get the Result of the last geocoder Request
+     * 
+     * @return the result of the last geocoder request
+     */
+    public GeocoderResult getGecoderResult()
+    {
+        return geocoderResult;
+    }
 
-	/**
-	 * fetches the url content
-	 *
-	 * @param address
-	 * @return
-	 * @throws IOException
-	 */
-	protected InputStream invokeService(final String address) throws IOException
-	{
-		URL url = new URL(address);
-		return url.openStream();
-	}
-
-	/**
-	 * url-encode a value
-	 *
-	 * @param value
-	 * @return
-	 */
-	private String urlEncode(final String value)
-	{
-		try
-		{
-			return URLEncoder.encode(value, "UTF-8");
-		}
-		catch (UnsupportedEncodingException ex)
-		{
-			throw new RuntimeException(ex.getMessage());
-		}
-	}
-
-	/**
-	 * READY-ONLY Get the Result of the last geocoder Request
-	 * 
-	 * @return the result of the last geocoder request
-	 */
-	public GeocoderResult getGecoderResult()
-	{
-		return geocoderResult;
-	}
-
-	/**
-	 * Convenience method to center and fit the zoom for an address, on the given map.
-	 * <p>
-	 * <b>Example:</b>
-	 * 
-	 * <pre>
-	 * GMap myMap = GMap(&quot;wicketId&quot;);
-	 * new Geocoder().centerAndFitZoomForAdress(myMap, &quot;Frankfurt am Main&quot;);
-	 * </pre>
-	 * 
-	 * <b>Result:</b><br/>
-	 * Frankfurt is centered and the zoom is suitable
-	 * </p>
-	 * 
-	 * @param map
-	 *            - the map where the address should shown
-	 * @param address
-	 *            - address as string for the google geocoder
-	 * @throws Exception
-	 */
-	public void centerAndFitZoomForAdress(GMap map, String address) throws Exception
-	{
-		this.geocode(address);
-		NortheastSoutwestInfo port = getGecoderResult().getResults()[0].getGeometry().getViewport();
-		GLatLng sw = new GLatLng(port.getSouthwest().getLat(), port.getSouthwest().getLng());
-		GLatLng ne = new GLatLng(port.getNortheast().getLat(), port.getNortheast().getLng());
-		map.setBounds(new GLatLngBounds(sw, ne));
-	}
+    /**
+     * Convenience method to center and fit the zoom for an address, on the given map.
+     * <p>
+     * <b>Example:</b>
+     * 
+     * <pre>
+     * GMap myMap = GMap(&quot;wicketId&quot;);
+     * new Geocoder().centerAndFitZoomForAdress(myMap, &quot;Frankfurt am Main&quot;);
+     * </pre>
+     * 
+     * <b>Result:</b><br/>
+     * Frankfurt is centered and the zoom is suitable
+     * </p>
+     * 
+     * @param map
+     *            - the map where the address should shown
+     * @param address
+     *            - address as string for the google geocoder
+     * @throws Exception
+     */
+    public void centerAndFitZoomForAdress(GMap map, String address) throws Exception
+    {
+        this.geocode(address);
+        NortheastSoutwestInfo port = getGecoderResult().getResults()[0].getGeometry().getViewport();
+        GLatLng sw = new GLatLng(port.getSouthwest().getLat(), port.getSouthwest().getLng());
+        GLatLng ne = new GLatLng(port.getNortheast().getLat(), port.getNortheast().getLng());
+        map.setBounds(new GLatLngBounds(sw, ne));
+    }
 }
