b97d7c68fe44bd6cb7a53f8636f3462ba5550cfc
Andrea Del Bene <adelbene@apache.org>
2015-01-31 15:28:49 +0100

    porting of changes from master branch

--- jdk-1.6-parent/stateless-parent/stateless-examples/src/main/java/org/wicketstuff/stateless/demo/HomePage.java
+++ jdk-1.6-parent/stateless-parent/stateless-examples/src/main/java/org/wicketstuff/stateless/demo/HomePage.java
@@ -1,7 +1,24 @@
+/**
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package org.wicketstuff.stateless.demo;
 
 import java.util.Arrays;
 
+import org.apache.wicket.Component;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.devutils.stateless.StatelessComponent;
 import org.apache.wicket.markup.html.WebPage;
@@ -11,13 +28,14 @@ import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.StatelessForm;
 import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.markup.html.link.Link;
+import org.apache.wicket.markup.html.panel.FeedbackPanel;
 import org.apache.wicket.model.AbstractReadOnlyModel;
 import org.apache.wicket.model.Model;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.apache.wicket.util.string.StringValue;
 import org.wicketstuff.stateless.StatelessAjaxFallbackLink;
 import org.wicketstuff.stateless.StatelessAjaxFormComponentUpdatingBehavior;
-import org.wicketstuff.stateless.StatelessAjaxSubmittingLink;
+import org.wicketstuff.stateless.StatelessAjaxSubmitLink;
 
 /**
  * For testing only
@@ -51,39 +69,38 @@ public class HomePage extends WebPage {
 			public void onClick(final AjaxRequestTarget target) {
 				if (target != null) {
 					Integer counter = (Integer) c2.getDefaultModelObject();
-					System.out.println("label: " + counter);
 					updateParams(getPageParameters(), counter);
 					target.add(c2);
 				}
 			}
 		};
 
-		c2.setMarkupId(c2.getId()); // Make the id stable for each page instance
 		add(c2Link);
-		add(c2);
+		add(c2.setOutputMarkupId(true));
 
 		final String _a = getParameter(parameters, "a");
 		final String _b = getParameter(parameters, "b");
 		
-		final TextField<String> a = new TextField<String>("a",
+		final TextField<String> a = new TextField<String>("name",
 			new Model<String>(_a));
-		final TextField<String> b = new TextField<String>("b",
+		final TextField<String> b = new TextField<String>("surname",
 			new Model<String>(_b));
 
 		final Form<String> form = new StatelessForm<String>("inputForm") {
 
 			@Override
 			protected void onSubmit() {
-				System.out.format("clicked sumbit: a = [%s], b = [%s]%n",
-						a.getModelObject(), b.getModelObject());
+				
 			}
 			
 		};
-		final DropDownChoice<String> c = new DropDownChoice<String>("c",
+		final DropDownChoice<String> select = new DropDownChoice<String>("select",
 				new Model<String>("2"), Arrays.asList(new String[] { "1", "2",
 						"3" }));
-
-		c.add(new StatelessAjaxFormComponentUpdatingBehavior("change") {
+		final Label selectedValue = new Label("selectedValue", "");
+		add(selectedValue.setOutputMarkupId(true));
+		
+		select.add(new StatelessAjaxFormComponentUpdatingBehavior("change") {
 
 			@Override
 			protected PageParameters getPageParameters() {
@@ -92,19 +109,42 @@ public class HomePage extends WebPage {
 
 			@Override
 			protected void onUpdate(final AjaxRequestTarget target) {
-				final String value = c.getModelObject();
-				System.out.println("xxxxxxxxxxxxxxxxxx: " + value);
+				final String value = select.getModelObject();
+				selectedValue.setDefaultModelObject("Selected value: " + value);
+				target.add(selectedValue);
 			}
 		});
-		c.setMarkupId("c");
-		form.add(a);
-		form.add(b);
 		
-		form.add(new StatelessAjaxSubmittingLink("submit"));
+		form.add(a.setRequired(true));
+		form.add(b.setRequired(true));
+		
+		final Component feedback = new FeedbackPanel("feedback");
+		final Label submittedValues = new Label("submittedValues", "");
+		
+		form.add(feedback.setOutputMarkupId(true));
+		form.add(submittedValues.setOutputMarkupId(true));
+		
+		form.add(new StatelessAjaxSubmitLink("submit"){
+			@Override
+			protected void onError(AjaxRequestTarget target)
+			{
+				super.onError(target);
+				target.add(feedback);
+			}
+			@Override
+			protected void onSubmit(AjaxRequestTarget target)
+			{		
+				super.onSubmit(target);
+				String values = "Your name is: " + a.getModelObject() +
+					" " + b.getModelObject();
+				submittedValues.setDefaultModelObject(values);
+				target.add(feedback, submittedValues);
+			}
+		});
 		
 		add(form);
 
-		add(c);
+		add(select);
 	}
 
 	private String getParameter(final PageParameters parameters,
