66609712ce22d30bcf202fef801142a5fdbf71c0
Andrea Del Bene <adelbene@apache.org>
2015-01-31 12:42:57 +0100

    Added Apache license and JavaDocs

--- jdk-1.7-parent/stateless-parent/stateless/src/main/java/org/wicketstuff/stateless/StatelessAjaxFormSubmitBehavior.java
+++ jdk-1.7-parent/stateless-parent/stateless/src/main/java/org/wicketstuff/stateless/StatelessAjaxFormSubmitBehavior.java
@@ -1,3 +1,19 @@
+/**
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package org.wicketstuff.stateless;
 
 import org.apache.wicket.Component;
@@ -8,11 +24,20 @@ import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.IFormSubmitter;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 
+/**
+ * Stateless version of AjaxFormSubmitBehavior.
+ */
 public class StatelessAjaxFormSubmitBehavior extends StatelessAjaxEventBehavior
 {
 
+	/** The default form processing flag. */
 	private boolean defaultFormProcessing = true;
 
+	/**
+	 * Instantiates a new stateless ajax form submit behavior.
+	 *
+	 * @param event the event
+	 */
 	public StatelessAjaxFormSubmitBehavior(String event)
 	{
 		super(event);
@@ -48,8 +73,8 @@ public class StatelessAjaxFormSubmitBehavior extends StatelessAjaxEventBehavior
 	}
 	
 	/**
-	 * Finds form that will be submitted
-	 * 
+	 * Finds form that will be submitted.
+	 *
 	 * @return form to submit or {@code null} if none found
 	 */
 	protected Form<?> findForm()
@@ -72,13 +97,24 @@ public class StatelessAjaxFormSubmitBehavior extends StatelessAjaxEventBehavior
 		findForm().getRootForm().onFormSubmitted(new AjaxFormSubmitter(this, target));
 	}
 	
+	/**
+	 * The Class AjaxFormSubmitter.
+	 */
 	class AjaxFormSubmitter implements IFormSubmitter
 	{
 
+		/** The submit behavior. */
 		private final StatelessAjaxFormSubmitBehavior submitBehavior;
 		
+		/** The target. */
 		private final AjaxRequestTarget target;
 		
+		/**
+		 * Instantiates a new ajax form submitter.
+		 *
+		 * @param submitBehavior the submit behavior
+		 * @param target the target
+		 */
 		public AjaxFormSubmitter(StatelessAjaxFormSubmitBehavior submitBehavior,
 			AjaxRequestTarget target)
 		{
@@ -121,6 +157,8 @@ public class StatelessAjaxFormSubmitBehavior extends StatelessAjaxEventBehavior
 	/**
 	 * Override this method to provide special submit handling in a multi-button form. This method
 	 * will be called <em>after</em> the form's onSubmit method.
+	 *
+	 * @param target the target
 	 */
 	protected void onAfterSubmit(AjaxRequestTarget target)
 	{
@@ -129,25 +167,37 @@ public class StatelessAjaxFormSubmitBehavior extends StatelessAjaxEventBehavior
 	/**
 	 * Override this method to provide special submit handling in a multi-button form. This method
 	 * will be called <em>before</em> the form's onSubmit method.
+	 *
+	 * @param target the target
 	 */
 	protected void onSubmit(AjaxRequestTarget target)
 	{
 	}
 
 	/**
-	 * Listener method invoked when the form has been processed and errors occurred
-	 * 
-	 * @param target
+	 * Listener method invoked when the form has been processed and errors occurred.
+	 *
+	 * @param target the target
 	 */
 	protected void onError(AjaxRequestTarget target)
 	{
 	}
 	
+	/**
+	 * Gets the default form processing.
+	 *
+	 * @return the default form processing
+	 */
 	public boolean getDefaultFormProcessing()
 	{
 		return defaultFormProcessing ;
 	}
 
+	/**
+	 * Sets the default form processing.
+	 *
+	 * @param defaultFormProcessing the new default form processing
+	 */
 	public void setDefaultFormProcessing(boolean defaultFormProcessing)
 	{
 		this.defaultFormProcessing = defaultFormProcessing;
