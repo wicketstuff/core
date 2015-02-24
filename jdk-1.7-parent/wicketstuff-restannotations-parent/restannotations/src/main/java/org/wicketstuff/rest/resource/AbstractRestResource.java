27dc522b53b2eff0d51edd77a4ab3d88e82be6cf
Andrea Del Bene <adelbene@apache.org>
2015-02-24 11:09:15 +0100

    Issue #384

--- jdk-1.7-parent/wicketstuff-restannotations-parent/restannotations/src/main/java/org/wicketstuff/rest/resource/AbstractRestResource.java
+++ jdk-1.7-parent/wicketstuff-restannotations-parent/restannotations/src/main/java/org/wicketstuff/rest/resource/AbstractRestResource.java
@@ -599,15 +599,26 @@ public abstract class AbstractRestResource<T extends IWebSerialDeserial> impleme
 		{
 			return method.invoke(this, parametersValues.toArray());
 		}
-		catch (Exception e)
+		catch (Exception exception)
 		{
-			response.setStatus(500);
-			response.write("General server error.");
+			handleException(response, exception);
 
 			log.debug("Error invoking method '" + method.getName() + "'");
 		}
 
 		return null;
+	}
+	
+	/**
+     * Handle Exception. Default: set response Status to 500. Override this method to implement
+     * customized error handling
+     * @param exception The Exception
+     * @param response Response-Object
+     */
+	protected void handleException(WebResponse response, Exception exception)
+	{
+		response.setStatus(500);
+		response.write("General server error.");
 	}
 
 	/**
