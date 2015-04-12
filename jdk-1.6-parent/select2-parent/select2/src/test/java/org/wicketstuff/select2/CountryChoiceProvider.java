1e7b74fc763156d55fb39791fd391233ddaaf89e
Alexander Morozov <alexander.v.morozov@gmail.com>
2015-04-12 13:29:50 +0600

    - Added Select2ChoiceTest that reproduces issue with loosing value by Select2Choice after form submit. - Override convertInput() and convertValue() in order to convert and process properly Select2Choice validation phase. - Override getInputAsArray() for Select2MultiChoice in order to satisfy FormComponent's contract. Implement convertValue() with ChoiceProvider.

---
+++ jdk-1.6-parent/select2-parent/select2/src/test/java/org/wicketstuff/select2/CountryChoiceProvider.java
@@ -0,0 +1,65 @@
+package org.wicketstuff.select2;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.apache.wicket.ajax.json.JSONException;
+import org.apache.wicket.ajax.json.JSONWriter;
+
+/**
+ * @author lexx
+ */
+public final class CountryChoiceProvider extends ChoiceProvider<Country>
+{
+
+	private static final int PAGE_SIZE = 5;
+
+	@Override
+	public void query(String term, int page, Response<Country> response)
+	{
+		response.addAll(countryMatches(term, page, PAGE_SIZE));
+		response.setHasMore(response.size() == PAGE_SIZE);
+	}
+
+	@Override
+	public void toJson(Country choice, JSONWriter writer) throws JSONException
+	{
+		writer.key("id").value(choice.name()).key("text").value(choice.getDisplayName());
+	}
+
+	@Override
+	public Collection<Country> toChoices(Collection<String> ids)
+	{
+		ArrayList<Country> countries = new ArrayList<Country>(PAGE_SIZE);
+		for (String id : ids)
+		{
+			countries.add(Country.valueOf(id));
+		}
+		return countries;
+	}
+
+	private static List<Country> countryMatches(String term, int page, int pageSize)
+	{
+		List<Country> result = new ArrayList<Country>(PAGE_SIZE);
+		int offset = page * pageSize;
+		int matched = 0;
+		for (Country country : Country.values())
+		{
+			if (result.size() == pageSize)
+			{
+				break;
+			}
+			if (country.getDisplayName().toUpperCase().contains(term.toUpperCase()))
+			{
+				matched++;
+				if (matched > offset)
+				{
+					result.add(country);
+				}
+			}
+		}
+		return result;
+	}
+
+}
