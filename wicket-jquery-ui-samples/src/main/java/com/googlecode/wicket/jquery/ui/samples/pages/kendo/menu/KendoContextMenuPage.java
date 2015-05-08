428afa5582ec5b86f9524fd00f86c7db472109aa
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2015-05-08 09:09:59 +0300

    Issue #165 Set a target for the context menu so it doesn't open on the whole document/window

--- wicket-jquery-ui-samples/src/main/java/com/googlecode/wicket/jquery/ui/samples/pages/kendo/menu/KendoContextMenuPage.java
+++ wicket-jquery-ui-samples/src/main/java/com/googlecode/wicket/jquery/ui/samples/pages/kendo/menu/KendoContextMenuPage.java
@@ -2,6 +2,8 @@ package com.googlecode.wicket.jquery.ui.samples.pages.kendo.menu;
 
 import org.apache.wicket.ajax.AjaxRequestTarget;
 
+import com.googlecode.wicket.jquery.core.JQueryBehavior;
+import com.googlecode.wicket.jquery.core.Options;
 import com.googlecode.wicket.kendo.ui.widget.menu.ContextMenu;
 import com.googlecode.wicket.kendo.ui.widget.menu.item.IMenuItem;
 
@@ -15,6 +17,13 @@ public class KendoContextMenuPage extends AbstractMenuPage
 		this.add(new ContextMenu("menu", KendoMenuPage.newMenuItemList()) {
 
 			private static final long serialVersionUID = 1L;
+
+			@Override
+			public void onConfigure(JQueryBehavior behavior) {
+				super.onConfigure(behavior);
+
+				behavior.setOption("target", Options.asString("#menu-target"));
+			}
 
 			@Override
 			public void onClick(AjaxRequestTarget target, IMenuItem item)
