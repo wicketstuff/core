82e62b5560ed301b7660f31b7c8eb27df383f808
JoachimRohde <webmaster@joachimrohde.com>
2016-06-27 14:34:05 +0200

    [GMap3] Usage of API key is now supported and encouraged
    Since 22th June 2016 Google Maps requires an API-key, see http://googlegeodevelopers.blogspot.de/2016/06/building-for-scale-updates-to-google.html

--- gmap3-parent/gmap3/src/main/java/org/wicketstuff/gmap/GMapHeaderContributor.java
+++ gmap3-parent/gmap3/src/main/java/org/wicketstuff/gmap/GMapHeaderContributor.java
@@ -10,12 +10,21 @@ public class GMapHeaderContributor extends Behavior
 
     private static final long serialVersionUID = 1L;
     // URL for Google Maps' API endpoint.
-    private static final String GMAP_API_URL = "%s://maps.google.com/maps/api/js?v=3&amp";
+    private static final String GMAP_API_URL = "%s://maps.googleapis.com/maps/api/js?v=3&";
     private static final String HTTP = "http";
     // We have some custom Javascript.
     private String scheme;
     private String sensor = "false";
+    private String apiKey = null;
 
+    /**
+     * @deprecated since 22th June 2016 Google Maps requires an API-key,
+     * therefore you should use
+     * {@link #GMapHeaderContributor(java.lang.String, java.lang.String) }
+     * instead of this constructor
+     * @see
+     * http://googlegeodevelopers.blogspot.de/2016/06/building-for-scale-updates-to-google.html
+     */
     public GMapHeaderContributor()
     {
         this(HTTP, false);
@@ -32,6 +41,14 @@ public class GMapHeaderContributor extends Behavior
         this(HTTP, sensor);
     }
 
+    /**
+     * @deprecated since 22th June 2016 Google Maps requires an API-key,
+     * therefore you should use
+     * {@link #GMapHeaderContributor(java.lang.String, java.lang.String) }
+     * instead of this constructor
+     * @see
+     * http://googlegeodevelopers.blogspot.de/2016/06/building-for-scale-updates-to-google.html
+     */
     public GMapHeaderContributor(final String scheme)
     {
         this(scheme, false);
@@ -44,10 +61,10 @@ public class GMapHeaderContributor extends Behavior
      *
      * @param scheme http or https?
      * @param sensor this parameter will be ignored
-     * 
+     *
      * @deprecated Since the sensor-parameter is no longer required from Google
-     * you should use {@link #GMapHeaderContributor(java.lang.String) } instead of this
-     * constructor
+     * you should use {@link #GMapHeaderContributor(java.lang.String) } instead
+     * of this constructor
      */
     public GMapHeaderContributor(final String scheme, final boolean sensor)
     {
@@ -58,12 +75,28 @@ public class GMapHeaderContributor extends Behavior
         }
     }
 
+    
+    /**
+     * @param scheme http or https?
+     * @param apiKey your Google Maps API-key
+     */
+    public GMapHeaderContributor(final String scheme, final String apiKey)
+    {
+        this.scheme = scheme;
+        this.apiKey = apiKey;
+    }
+
     @Override
     public void renderHead(Component component, IHeaderResponse response)
     {
         super.renderHead(component, response);
         response.render(JavaScriptHeaderItem.forReference(WicketGMapJsReference.INSTANCE));
-        response.render(JavaScriptHeaderItem.forUrl(String.format(GMAP_API_URL, scheme)));
+        String url = String.format(GMAP_API_URL, scheme);
+        if (apiKey != null)
+        {
+            url = url + "apiKey=" + apiKey;            
+        }
+        response.render(JavaScriptHeaderItem.forUrl(url));
     }
 
     /**
@@ -74,4 +107,5 @@ public class GMapHeaderContributor extends Behavior
     {
         return sensor;
     }
+    
 }
