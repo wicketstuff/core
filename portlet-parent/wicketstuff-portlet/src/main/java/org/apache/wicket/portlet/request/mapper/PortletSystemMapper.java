e63da3069fb20e8e3db0a645acf46d047aab0d11
kkaravitis <kkaravitis@gmail.com>
2016-07-07 19:06:06 +0300

    #523

--- portlet-parent/wicketstuff-portlet/src/main/java/org/apache/wicket/portlet/request/mapper/PortletSystemMapper.java
+++ portlet-parent/wicketstuff-portlet/src/main/java/org/apache/wicket/portlet/request/mapper/PortletSystemMapper.java
@@ -1,13 +1,19 @@
 package org.apache.wicket.portlet.request.mapper;
 
+import java.util.Iterator;
+
 import org.apache.wicket.Application;
+import org.apache.wicket.Page;
 import org.apache.wicket.SystemMapper;
 import org.apache.wicket.core.request.mapper.BookmarkableMapper;
+import org.apache.wicket.core.request.mapper.HomePageMapper;
+import org.apache.wicket.core.request.mapper.MountedMapper;
+import org.apache.wicket.protocol.http.WebApplication;
 import org.apache.wicket.request.IRequestHandler;
 import org.apache.wicket.request.IRequestMapper;
 import org.apache.wicket.request.Url;
-import org.apache.wicket.core.request.mapper.HomePageMapper;
 import org.apache.wicket.request.component.IRequestablePage;
+import org.apache.wicket.request.mapper.CompoundRequestMapper;
 import org.apache.wicket.request.mapper.info.PageInfo;
 import org.apache.wicket.request.mapper.parameter.IPageParametersEncoder;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
@@ -26,12 +32,59 @@ public class PortletSystemMapper extends SystemMapper
 
 	private MapperDelegate delegate = new MapperDelegate();
 
+	
+	/**
+	 * Mounts a page class of the application to the given path.
+	 * @param <T>
+	 * 
+	 * @param <T>
+	 *            type of page
+	 * 
+	 * @param application
+	 * 			
+	 * @param path
+	 *            the path to mount the page class on
+	 * @param pageClass
+	 *            the page class to be mounted
+	 */
+	public static <T extends Page> MountedMapper mountPage(final WebApplication application, final String path, final Class<T> pageClass)
+	{
+		MountedMapper mapper = new MountedMapper(path, pageClass) {
+			private MapperDelegate delegate = new MapperDelegate();
+			
+			@Override
+			protected IRequestHandler processHybrid(PageInfo pageInfo,
+					Class<? extends IRequestablePage> pageClass,
+					PageParameters pageParameters, Integer renderCount) {
+				
+				return delegate.processHybrid(pageInfo, pageClass, pageParameters, renderCount);
+			}
+		};
+		application.mount(mapper);
+		
+		return mapper;
+	}
+	
 	/**
 	 * @param application
 	 */
 	public PortletSystemMapper(Application application)
 	{
 		super(application);
+		addMountedMappers(application);
+	}
+	
+	protected void addMountedMappers(Application application) {
+		IRequestMapper rootMapper = application.getRootRequestMapper();
+		if (rootMapper instanceof CompoundRequestMapper) {
+			Iterator<IRequestMapper> iterator = ((CompoundRequestMapper)rootMapper).iterator();
+			while (iterator.hasNext()) {
+				IRequestMapper requestMapper = iterator.next();
+				if (requestMapper instanceof MountedMapper && !(requestMapper instanceof HomePageMapper)) {
+					add(requestMapper);
+				}
+			}
+		}
 	}
 
 	/**
