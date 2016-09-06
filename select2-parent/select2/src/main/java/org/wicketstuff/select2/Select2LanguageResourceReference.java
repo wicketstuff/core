24c15adfe6d91aac28a717f1306695891a5bc434
Tom Götz <tom@decoded.de>
2016-09-06 08:19:17 +0200

    [select2] Adding check if configured/requested i18n file exists (with fallback to "en")

--- select2-parent/select2/src/main/java/org/wicketstuff/select2/Select2LanguageResourceReference.java
+++ select2-parent/select2/src/main/java/org/wicketstuff/select2/Select2LanguageResourceReference.java
@@ -10,11 +10,33 @@ import org.apache.wicket.request.resource.JavaScriptResourceReference;
  */
 class Select2LanguageResourceReference extends JavaScriptResourceReference {
 
+    private static final String resourceName = "res/js/i18n/%s.js";
+    private static final String defaultLanguage = "en";
+
     /**
-     * @param language code to load
+     * @param language i18n file to load (e.g. "en", "de", "fr" ...)
      */
     Select2LanguageResourceReference(String language) {
-        super(Select2LanguageResourceReference.class, String.format("res/js/i18n/%s.js", language));
+        super(Select2LanguageResourceReference.class, getResourceName(language));
+    }
+
+    /**
+     * Returns the resource name of the i18n file, uses a fallback to defaultLanguage
+     * if requested resource file does not exist
+     *
+     * @param language i18n file to load
+     * @return resource name
+     */
+    private static String getResourceName(String language) {
+        try {
+            String name = String.format(resourceName, language);
+            if (Select2LanguageResourceReference.class.getResource(name) != null) {
+                return name;
+            }
+        } catch (Exception ignore) {
+            // noop
+        }
+        return String.format(resourceName, defaultLanguage);
     }
 
 }
