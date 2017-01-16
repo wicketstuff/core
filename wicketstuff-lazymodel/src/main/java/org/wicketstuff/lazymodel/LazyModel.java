5ce7d2d213d9e29f4577dde51cf24baf8258d18b
Sven Meier <svenmeier@apache.org>
2017-01-16 07:11:30 +0100

    issue #569 getObject's return might be a type variable

--- wicketstuff-lazymodel/src/main/java/org/wicketstuff/lazymodel/LazyModel.java
+++ wicketstuff-lazymodel/src/main/java/org/wicketstuff/lazymodel/LazyModel.java
@@ -568,7 +568,7 @@ public class LazyModel<T> implements IModel<T>, IObjectClassAwareModel<T>,
 		}
 
 		Type type = getType(target);
-		if (type instanceof TypeVariable<?>) {
+		if (type == null) {
 			throw new WicketRuntimeException("cannot detect target type");
 		}
 		Evaluation<T> evaluation = new BoundEvaluation<T>(type, target);
@@ -593,8 +593,11 @@ public class LazyModel<T> implements IModel<T>, IObjectClassAwareModel<T>,
 			try {
 				type = model.getClass().getMethod("getObject")
 						.getGenericReturnType();
-			} catch (Exception ex) {
-				throw new WicketRuntimeException(ex);
+			} catch (Exception fallThrough) {
+			}
+			
+			if (type instanceof TypeVariable) {
+				type = Reflection.resultType(model.getClass(), type);
 			}
 		}
 
