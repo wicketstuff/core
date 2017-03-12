df4862780f376cb45dcd0a499e4c57fa3443673a
sbriquet <sbriquet@apache.org>
2017-03-12 12:27:05 +0100

    Core: added FeedbackUtils#reload

--- wicket-jquery-ui-core/src/main/java/com/googlecode/wicket/jquery/core/utils/FeedbackUtils.java
+++ wicket-jquery-ui-core/src/main/java/com/googlecode/wicket/jquery/core/utils/FeedbackUtils.java
@@ -21,6 +21,7 @@ import java.io.Serializable;
 import org.apache.wicket.Component;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.feedback.FeedbackMessage;
+import org.apache.wicket.markup.html.panel.FeedbackPanel;
 import org.apache.wicket.protocol.http.WebSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -28,7 +29,27 @@ import org.slf4j.LoggerFactory;
 import com.googlecode.wicket.jquery.core.ajax.FeedbackPayload;
 
 /**
- * Utility class for handling feedback session messages and feedback ajax messages
+ * Utility class for handling feedback session messages and feedback ajax messages. The hosting page should implement a code like:<br>
+ * 
+ * <pre>
+ * <code>
+ * public void onEvent(IEvent&lt;?&gt; event)
+ * {
+ *     super.onEvent(event);
+ * 
+ *     if (event.getPayload() instanceof FeedbackPayload)
+ *     {
+ *         FeedbackPayload payload = (FeedbackPayload) event.getPayload();
+ * 
+ *         if (payload.getLevel() == FeedbackMessage.INFO)
+ *         {
+ *             this.info(payload.getMessage());
+ *             payload.getTarget().add(this.feedbackPanel);
+ *         }
+ *     }
+ * }
+ * </code>
+ * </pre>
  * 
  * @author Sebastien Briquet - sebfz1
  *
@@ -114,43 +135,129 @@ public class FeedbackUtils
 
 	// Ajax based //
 
+	/**
+	 * Sends an ajax {@link FeedbackMessage#DEBUG} message to the hosting page
+	 * 
+	 * @param component the {@link Component} sending the event
+	 * @param target the {@link AjaxRequestTarget}
+	 * @param message the message
+	 */
 	public static void debug(Component component, AjaxRequestTarget target, String message)
 	{
 		BroadcastUtils.breadth(component.getPage(), new FeedbackPayload(target, FeedbackMessage.DEBUG, message));
 	}
 
+	/**
+	 * Sends an ajax {@link FeedbackMessage#INFO} message to the hosting page
+	 * 
+	 * @param component the {@link Component} sending the event
+	 * @param target the {@link AjaxRequestTarget}
+	 * @param message the message
+	 */
 	public static void info(Component component, AjaxRequestTarget target, String message)
 	{
 		BroadcastUtils.breadth(component.getPage(), new FeedbackPayload(target, FeedbackMessage.INFO, message));
 	}
 
+	/**
+	 * Sends an ajax {@link FeedbackMessage#SUCCESS} message to the hosting page
+	 * 
+	 * @param component the {@link Component} sending the event
+	 * @param target the {@link AjaxRequestTarget}
+	 * @param message the message
+	 */
 	public static void success(Component component, AjaxRequestTarget target, String message)
 	{
 		BroadcastUtils.breadth(component.getPage(), new FeedbackPayload(target, FeedbackMessage.SUCCESS, message));
 	}
 
+	/**
+	 * Sends an ajax {@link FeedbackMessage#WARNING} message to the hosting page
+	 * 
+	 * @param component the {@link Component} sending the event
+	 * @param target the {@link AjaxRequestTarget}
+	 * @param message the message
+	 */
 	public static void warn(Component component, AjaxRequestTarget target, String message)
 	{
 		BroadcastUtils.breadth(component.getPage(), new FeedbackPayload(target, FeedbackMessage.WARNING, message));
 	}
 
-	public static void error(Component component, AjaxRequestTarget target, Exception e)
+	/**
+	 * Sends an ajax {@link FeedbackMessage#ERROR} exception message to the hosting page
+	 * 
+	 * @param component the {@link Component} sending the event
+	 * @param target the {@link AjaxRequestTarget}
+	 * @param exception the {@link Exception}
+	 */
+	public static void error(Component component, AjaxRequestTarget target, Exception exception)
 	{
-		FeedbackUtils.error(component, target, e.getMessage());
+		FeedbackUtils.error(component, target, exception.getMessage());
 	}
 
+	/**
+	 * Sends an ajax {@link FeedbackMessage#ERROR} message to the hosting page
+	 * 
+	 * @param component the {@link Component} sending the event
+	 * @param target the {@link AjaxRequestTarget}
+	 * @param message the message
+	 */
 	public static void error(Component component, AjaxRequestTarget target, String message)
 	{
 		BroadcastUtils.breadth(component.getPage(), new FeedbackPayload(target, FeedbackMessage.ERROR, message));
 	}
 
-	public static void fatal(Component component, AjaxRequestTarget target, Exception e)
+	/**
+	 * Sends an ajax {@link FeedbackMessage#FATAL} exception message to the hosting page
+	 * 
+	 * @param component the {@link Component} sending the event
+	 * @param target the {@link AjaxRequestTarget}
+	 * @param exception the {@link Exception}
+	 */
+	public static void fatal(Component component, AjaxRequestTarget target, Exception exception)
 	{
-		FeedbackUtils.fatal(component, target, e.getMessage());
+		FeedbackUtils.fatal(component, target, exception.getMessage());
 	}
 
+	/**
+	 * Sends an ajax {@link FeedbackMessage#FATAL} message to the hosting page
+	 * 
+	 * @param component the {@link Component} sending the event
+	 * @param target the {@link AjaxRequestTarget}
+	 * @param message the message
+	 */
 	public static void fatal(Component component, AjaxRequestTarget target, String message)
 	{
 		BroadcastUtils.breadth(component.getPage(), new FeedbackPayload(target, FeedbackMessage.FATAL, message));
+	}
+
+	/**
+	 * Aims the reload a {@link FeedbackPanel}. The hosting page should implement a code like:<br>
+	 * 
+	 * <pre>
+	 * <code>
+	 * public void onEvent(IEvent&lt;?&gt; event)
+	 * {
+	 *     super.onEvent(event);
+	 * 
+	 *     if (event.getPayload() instanceof FeedbackPayload)
+	 *     {
+	 *         FeedbackPayload payload = (FeedbackPayload) event.getPayload();
+	 * 
+	 *         if (payload.getLevel() == FeedbackMessage.UNDEFINED)
+	 *         {
+	 *             payload.getTarget().add(this.feedbackPanel);
+	 *         }
+	 *     }
+	 * }
+	 * </code>
+	 * </pre>
+	 * 
+	 * @param component the {@link Component} sending the event
+	 * @param target the {@link AjaxRequestTarget}
+	 */
+	public static void reload(Component component, AjaxRequestTarget target)
+	{
+		BroadcastUtils.breadth(component.getPage(), new FeedbackPayload(target));
 	}
 }
