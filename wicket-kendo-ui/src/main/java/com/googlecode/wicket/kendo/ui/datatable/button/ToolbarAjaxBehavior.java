7176532cb43eef368869b0e01b6603355aad0db7
sbriquet <sbriquet@apache.org>
2017-03-12 13:27:30 +0100

    Kendo UI: fixed ToolbarAjaxBehavior (dot before property name)

--- wicket-kendo-ui/src/main/java/com/googlecode/wicket/kendo/ui/datatable/button/ToolbarAjaxBehavior.java
+++ wicket-kendo-ui/src/main/java/com/googlecode/wicket/kendo/ui/datatable/button/ToolbarAjaxBehavior.java
@@ -86,8 +86,8 @@ public class ToolbarAjaxBehavior extends JQueryAjaxBehavior
 		if (property != null)
 		{
 			statement += "var $grid = jQuery(e.target).closest('.k-grid').data('kendoGrid');\n"; // lf
-			statement += "jQuery.each($grid.select(), function(index, row) { values.push($grid.dataItem(row)" + property + "); }"; // lf
-			statement += ");\n";
+			statement += "jQuery.each($grid.select(), function(index, row) { values.push($grid.dataItem(row)." + property + "); }"; // lf
+			statement += ");\n console.log('values', values);";
 		}
 
 		return statement + super.getCallbackFunctionBody(parameters);
