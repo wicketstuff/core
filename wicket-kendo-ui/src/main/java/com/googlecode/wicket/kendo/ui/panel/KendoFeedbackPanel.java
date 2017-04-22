fdb255dcb891776fbc9f821ce963df26f62f98cd
Sebastien Briquet <sbriquet@apache.org>
2017-04-22 11:31:51 +0200

    Kendo UI: fixed KendoFeedbackPanel (multiple msg are reprocessed)

--- wicket-kendo-ui/src/main/java/com/googlecode/wicket/kendo/ui/panel/KendoFeedbackPanel.java
+++ wicket-kendo-ui/src/main/java/com/googlecode/wicket/kendo/ui/panel/KendoFeedbackPanel.java
@@ -154,9 +154,14 @@ public class KendoFeedbackPanel extends WebMarkupContainer implements IJQueryWid
 	{
 		for (FeedbackMessage message : this.getModelObject())
 		{
-			this.widgetBehavior.show(handler, message.getMessage(), message.getLevelAsString());
-			message.markRendered();
+			if (!message.isRendered())
+			{
+				this.widgetBehavior.show(handler, message.getMessage(), message.getLevelAsString());
+				message.markRendered();
+			}
 		}
+
+		this.getModel().detach(); // forces the retrieval of next messages
 	}
 
 	/**
@@ -171,7 +176,7 @@ public class KendoFeedbackPanel extends WebMarkupContainer implements IJQueryWid
 		{
 			this.hide(handler);
 		}
-		
+
 		this.refresh(handler);
 	}
 
