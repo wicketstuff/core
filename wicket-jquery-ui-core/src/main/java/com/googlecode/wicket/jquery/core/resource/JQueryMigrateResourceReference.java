c54a9d58400467670b7c9baa2a0f8d0fcac73c74
sbriquet <sbriquet@apache.org>
2017-05-21 14:17:38 +0200

    JQueryMigrateResourceReference, minor update

--- wicket-jquery-ui-core/src/main/java/com/googlecode/wicket/jquery/core/resource/JQueryMigrateResourceReference.java
+++ wicket-jquery-ui-core/src/main/java/com/googlecode/wicket/jquery/core/resource/JQueryMigrateResourceReference.java
@@ -1,12 +1,12 @@
 package com.googlecode.wicket.jquery.core.resource;
 
-import java.util.Arrays;
 import java.util.List;
 
 import org.apache.wicket.markup.head.HeaderItem;
 import org.apache.wicket.markup.head.JavaScriptHeaderItem;
 import org.apache.wicket.request.resource.ResourceReference;
 import org.apache.wicket.resource.DynamicJQueryResourceReference;
+import org.apache.wicket.util.lang.Generics;
 
 /**
  * Provides the {@link ResourceReference} for the {@code jquery-migrate-3.0.0.js} javascript library (CDN).<br>
@@ -44,7 +44,7 @@ public class JQueryMigrateResourceReference extends JQueryUrlResourceReference
 	 *
 	 * @return the single instance of the resource reference
 	 */
-	public static JQueryUrlResourceReference get()
+	public static JQueryMigrateResourceReference get()
 	{
 		return INSTANCE;
 	}
@@ -52,6 +52,9 @@ public class JQueryMigrateResourceReference extends JQueryUrlResourceReference
 	@Override
 	public List<HeaderItem> getDependencies()
 	{
-		return Arrays.asList(JavaScriptHeaderItem.forReference(DynamicJQueryResourceReference.getV3()));
+		List<HeaderItem> dependencies = Generics.newArrayList();
+		dependencies.add(JavaScriptHeaderItem.forReference(DynamicJQueryResourceReference.getV3()));
+
+		return dependencies;
 	}
 }
