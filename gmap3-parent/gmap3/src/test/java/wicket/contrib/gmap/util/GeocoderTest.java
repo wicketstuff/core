9a3bf8c7c0b39fabbe1cc18ce768f7212927b746
Maxim Solodovnik <solomax666@gmail.com>
2018-02-19 21:49:46 +0700

    Redundant local Geocoder is removed

--- gmap3-parent/gmap3/src/test/java/wicket/contrib/gmap/util/GeocoderTest.java
+++ gmap3-parent/gmap3/src/test/java/wicket/contrib/gmap/util/GeocoderTest.java
@@ -15,18 +15,17 @@ public class GeocoderTest
 {
 	public static final String DEFAULT_API_KEY = "YOUR_API_KEY";
 	private String apiKey = DEFAULT_API_KEY;
-	private Geocoder coder;
 
 	@Before
 	public void setUp()
 	{
 		apiKey = System.getProperty("wicketstuff.gmap3.apiKey", DEFAULT_API_KEY);
-		coder = new Geocoder(apiKey);
 	}
 
 	@Test
 	public void testEncode()
 	{
+		Geocoder coder = new Geocoder(apiKey);
 		String encode = coder.encode("Salzburgerstraße 205, 4030 Linz, Österreich");
 		Assert.assertEquals(new StringBuilder("http://maps.googleapis.com/maps/api/geocode/json?apiKey=")
 				.append(apiKey).append("&address=Salzburgerstra%C3%9Fe+205%2C+4030+Linz%2C+%C3%96sterreich").toString()
