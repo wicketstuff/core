6d1dbc95ca0a81833b1f48de978ff74fb104afdf
Maxim Solodovnik <solomax666@gmail.com>
2018-07-23 13:38:01 +0700

    Merge pull request #639 from aldaris/depfix
    Remove test library from runtime dependency path

--- javaee-inject-parent/javaee-inject/src/test/java/org/wicketstuff/javaee/naming/global/GlobalJndiNamingStrategyTest.java
+++ javaee-inject-parent/javaee-inject/src/test/java/org/wicketstuff/javaee/naming/global/GlobalJndiNamingStrategyTest.java
@@ -1,10 +1,23 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.wicketstuff.javaee.naming.global;
 
-
+import org.testng.Assert;
 import org.testng.annotations.Test;
-
-import static org.fest.assertions.Assertions.assertThat;
-
 
 /**
  * The container has to register one JNDI global entry for every local and remote business interface implemented by the EJB and its no-interface view.
@@ -23,8 +36,9 @@ public class GlobalJndiNamingStrategyTest {
 
     @Test
     public void nameAccordingToSpecification() {
-        assertThat(namingStrategy.calculateName(null, String.class)).isEqualTo("java:global/appname/modulename/java.lang.String");
-        assertThat(namingStrategy.calculateName("beanname", String.class)).isEqualTo("java:global/appname/modulename/beanname!java.lang.String");
+        Assert.assertEquals(namingStrategy.calculateName(null, String.class),
+                "java:global/appname/modulename/java.lang.String");
+        Assert.assertEquals(namingStrategy.calculateName("beanname", String.class),
+                "java:global/appname/modulename/beanname!java.lang.String");
     }
-
 }
