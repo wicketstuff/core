ce67a224bac0895eef330d7cb57435a4f8b48617
Sebastien Briquet <sbriquet@apache.org>
2019-03-16 12:28:47 +0100

    Kendo UI: Upgraded to kendoui.2019.1.220.core

--- wicket-kendo-ui-culture/src/main/resources/com/googlecode/wicket/kendo/ui/resource/cultures/kendo.culture.kw.min.js
+++ wicket-kendo-ui-culture/src/main/resources/com/googlecode/wicket/kendo/ui/resource/cultures/kendo.culture.kw.min.js
@@ -1,5 +1,5 @@
 /** 
- * Copyright 2018 Telerik EAD                                                                                                                                                                           
+ * Copyright 2019 Progress Software Corporation and/or one of its subsidiaries or affiliates. All rights reserved.                                                                                      
  *                                                                                                                                                                                                      
  * Licensed under the Apache License, Version 2.0 (the "License");                                                                                                                                      
  * you may not use this file except in compliance with the License.                                                                                                                                     
