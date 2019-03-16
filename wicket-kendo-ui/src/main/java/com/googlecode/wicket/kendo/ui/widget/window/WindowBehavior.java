0713fb4c93d539f6845c58e3c02bab542fe039c9
Sebastien Briquet <sbriquet@apache.org>
2019-03-16 12:22:46 +0100

    Kendo UI - WindowBehavior, fixed custom button click event handler

--- wicket-kendo-ui/src/main/java/com/googlecode/wicket/kendo/ui/widget/window/WindowBehavior.java
+++ wicket-kendo-ui/src/main/java/com/googlecode/wicket/kendo/ui/widget/window/WindowBehavior.java
@@ -201,11 +201,8 @@ public class WindowBehavior extends KendoUIBehavior implements IJQueryAjaxAware
 		protected CallbackParameter[] getCallbackParameters()
 		{
 			return new CallbackParameter[] { CallbackParameter.context("e"), // lf
-					CallbackParameter.resolved("action", "jQuery(e.target).attr('class').match(/k-i-(\\w+)/)[1]") // lf
+					CallbackParameter.resolved("action", "jQuery(e.target).find('span').attr('class').match(/k-i-(\\w+)/)[1]") // lf
 			};
-			
-			// FIXME TypeError: jQuery(...).attr(...).match(...) is null
-			// var params = [{"name":"action","value":jQuery(e.target).attr('class').match(/k-i...
 		}
 
 		@Override
