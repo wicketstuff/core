b5486485fd7e3ad3ecb2e5e16a3b5cc43da20c8c
Maxim Solodovnik <solomax666@gmail.com>
2019-04-15 22:02:59 +0700

    Initial attempt to fix cometd (tests are 'semi-working')

--- push-parent/push-cometd/src/main/java/org/wicketstuff/push/cometd/CometdPushEventHandlerTemplate.js
+++ push-parent/push-cometd/src/main/java/org/wicketstuff/push/cometd/CometdPushEventHandlerTemplate.js
@@ -2,23 +2,20 @@ var onEventFor${COMETD_CHANNEL_ID}_isRunning = false;
 var onEventFor${COMETD_CHANNEL_ID}_isScheduled = false;
 function onEventFor${COMETD_CHANNEL_ID}(message) {
 	if (message.data == "pollEvents") {
-	    if (onEventFor${COMETD_CHANNEL_ID}_isRunning)
-	    {
-	    	onEventFor${COMETD_CHANNEL_ID}_isScheduled = true
-	    }
-	    else
-		{
+		if (onEventFor${COMETD_CHANNEL_ID}_isRunning) {
+			onEventFor${COMETD_CHANNEL_ID}_isScheduled = true
+		} else {
 			onEventFor${COMETD_CHANNEL_ID}_isScheduled = false
 			onEventFor${COMETD_CHANNEL_ID}_isRunning = true
-		    onFinished = function() {
-		    	onEventFor${COMETD_CHANNEL_ID}_isRunning = false
-		    	if (onEventFor${COMETD_CHANNEL_ID}_isScheduled)
+			onFinished = function() {
+				onEventFor${COMETD_CHANNEL_ID}_isRunning = false
+				if (onEventFor${COMETD_CHANNEL_ID}_isScheduled) {
 					onEventFor${COMETD_CHANNEL_ID}(message);
-		    }
-			wicketAjaxGet('${CALLBACK_URL}', onFinished, onFinished);
+				}
+			}
+			Wicket.Ajax.get({u: '${CALLBACK_URL}', sh: onFinished, fh: onFinished});
 		}
-	}
-	else if (message.data.match("^javascript:")) {
+	} else if (message.data.match("^javascript:")) {
 		eval(message.data.substring(12));
 	}
 }
