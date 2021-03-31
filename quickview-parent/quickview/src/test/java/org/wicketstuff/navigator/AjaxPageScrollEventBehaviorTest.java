2af78a9b997cef07755d665c9069a472df77f46d
Maxim Solodovnik <solomax666@gmail.com>
2021-03-31 21:08:19 +0700

    Dependencies are updated, build is fixed

--- quickview-parent/quickview/src/test/java/org/wicketstuff/navigator/AjaxPageScrollEventBehaviorTest.java
+++ quickview-parent/quickview/src/test/java/org/wicketstuff/navigator/AjaxPageScrollEventBehaviorTest.java
@@ -16,49 +16,44 @@
  */
 package org.wicketstuff.navigator;
 
-import org.wicketstuff.RepeaterUtil;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.attributes.AjaxRequestAttributes;
 import org.apache.wicket.ajax.attributes.IAjaxCallListener;
-import org.apache.wicket.markup.head.IHeaderResponse;
-import org.apache.wicket.markup.head.OnDomReadyHeaderItem;
 import org.apache.wicket.markup.html.WebMarkupContainer;
-import org.mockito.Mockito;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.wicketstuff.RepeaterUtil;
 
 /**
  * @author Vineet Semwal
  *
  */
-public class AjaxPageScrollEventBehaviorTest {
-    @Test(groups = {"wicketTests"})
-    public void  getPrecondition(){
-        WebMarkupContainer parent=new WebMarkupContainer("parent");
-        AjaxPageScrollEventBehavior.PageScrollListener listener=new AjaxPageScrollEventBehavior.PageScrollListener();
-        String actual= listener.getPrecondition(parent).toString();
-        String expected="return "+ RepeaterUtil.get().isPageScrollBarAtBottom();
-        Assert.assertEquals(actual, expected);
+public class AjaxPageScrollEventBehaviorTest extends TesterBase {
+	@Test(groups = { "wicketTests" })
+	public void getPrecondition() {
+		WebMarkupContainer parent = new WebMarkupContainer("parent");
+		AjaxPageScrollEventBehavior.PageScrollListener listener = new AjaxPageScrollEventBehavior.PageScrollListener();
+		String actual = listener.getPrecondition(parent).toString();
+		String expected = "return " + RepeaterUtil.get().isPageScrollBarAtBottom();
+		Assert.assertEquals(actual, expected);
 
-    }
+	}
 
-    @Test(groups = {"wicketTests"})
-    public void updateAjaxAttributes(){
-        AjaxPageScrollEventBehavior behavior=new AjaxPageScrollEventBehavior(){
-            @Override
-            protected void onScroll(AjaxRequestTarget target) {
-            }
-        };
-        AjaxRequestAttributes attributes=new AjaxRequestAttributes();
-        behavior.updateAjaxAttributes(attributes);
-        boolean  isAdded=false;
-        for(IAjaxCallListener listener:attributes.getAjaxCallListeners())
-        {
-            if(listener instanceof AjaxPageScrollEventBehavior.PageScrollListener){
-                isAdded=true;
-            }
-        }
-        Assert.assertTrue(isAdded);
-    }
-    
+	@Test(groups = { "wicketTests" })
+	public void updateAjaxAttributes() {
+		AjaxPageScrollEventBehavior behavior = new AjaxPageScrollEventBehavior() {
+			@Override
+			protected void onScroll(AjaxRequestTarget target) {
+			}
+		};
+		AjaxRequestAttributes attributes = new AjaxRequestAttributes();
+		behavior.updateAjaxAttributes(attributes);
+		boolean isAdded = false;
+		for (IAjaxCallListener listener : attributes.getAjaxCallListeners()) {
+			if (listener instanceof AjaxPageScrollEventBehavior.PageScrollListener) {
+				isAdded = true;
+			}
+		}
+		Assert.assertTrue(isAdded);
+	}
 }
