9ea33c91f91af9b5f76addccec334854eabd6288
Silas Porth <silas@porth-odernheim.de>
2023-10-26 17:40:42 +0200

    Expanded Example

--- editable-grid-parent/editable-grid-examples/src/main/java/org/wicketstuff/egrid/model/Person.java
+++ editable-grid-parent/editable-grid-examples/src/main/java/org/wicketstuff/egrid/model/Person.java
@@ -1,61 +1,65 @@
 package org.wicketstuff.egrid.model;
 
+import java.io.Serial;
 import java.io.Serializable;
 
-public class Person implements Serializable
-{
-	private static final long serialVersionUID = 1L;
+public class Person implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
 
-	private String name;
-	private String address;
-	private String age;
-	
-	public Person()
-	{
+    private String name;
+    private String address;
+    private int age;
+    private boolean married;
 
-	}
-	public Person(String name) 
-	{
-		this.name = name;
-	}
-	public Person(String name, String age, String address)
-	{
-		this.name = name;
-		this.age = age;
-		this.address = address;
-	}
+    public Person() {
+    }
 
-	public String getName()
-	{
-		return name;
-	}
+    public Person(final String name) {
+        this.name = name;
+    }
 
-	public void setName(String name)
-	{
-		this.name = name;
-	}
-	
-	public String getAge()
-	{
-		return age;
-	}
+    public Person(final String name, final int age, final String address, final boolean married) {
+        this.name = name;
+        this.age = age;
+        this.address = address;
+        this.married = married;
+    }
 
-	public void setAge(String age)
-	{
-		this.age = age;
-	}	
+    public String getName() {
+        return name;
+    }
 
-	public String getAddress()
-	{
-		return address;
-	}
-	public void setAddress(String address)
-	{
-		this.address = address;
-	}
-	@Override
-	public String toString()
-	{
-		return name;
-	}
+    public void setName(final String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(final int age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(final String address) {
+        this.address = address;
+    }
+
+    public boolean isMarried() {
+        return married;
+    }
+
+    public void setMarried(boolean married) {
+        this.married = married;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
 }
