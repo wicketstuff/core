ded8b0d33a74f1b9f4ab9360f898f6c91fbcfba2
renoth <johannes@renoth.dev>
2024-02-16 10:16:40 +0100

    - Fix Test

--- tinymce6-parent/tinymce6/src/test/java/wicket/contrib/tinymce/settings/TinyMCESettingsTest.java
+++ tinymce6-parent/tinymce6/src/test/java/wicket/contrib/tinymce/settings/TinyMCESettingsTest.java
@@ -2,39 +2,29 @@ package wicket.contrib.tinymce.settings;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.wicketstuff.tinymce6.settings.TinyMCESettings;
 
 /**
  * @author Iulian-Corneliu Costan (iulian.costan@gmail.com)
  * @author Frank Bille (fbille@avaleo.net)
+ * @author Johannes Renoth
  */
-public class TinyMCESettingsTest {
-	private TinyMCESettings settings;
-
-	@BeforeEach
-	protected void setUp() throws Exception {
-		settings = new TinyMCESettings(TinyMCESettings.Language.en_GB);
-	}
-
-	@AfterEach
-	protected void tearDown() throws Exception {
-		settings = null;
-	}
-
+class TinyMCESettingsTest
+{
 	@Test
-	public void testCustomSettings() {
+	void testCustomSettings()
+	{
+		final var settings = new TinyMCESettings(TinyMCESettings.Language.en_GB);
 		settings.addCustomSetting("setting_1: false");
 		settings.addCustomSetting("setting : \"help,blah\"");
 		assertEquals("""
-				,
-				\tpromotion : false,
-				\tstatusbar : false,
-				\ttypeahead_urls : false,
-				\tsetting_1: false,
-				\tsetting : "help,blah\"""", settings.toJavaScript());
+			,
+			\tpromotion : false,
+			\tstatusbar : false,
+			\ttypeahead_urls : false,
+			\tsetting_1: false,
+			\tsetting : "help,blah\"""", settings.toJavaScript());
 	}
 
 }
