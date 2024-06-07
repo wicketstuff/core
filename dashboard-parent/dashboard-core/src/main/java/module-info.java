e4df3d3b034ab32e650b1e2e59a01ed1b5335d09
Paul Anand <paul.anand@mcruncher.com>
2024-06-07 12:14:22 +0800

    Revert "#931 Fixed indentations"
    This reverts commit bbdff966fcba326f8dc675bb707ca8ff0cf8cad0.

--- dashboard-parent/dashboard-core/src/main/java/module-info.java
+++ dashboard-parent/dashboard-core/src/main/java/module-info.java
@@ -1,12 +1,12 @@
 module org.wicketstuff.dashboard {
-    exports org.wicketstuff.dashboard;
-    exports org.wicketstuff.dashboard.web;
-    exports org.wicketstuff.dashboard.web.util;
+	exports org.wicketstuff.dashboard;
+	exports org.wicketstuff.dashboard.web;
+	exports org.wicketstuff.dashboard.web.util;
 
-    requires org.apache.wicket.core;
-    requires org.apache.wicket.request;
-    requires org.apache.wicket.util;
-    requires xstream;
-    requires com.google.gson;
+	requires org.apache.wicket.core;
+	requires org.apache.wicket.request;
+	requires org.apache.wicket.util;
+	requires xstream;
+	requires com.google.gson;
     requires de.agilecoders.wicket.webjars;
 }
