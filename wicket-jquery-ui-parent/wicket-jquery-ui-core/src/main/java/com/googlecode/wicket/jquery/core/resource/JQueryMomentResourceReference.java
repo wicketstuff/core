e56994fb3b5d02244cabe148f38c63d524b3afc1
Maxim Solodovnik <solomax@apache.org>
2024-06-27 18:23:35 +0700

    Fixes issue #954: momentjs webjar is aligned with wicket-bootstrap

--- wicket-jquery-ui-parent/wicket-jquery-ui-core/src/main/java/com/googlecode/wicket/jquery/core/resource/JQueryMomentResourceReference.java
+++ wicket-jquery-ui-parent/wicket-jquery-ui-core/src/main/java/com/googlecode/wicket/jquery/core/resource/JQueryMomentResourceReference.java
@@ -17,13 +17,6 @@
 package com.googlecode.wicket.jquery.core.resource;
 
 import de.agilecoders.wicket.webjars.request.resource.WebjarsJavaScriptResourceReference;
-import org.apache.wicket.Application;
-import org.apache.wicket.markup.head.HeaderItem;
-import org.apache.wicket.markup.head.JavaScriptHeaderItem;
-import org.apache.wicket.request.resource.ResourceReference;
-import org.apache.wicket.resource.JQueryResourceReference;
-
-import java.util.List;
 
 /**
  * The resource reference for the jQuery Moment javascript library.
@@ -52,25 +45,6 @@ public class JQueryMomentResourceReference extends WebjarsJavaScriptResourceRefe
 	 */
 	private JQueryMomentResourceReference()
 	{
-		super("moment/current/moment.js");
-	}
-
-	@Override
-	public List<HeaderItem> getDependencies()
-	{
-		final ResourceReference backingLibraryReference;
-		if (Application.exists())
-		{
-			backingLibraryReference = Application.get()
-					.getJavaScriptLibrarySettings()
-					.getJQueryReference();
-		}
-		else
-		{
-			backingLibraryReference = JQueryResourceReference.getV3();
-		}
-		List<HeaderItem> dependencies = super.getDependencies();
-		dependencies.add(JavaScriptHeaderItem.forReference(backingLibraryReference));
-		return dependencies;
+		super("momentjs/current/min/moment.min.js");
 	}
 }
