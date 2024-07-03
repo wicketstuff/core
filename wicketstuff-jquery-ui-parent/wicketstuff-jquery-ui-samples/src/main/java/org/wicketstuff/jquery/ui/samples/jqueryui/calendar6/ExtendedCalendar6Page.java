2f6b066e09e960eff1a61889c2cc391ec4cb22e5
Maxim Solodovnik <solomax666@gmail.com>
2024-07-03 19:34:57 +0700

    Calendar6 fix (#960)
    * Drag'n'drop for Calendar v6 is fixed; Automatic-Module-Names are added
    
    * LocalDateTime is used in all callbacks

--- wicketstuff-jquery-ui-parent/wicketstuff-jquery-ui-samples/src/main/java/org/wicketstuff/jquery/ui/samples/jqueryui/calendar6/ExtendedCalendar6Page.java
+++ wicketstuff-jquery-ui-parent/wicketstuff-jquery-ui-samples/src/main/java/org/wicketstuff/jquery/ui/samples/jqueryui/calendar6/ExtendedCalendar6Page.java
@@ -14,12 +14,15 @@ import org.apache.wicket.markup.html.panel.FeedbackPanel;
 import org.wicketstuff.jquery.core.Options;
 import org.wicketstuff.jquery.ui.calendar6.Calendar;
 import org.wicketstuff.jquery.ui.calendar6.CalendarView;
+import org.wicketstuff.jquery.ui.calendar6.DateTimeDelta;
 import org.wicketstuff.jquery.ui.panel.JQueryFeedbackPanel;
 import org.wicketstuff.jquery.ui.samples.component.DemoCalendar6Dialog;
 import org.wicketstuff.jquery.ui.samples.data.DemoCalendar6Event;
 import org.wicketstuff.jquery.ui.samples.data.DemoCalendar6Model;
 import org.wicketstuff.jquery.ui.samples.data.dao.Calendar6DAO;
 import org.wicketstuff.jquery.ui.widget.dialog.DialogButton;
+
+import com.github.openjson.JSONObject;
 
 public class ExtendedCalendar6Page extends AbstractCalendar6Page // NOSONAR
 {
@@ -136,14 +139,26 @@ public class ExtendedCalendar6Page extends AbstractCalendar6Page // NOSONAR
 			}
 
 			@Override
-			public void onEventDrop(AjaxRequestTarget target, String eventId, long delta, boolean allDay)
+			public void onEventDrop(AjaxRequestTarget target, String eventId, DateTimeDelta delta, boolean allDay)
 			{
 				DemoCalendar6Event event = Calendar6DAO.getEvent(eventId);
 
 				if (event != null)
 				{
-					event.setStart(event.getStart() != null ? event.getStart().plus(delta, ChronoUnit.MILLIS) : null); // recompute start date
-					event.setEnd(event.getEnd() != null ? event.getEnd().plus(delta, ChronoUnit.MILLIS) : null); // recompute end date
+					event.setStart(event.getStart() == null
+							? null
+							: event.getStart()
+									.plus(delta.years(), ChronoUnit.YEARS)
+									.plus(delta.months(), ChronoUnit.MONTHS)
+									.plus(delta.days(), ChronoUnit.DAYS)
+									.plus(delta.millis(), ChronoUnit.MILLIS)); // recompute start date
+					event.setEnd(event.getEnd() == null
+							? null
+							: event.getEnd()
+									.plus(delta.years(), ChronoUnit.YEARS)
+									.plus(delta.months(), ChronoUnit.MONTHS)
+									.plus(delta.days(), ChronoUnit.DAYS)
+									.plus(delta.millis(), ChronoUnit.MILLIS)); // recompute end date
 					event.setAllDay(allDay);
 
 					this.info(String.format("%s changed to %s", event.getTitle(), event.getStart()));
@@ -152,14 +167,18 @@ public class ExtendedCalendar6Page extends AbstractCalendar6Page // NOSONAR
 			}
 
 			@Override
-			public void onEventResize(AjaxRequestTarget target, String eventId, long delta)
+			public void onEventResize(AjaxRequestTarget target, String eventId, DateTimeDelta delta)
 			{
 				DemoCalendar6Event event = Calendar6DAO.getEvent(eventId);
 
 				if (event != null)
 				{
 					LocalDateTime date = event.getEnd() == null ? event.getStart() : event.getEnd();
-					event.setEnd(date.plus(delta, ChronoUnit.MILLIS));
+					event.setEnd(date
+							.plus(delta.years(), ChronoUnit.YEARS)
+							.plus(delta.months(), ChronoUnit.MONTHS)
+							.plus(delta.days(), ChronoUnit.DAYS)
+							.plus(delta.millis(), ChronoUnit.MILLIS));
 
 					this.info(String.format("%s now ends the %s", event.getTitle(), event.getEnd()));
 					target.add(feedback);
