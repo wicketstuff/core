f46fc954ddc81b10117685b2fd4b4a8e8259f103
Maxim Solodovnik <solomax@apache.org>
2026-03-22 18:29:34 +0700

    Jasperreports are updated to 7.x
    Signed-off-by: Maxim Solodovnik <solomax@apache.org>

--- jasperreports-parent/jasperreports-examples/src/main/java/org/wicketstuff/jasperreports/examples/SimplePdfPage.java
+++ jasperreports-parent/jasperreports-examples/src/main/java/org/wicketstuff/jasperreports/examples/SimplePdfPage.java
@@ -12,14 +12,10 @@
  */
 package org.wicketstuff.jasperreports.examples;
 
-import jakarta.servlet.ServletContext;
-
 import java.io.File;
-import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.wicket.markup.html.WebPage;
-import org.apache.wicket.protocol.http.WebApplication;
 import org.wicketstuff.jasperreports.EmbeddedJRReport;
 import org.wicketstuff.jasperreports.JRConcreteResource;
 import org.wicketstuff.jasperreports.JRResource;
@@ -27,26 +23,22 @@ import org.wicketstuff.jasperreports.handlers.PdfResourceHandler;
 
 /**
  * Simple Jasper reports example with PDF output and a jasper reports panel..
- * 
+ *
  * @author Eelco Hillenius
  */
-public class SimplePdfPage extends WebPage
-{
+public class SimplePdfPage extends WebPage {
 	private static final long serialVersionUID = 1L;
 
 	/**
 	 * Constructor.
 	 */
-	public SimplePdfPage()
-	{
-		ServletContext context = ((WebApplication)getApplication()).getServletContext();
-		final File reportFile = new File(context.getRealPath("/reports/WebappReport.jasper"));
+	public SimplePdfPage() {
+		final File base = new File(System.getProperty("jasperreportsDirectory"));
+		final File reportFile = new File(base, "WebappReport.jasper");
 
-		final Map<String, Object> parameters = new HashMap<String, Object>();
-		parameters.put("BaseDir", new File(context.getRealPath("/reports")));
-		JRResource pdfResource = new JRConcreteResource<PdfResourceHandler>(reportFile,
-			new PdfResourceHandler()).setReportParameters(parameters).setReportDataSource(
-			new WebappDataSource());
+		JRResource pdfResource = new JRConcreteResource<PdfResourceHandler>(reportFile, new PdfResourceHandler())
+				.setReportParameters(Map.of("BaseDir", base))
+				.setReportDataSource(new WebappDataSource());
 		add(new EmbeddedJRReport("report", pdfResource));
 	}
 
@@ -54,8 +46,7 @@ public class SimplePdfPage extends WebPage
 	 * @see org.apache.wicket.Component#isVersioned()
 	 */
 	@Override
-	public boolean isVersioned()
-	{
+	public boolean isVersioned() {
 		return false;
 	}
 }
