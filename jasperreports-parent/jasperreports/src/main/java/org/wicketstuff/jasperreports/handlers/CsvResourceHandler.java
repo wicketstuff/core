2397dd585267b9687fecb967aaf3dac5cc775a49
Maxim Solodovnik <solomax@apache.org>
2026-03-24 15:58:29 +0700

    Examples more or less works as expected
    Signed-off-by: Maxim Solodovnik <solomax@apache.org>

--- jasperreports-parent/jasperreports/src/main/java/org/wicketstuff/jasperreports/handlers/CsvResourceHandler.java
+++ jasperreports-parent/jasperreports/src/main/java/org/wicketstuff/jasperreports/handlers/CsvResourceHandler.java
@@ -1,14 +1,14 @@
 /*
  * $Id: CsvResourceHandler.java 5335 2010-06-14 13:28:39Z cdeal $ $Revision:
  * 1.4 $ $Date: 2010-06-14 09:28:39 -0400 (Mon, 14 Jun 2010) $
- * 
+ *
  * ==============================================================================
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -17,10 +17,13 @@
  */
 package org.wicketstuff.jasperreports.handlers;
 
+import java.io.OutputStream;
 import java.io.Serializable;
 
 import net.sf.jasperreports.engine.JRAbstractExporter;
 import net.sf.jasperreports.engine.export.JRCsvExporter;
+import net.sf.jasperreports.export.ExporterOutput;
+import net.sf.jasperreports.export.SimpleWriterExporterOutput;
 
 /**
  * @author cdeal
@@ -35,6 +38,11 @@ public class CsvResourceHandler implements IJRResourceHandler, Serializable
 	public JRAbstractExporter newExporter()
 	{
 		return new JRCsvExporter();
+	}
+
+	@Override
+	public ExporterOutput newExporterOutput(OutputStream os) {
+		return new SimpleWriterExporterOutput(os);
 	}
 
 	/**
